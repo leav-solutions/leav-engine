43683e6688039b43215eccaf6efb7960ea2f52de
Teddy <t.paul@cacom.fr>
2018-05-22 18:00:57 +0200

    ActionsList: add formatNumber

---
+++ src/domain/coreActions/formatNumberAction.ts
@@ -0,0 +1,80 @@
+import {
+    ActionsListIOTypes,
+    IActionsListContext,
+    IActionsListFunction,
+    ActionsListValueType
+} from '../../_types/actionsList';
+import {flow, partialRight} from 'lodash';
+
+export default function(): IActionsListFunction {
+    const _toString = (num, d) =>
+        num.toLocaleString('en', {
+            minimumFractionDigits: d,
+            maximumFractionDigits: d
+        });
+
+    const _formatSeparators = (num, thousSep, decSep) => {
+        const newSeps = {',': thousSep, '.': decSep};
+        return num.replace(/[,.]/g, m => newSeps[m]);
+    };
+
+    const _addPrefix = (n, prefix) => '' + prefix + n;
+    const _addSuffix = (n, suffix) => '' + n + suffix;
+
+    return {
+        name: 'formatNumber',
+        description: 'Format a number',
+        inputTypes: [ActionsListIOTypes.NUMBER],
+        outputTypes: [ActionsListIOTypes.STRING],
+        params: [
+            {
+                name: 'decimals',
+                type: 'number',
+                description: 'Number of decimals'
+            },
+            {
+                name: 'thousandsSeparator',
+                type: 'string',
+                description: 'Thousands separator'
+            },
+            {
+                name: 'decimalSeparator',
+                type: 'string',
+                description: 'Decimals separator'
+            },
+            {
+                name: 'prefix',
+                type: 'string',
+                description: 'Number prefix'
+            },
+            {
+                name: 'suffix',
+                type: 'string',
+                description: 'Number suffix'
+            }
+        ],
+        action: (value: ActionsListValueType, params: any, ctx: IActionsListContext): string => {
+            const defaultParams = {
+                decimals: 2,
+                thousandsSeparator: ',',
+                decimalsSeparator: '.',
+                prefix: '',
+                suffix: ''
+            };
+
+            const userParams = {
+                ...defaultParams,
+                ...params
+            };
+
+            return isNaN(Number(value))
+                ? ''
+                : flow(
+                      partialRight(_toString, userParams.decimals),
+                      partialRight(_formatSeparators, userParams.thousandsSeparator, userParams.decimalsSeparator),
+                      partialRight(_addPrefix, userParams.prefix),
+                      partialRight(_addSuffix, userParams.suffix)
+                  )(Number(value));
+        }
+    };
+}
