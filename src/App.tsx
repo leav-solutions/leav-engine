344aa477a9e02827f08a521572dfcc0e0d2dec1e
Teddy <t.paul@cacom.fr>
2018-08-23 11:57:11 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to Admin App</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
