01cc4e2daf7ded3aa058f76448db01441b63f1bd
Teddy <t.paul@cacom.fr>
2018-08-23 17:09:48 +0200

    Add Apollo Provider for Graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
+import ApolloClient from 'apollo-boost';
 import * as React from 'react';
+import {ApolloProvider} from 'react-apollo';
 import './App.css';
 
-class App extends React.Component {
-    public render() {
-        return (
+function App(): JSX.Element {
+    // TODO: handle auth token properly
+    const gqlClient = new ApolloClient({
+        uri: process.env.REACT_APP_API_URL,
+        headers: {
+            Authorization: process.env.REACT_APP_USER_TOKEN
+        }
+    });
+
+    return (
+        <ApolloProvider client={gqlClient}>
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Welcome to Admin App</h1>
                 </header>
+                <p>{process.env.REACT_APP_API_URL}</p>
             </div>
-        );
-    }
+        </ApolloProvider>
+    );
 }
 
 export default App;
