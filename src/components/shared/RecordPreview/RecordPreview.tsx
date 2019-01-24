02b8c754beb12f5bc9dac37ec0fa7739f626d176
Teddy <t.paul@cacom.fr>
2019-01-24 11:35:43 +0100

    Fix bug record preview color change on each render

--- src/components/shared/RecordPreview/RecordPreview.tsx
+++ src/components/shared/RecordPreview/RecordPreview.tsx
@@ -9,35 +9,45 @@ interface IRecordPreviewProps {
     style?: React.CSSProperties;
 }
 
-function RecordPreview({label, color, image, style}: IRecordPreviewProps): JSX.Element {
-    if (image) {
-        return <Image src={image} avatar style={style} />;
+class RecordPreview extends React.Component<IRecordPreviewProps> {
+    constructor(props: IRecordPreviewProps) {
+        super(props);
     }
 
-    const initial = label[0].toLocaleUpperCase();
+    public shouldComponentUpdate = (p): boolean => p.color !== this.props.color || p.image !== this.props.image;
 
-    const bgColor = color || getRandomColor();
-    const fontColor = getInvertColor(bgColor);
+    public render() {
+        const {label, color, image, style} = this.props;
 
-    const forcedStyle: React.CSSProperties = {
-        backgroundColor: bgColor,
-        color: fontColor,
-        fontSize: '1.1em',
-        height: '2em',
-        width: '2em',
-        padding: '5px',
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        borderRadius: '50%'
-    };
+        if (image) {
+            return <Image src={image} avatar style={style} />;
+        }
 
-    return (
-        <div className="initial" style={{...style, ...forcedStyle}}>
-            {initial}
-        </div>
-    );
+        const initial = label[0].toLocaleUpperCase();
+
+        const bgColor = color || getRandomColor();
+        const fontColor = getInvertColor(bgColor);
+
+        const forcedStyle: React.CSSProperties = {
+            backgroundColor: bgColor,
+            color: fontColor,
+            fontSize: '1.1em',
+            height: '2em',
+            width: '2em',
+            padding: '5px',
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            borderRadius: '50%'
+        };
+
+        return (
+            <div className="initial" style={{...style, ...forcedStyle}}>
+                {initial}
+            </div>
+        );
+    }
 }
 
 export default RecordPreview;
