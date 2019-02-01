dd88467148aac8e1a9fc254067d4aae629ee4c83
Teddy <t.paul@cacom.fr>
2019-02-01 15:03:18 +0100

    Fix bug on RecordCard with styled-components

--- src/components/shared/RecordCard/RecordCard.tsx
+++ src/components/shared/RecordCard/RecordCard.tsx
@@ -10,39 +10,44 @@ interface IRecordCardProps extends WithNamespaces {
     style?: CSSObject;
 }
 
+interface IWrapperProps {
+    recordColor: string | null;
+    style?: CSSObject;
+}
+
+/* tslint:disable:variable-name */
+const Wrapper = styled.div<IWrapperProps>`
+    border-left: 5px solid ${props => props.recordColor || 'transparent'};
+    display: flex;
+    flex-direction: row;
+    ${props => props.style}
+`;
+Wrapper.displayName = 'Wrapper';
+
+const CardPart = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const PreviewWrapper = styled(CardPart)`
+    margin: 0 0.8em;
+`;
+
+const RecordLabel = styled.div`
+    font-weight: bold;
+`;
+
+const LibLabel = styled.div`
+    font-weight: normal;
+    color: rgba(0, 0, 0, 0.4);
+    fontsize: 0.9em;
+`;
+/* tslint:enable:variable-name */
+
 function RecordCard({record, style, i18n}: IRecordCardProps): JSX.Element {
-    /* tslint:disable:variable-name */
-    const Wrapper = styled.div`
-        border-left: 5px solid ${record.color || 'transparent'};
-        display: flex;
-        flex-direction: row;
-        ${style}
-    `;
-    Wrapper.displayName = 'Wrapper';
-
-    const CardPart = styled.div`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-    `;
-
-    const PreviewWrapper = styled(CardPart)`
-        margin: 0 0.8em;
-    `;
-
-    const RecordLabel = styled.div`
-        font-weight: bold;
-    `;
-
-    const LibLabel = styled.div`
-        font-weight: normal;
-        color: rgba(0, 0, 0, 0.4);
-        fontsize: 0.9em;
-    `;
-    /* tslint:enable:variable-name */
-
     return (
-        <Wrapper className="ui fluid">
+        <Wrapper recordColor={record.color} style={style} className="ui fluid">
             <PreviewWrapper className="ui">
                 <RecordPreview label={record.label || record.id} color={record.color} image={record.preview} />
             </PreviewWrapper>
