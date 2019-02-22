8a137a514742a2dcd5cea342bfbd2b2b351f99a0
Teddy <t.paul@cacom.fr>
2019-02-22 17:20:30 +0100

    Fix Mockify

--- src/_types/Mockify.ts
+++ src/_types/Mockify.ts
@@ -1,3 +1,3 @@
 // Used to mock any interface, turning all function properties to an optionnal mock
 // Mockified object must be then passed to a function with a type assertion
-export type Mockify<T> = {[P in keyof T]?: P extends () => void ? jest.Mock<{}> : T[P]};
+export type Mockify<T> = {[P in keyof T]?: T[P] extends (...args: any) => any ? jest.Mock : T[P]};
