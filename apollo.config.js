ca117ca3a94ec09ae62ee2eb5256a476575611a2
Teddy <t.paul@cacom.fr>
2019-02-28 10:50:43 +0100

    Upgrade Apollo client

---
+++ apollo.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    client: {
+        service: {
+            name: 'default',
+            url: 'http://localhost:4001/graphql',
+            headers: {
+                authorization:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyMjQ3NjUzNTEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1MjgxOTE4MDgsImV4cCI6NDY1MjM5NDIwOH0.XH6Xu108WdLMpQdOa-KnaZU541QP4jWdCkF1O0yyefg'
+            }
+        }
+    }
+};
