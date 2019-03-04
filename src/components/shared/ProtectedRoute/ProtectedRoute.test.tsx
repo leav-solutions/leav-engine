f24c88696902bf5c89b2a7e3c3dd545509ca01b3
Teddy <t.paul@cacom.fr>
2019-03-04 17:11:23 +0100

    fixup protec route

--- src/components/shared/ProtectedRoute/ProtectedRoute.test.tsx
+++ src/components/shared/ProtectedRoute/ProtectedRoute.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {PermissionsActions} from '../../../_gqlTypes/globalTypes';
 import UserContext from '../UserContext';
+import {IUserContext} from '../UserContext/UserContext';
 import ProtectedRoute from './ProtectedRoute';
 
 describe('ProtectedRoute', () => {
