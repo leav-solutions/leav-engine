ac94667a1e25c7e0cc23cadb271087683de561e3
Teddy <t.paul@cacom.fr>
2019-03-04 17:22:58 +0100

    Convert Libraries to func component with hooks

--- src/components/libraries/Libraries/Libraries.tsx
+++ src/components/libraries/Libraries/Libraries.tsx
@@ -1,6 +1,5 @@
 import {History} from 'history';
-import {i18n} from 'i18next';
-import React from 'react';
+import React, {useState} from 'react';
 import {withNamespaces, WithNamespaces} from 'react-i18next';
 import {Link} from 'react-router-dom';
 import {Button, Grid, Header, Icon} from 'semantic-ui-react';
@@ -9,69 +8,14 @@ import {addWildcardToFilters, getSysTranslationQueryLanguage} from '../../../uti
 import LibrariesList from '../LibrariesList';
 
 interface ILibrariesProps extends WithNamespaces {
-    i18n: i18n;
     history: History;
 }
 
-interface ILibrariesState {
-    filters: any;
-}
+function Libraries({i18n: i18next, t, history}: ILibrariesProps): JSX.Element {
+    const lang = getSysTranslationQueryLanguage(i18next);
+    const [filters, setFilters] = useState<any>({});
 
-class Libraries extends React.Component<ILibrariesProps, ILibrariesState> {
-    constructor(props: ILibrariesProps) {
-        super(props);
-
-        this.state = {
-            filters: {}
-        };
-    }
-
-    public render() {
-        const {i18n: i18next} = this.props;
-        const {filters} = this.state;
-
-        const lang = getSysTranslationQueryLanguage(i18next);
-
-        return (
-            <React.Fragment>
-                <Grid>
-                    <Grid.Column textAlign="left" floated="left" width={8} verticalAlign="middle">
-                        <Header size="large">
-                            <Icon name="folder outline" />
-                            {this.props.t('libraries.title')}
-                        </Header>
-                    </Grid.Column>
-                    <Grid.Column floated="right" width={3} textAlign="right" verticalAlign="middle">
-                        <Button icon labelPosition="left" size="medium" as={Link} to={'/libraries/edit'}>
-                            <Icon name="plus" />
-                            {this.props.t('libraries.new')}
-                        </Button>
-                    </Grid.Column>
-                </Grid>
-                <LibrariesQuery query={getLibsQuery} variables={{...addWildcardToFilters(filters), lang}}>
-                    {({loading, error, data}) => {
-                        if (typeof error !== 'undefined') {
-                            return <p>Error: {error.message}</p>;
-                        }
-
-                        const onRowClick = library => this.props.history.push('/libraries/edit/' + library.id);
-
-                        return (
-                            <LibrariesList
-                                loading={loading || !data}
-                                libraries={data ? data.libraries : []}
-                                onRowClick={onRowClick}
-                                onFiltersUpdate={this._onFiltersUpdate}
-                                filters={filters}
-                            />
-                        );
-                    }}
-                </LibrariesQuery>
-            </React.Fragment>
-        );
-    }
-
-    private _onFiltersUpdate = (filterElem: any) => {
+    const _onFiltersUpdate = (filterElem: any) => {
         const newElemState =
             filterElem.type === 'checkbox'
                 ? filterElem.indeterminate
@@ -79,13 +23,49 @@ class Libraries extends React.Component<ILibrariesProps, ILibrariesState> {
                     : filterElem.checked
                 : filterElem.value;
 
-        this.setState({
-            filters: {
-                ...this.state.filters,
-                [filterElem.name]: newElemState
-            }
+        setFilters({
+            ...filters,
+            [filterElem.name]: newElemState
         });
-    }
+    };
+
+    return (
+        <React.Fragment>
+            <Grid>
+                <Grid.Column textAlign="left" floated="left" width={8} verticalAlign="middle">
+                    <Header size="large">
+                        <Icon name="folder outline" />
+                        {t('libraries.title')}
+                    </Header>
+                </Grid.Column>
+                <Grid.Column floated="right" width={3} textAlign="right" verticalAlign="middle">
+                    <Button icon labelPosition="left" size="medium" as={Link} to={'/libraries/edit'}>
+                        <Icon name="plus" />
+                        {t('libraries.new')}
+                    </Button>
+                </Grid.Column>
+            </Grid>
+            <LibrariesQuery query={getLibsQuery} variables={{...addWildcardToFilters(filters), lang}}>
+                {({loading, error, data}) => {
+                    if (typeof error !== 'undefined') {
+                        return <p>Error: {error.message}</p>;
+                    }
+
+                    const onRowClick = library => history.push('/libraries/edit/' + library.id);
+
+                    return (
+                        <LibrariesList
+                            loading={loading || !data}
+                            libraries={data ? data.libraries : []}
+                            onRowClick={onRowClick}
+                            onFiltersUpdate={_onFiltersUpdate}
+                            filters={filters}
+                        />
+                    );
+                }}
+            </LibrariesQuery>
+        </React.Fragment>
+    );
 }
 
 export default withNamespaces()(Libraries);
