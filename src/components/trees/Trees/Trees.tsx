b02cff5fe5ae656e9e6788f4703bc1e9fde97ec8
Teddy <t.paul@cacom.fr>
2019-03-05 14:50:26 +0100

    Convert Trees to func component

--- src/components/trees/Trees/Trees.tsx
+++ src/components/trees/Trees/Trees.tsx
@@ -1,5 +1,5 @@
 import {History} from 'history';
-import React from 'react';
+import React, {useState} from 'react';
 import {WithNamespaces, withNamespaces} from 'react-i18next';
 import {Link} from 'react-router-dom';
 import {Button, Grid, Header, Icon} from 'semantic-ui-react';
@@ -12,63 +12,11 @@ interface ITreesProps extends WithNamespaces {
     history: History;
 }
 
-interface ITreesState {
-    filters: Partial<GET_TREESVariables>;
-}
+function Trees({history, t, i18n: i18next}: ITreesProps): JSX.Element {
+    const lang = getSysTranslationQueryLanguage(i18next);
+    const [filters, setFilters] = useState<Partial<GET_TREESVariables>>({});
 
-class Trees extends React.Component<ITreesProps, ITreesState> {
-    constructor(props: ITreesProps) {
-        super(props);
-
-        this.state = {
-            filters: {}
-        };
-    }
-
-    public render() {
-        const {t, i18n: i18next} = this.props;
-        const {filters} = this.state;
-        const lang = getSysTranslationQueryLanguage(i18next);
-        return (
-            <React.Fragment>
-                <Grid>
-                    <Grid.Column textAlign="left" floated="left" width={8} verticalAlign="middle">
-                        <Header size="large">
-                            <Icon name="share alternate" />
-                            {t('trees.title')}
-                        </Header>
-                    </Grid.Column>
-                    <Grid.Column floated="right" width={3} textAlign="right" verticalAlign="middle">
-                        <Button icon labelPosition="left" size="medium" as={Link} to={'/trees/edit/'}>
-                            <Icon name="plus" />
-                            {t('trees.new')}
-                        </Button>
-                    </Grid.Column>
-                </Grid>
-                <TreesQuery query={getTreesQuery} variables={{...addWildcardToFilters(filters), lang}}>
-                    {({loading, error, data}) => {
-                        if (typeof error !== 'undefined') {
-                            return <p>Error: {error.message}</p>;
-                        }
-
-                        const onRowClick = tree => this.props.history.push('/trees/edit/' + tree.id);
-
-                        return (
-                            <TreesList
-                                trees={data ? data.trees : null}
-                                onRowClick={onRowClick}
-                                onFiltersUpdate={this._onFiltersUpdate}
-                                filters={filters}
-                                loading={loading}
-                            />
-                        );
-                    }}
-                </TreesQuery>
-            </React.Fragment>
-        );
-    }
-
-    private _onFiltersUpdate = (filterElem: any) => {
+    const _onFiltersUpdate = (filterElem: any) => {
         const newElemState =
             filterElem.type === 'checkbox'
                 ? filterElem.indeterminate
@@ -76,13 +24,49 @@ class Trees extends React.Component<ITreesProps, ITreesState> {
                     : filterElem.checked
                 : filterElem.value;
 
-        this.setState({
-            filters: {
-                ...this.state.filters,
-                [filterElem.name]: newElemState
-            }
+        setFilters({
+            ...filters,
+            [filterElem.name]: newElemState
         });
-    }
+    };
+
+    return (
+        <React.Fragment>
+            <Grid>
+                <Grid.Column textAlign="left" floated="left" width={8} verticalAlign="middle">
+                    <Header size="large">
+                        <Icon name="share alternate" />
+                        {t('trees.title')}
+                    </Header>
+                </Grid.Column>
+                <Grid.Column floated="right" width={3} textAlign="right" verticalAlign="middle">
+                    <Button icon labelPosition="left" size="medium" as={Link} to={'/trees/edit/'}>
+                        <Icon name="plus" />
+                        {t('trees.new')}
+                    </Button>
+                </Grid.Column>
+            </Grid>
+            <TreesQuery query={getTreesQuery} variables={{...addWildcardToFilters(filters), lang}}>
+                {({loading, error, data}) => {
+                    if (typeof error !== 'undefined') {
+                        return <p>Error: {error.message}</p>;
+                    }
+
+                    const onRowClick = tree => history.push('/trees/edit/' + tree.id);
+
+                    return (
+                        <TreesList
+                            trees={data ? data.trees : null}
+                            onRowClick={onRowClick}
+                            onFiltersUpdate={_onFiltersUpdate}
+                            filters={filters}
+                            loading={loading}
+                        />
+                    );
+                }}
+            </TreesQuery>
+        </React.Fragment>
+    );
 }
 
 export default withNamespaces()(Trees);
