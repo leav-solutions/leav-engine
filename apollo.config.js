af634777b8cc48fd45224a31294c2695d9b9567f
Teddy <t.paul@cacom.fr>
2019-06-20 17:10:37 +0200

    Move token for Apollo tools in ignored file

--- apollo.config.js
+++ apollo.config.js
@@ -1,11 +1,13 @@
+const token = require('./apollo.token');
+
+const apiUrl = 'http://localhost:4001';
 module.exports = {
     client: {
         service: {
             name: 'default',
-            url: 'http://localhost:4001/graphql',
+            url: `${apiUrl}/graphql`,
             headers: {
-                authorization:
-                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIyMjQ3NjUzNTEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1MjgxOTE4MDgsImV4cCI6NDY1MjM5NDIwOH0.XH6Xu108WdLMpQdOa-KnaZU541QP4jWdCkF1O0yyefg'
+                authorization: token
             }
         }
     }
