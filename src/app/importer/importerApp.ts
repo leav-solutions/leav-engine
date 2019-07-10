cce106a5229a4dfa9d4c06dd23c7c5c880468442
Teddy <t.paul@cacom.fr>
2019-07-10 15:05:30 +0200

    Add importerApp

---
+++ src/app/importer/importerApp.ts
@@ -0,0 +1,254 @@
+import {CollectionType} from 'arangojs/lib/cjs/collection';
+import {AwilixContainer} from 'awilix';
+import {IAttributeDomain} from 'domain/attribute/attributeDomain';
+import {ILibraryDomain} from 'domain/library/libraryDomain';
+import {IRecordDomain} from 'domain/record/recordDomain';
+import {ITreeDomain} from 'domain/tree/treeDomain';
+import {IValueDomain} from 'domain/value/valueDomain';
+import {promises as fs} from 'fs';
+import {IDbUtils} from 'infra/db/dbUtils';
+import {now} from 'moment';
+import {isArray} from 'util';
+import {IQueryInfos} from '_types/queryInfos';
+import {collectionTypes, IDbService} from '../../infra/db/dbService';
+
+export interface IImporterApp {
+    import(filepath: string, clear: boolean): Promise<void>;
+}
+
+export default function(
+    libraryDomain: ILibraryDomain,
+    treeDomain: ITreeDomain,
+    attributeDomain: IAttributeDomain,
+    recordDomain: IRecordDomain,
+    valueDomain: IValueDomain,
+    depsManager: AwilixContainer,
+    dbUtils: IDbUtils,
+    dbService: IDbService
+): IImporterApp {
+    const _clearDb = async () => {
+        // Drop all collections
+        const cols = await dbService.db.listCollections();
+        for (const col of cols) {
+            const colType =
+                col.type === CollectionType.DOCUMENT_COLLECTION ? collectionTypes.DOCUMENT : collectionTypes.EDGE;
+            await dbService.dropCollection(col.name, colType);
+        }
+    };
+
+    const _processLibraries = (libraries, infos: IQueryInfos): Promise<any[]> => {
+        return Promise.all(
+            libraries.map(lib => {
+                const libToSave = {...lib};
+
+                // Remove fields which require attribute creation. We'll take care of it later
+                delete libToSave.attributes;
+                delete libToSave.recordIdentityConf;
+
+                return libraryDomain.saveLibrary(libToSave, infos);
+            })
+        );
+    };
+
+    const _processTrees = (trees, infos): Promise<any[]> => {
+        return Promise.all(
+            trees.map(tree => {
+                const treeToSave = {...tree};
+                delete treeToSave.content;
+                return treeDomain.saveTree(tree, infos);
+            })
+        );
+    };
+
+    const _processAttributes = (attributes, infos): Promise<any[]> => {
+        return Promise.all(
+            attributes.map(attr => {
+                return attributeDomain.saveAttribute(attr, infos);
+            })
+        );
+    };
+
+    const _processLibrariesAttributes = (libraries, attributesById, infos: IQueryInfos): Promise<any[]> => {
+        return Promise.all(
+            libraries.map(lib => {
+                const libAttributes = lib.attributes.map(a => attributesById[a]);
+                const libToSave = {
+                    id: lib.id,
+                    attributes: libAttributes,
+                    recordIdentityConf: lib.recordIdentityConf
+                };
+                return libraryDomain.saveLibrary(libToSave, infos);
+            })
+        );
+    };
+
+    const _processRecords = async (records): Promise<{[key: number]: number}> => {
+        const recordsMapping = {};
+        const recordsToImport = [];
+        for (const libName of Object.keys(records)) {
+            if (isArray(records[libName])) {
+                for (const record of records[libName]) {
+                    recordsToImport.push({library: libName, key: record.key});
+                }
+            } else if (records[libName]) {
+                for (let j = 0; j < records[libName]; j++) {
+                    recordsToImport.push({library: libName, key: j});
+                }
+            }
+        }
+
+        await Promise.all(
+            recordsToImport.map(async r => {
+                const createdRecord = await recordDomain.createRecord(r.library);
+                recordsMapping[r.library] = recordsMapping[r.library] || {};
+
+                recordsMapping[r.library][r.key] = createdRecord.id;
+            })
+        );
+
+        return recordsMapping;
+    };
+
+    const _processTreeContent = (trees, recordsMapping): Promise<any[]> => {
+        return Promise.all(trees.map(tree => _saveTreeContent(tree.id, tree.content, null, recordsMapping)));
+    };
+
+    const _processValues = (values, attributesById, recordsMapping, infos): Promise<any[]> => {
+        const valuesToImport = [];
+        for (const libName of Object.keys(values)) {
+            for (const attrName of Object.keys(values[libName])) {
+                const valSettings = values[libName][attrName];
+                if (isArray(valSettings)) {
+                    for (const val of valSettings) {
+                        valuesToImport.push({
+                            library: libName,
+                            attribute: attrName,
+                            recordKey: val.recordKey,
+                            value: val.value
+                        });
+                    }
+                } else if (valSettings.generate) {
+                    for (const key of Object.keys(recordsMapping[libName])) {
+                        const value = !!valSettings.values
+                            ? valSettings.values[Number(key) % valSettings.values.length]
+                            : attrName + key;
+
+                        valuesToImport.push({
+                            library: libName,
+                            attribute: attrName,
+                            recordKey: key,
+                            value,
+                            version: !!valSettings.versions
+                                ? valSettings.versions[Number(key) % valSettings.versions.length]
+                                : null
+                        });
+                    }
+                }
+            }
+        }
+
+        return Promise.all(
+            valuesToImport.map(async v => {
+                const attrType = attributesById[v.attribute].type;
+                if (attrType === 'tree') {
+                    const [library, key] = v.value.split('/');
+                    v.value = `${library}/${recordsMapping[library][key]}`;
+                } else if (attrType === 'advanced_link' || attrType === 'simple_link') {
+                    v.value = recordsMapping[attributesById[v.attribute].linked_library][v.value];
+                }
+
+                let version = null;
+                if (v.version) {
+                    version = {};
+                    for (const treeName of Object.keys(v.version)) {
+                        const [lib, key] = v.version[treeName].split('/');
+                        version[treeName] = {library: lib, id: recordsMapping[lib][key]};
+                    }
+                }
+
+                const createdValue = await valueDomain.saveValue(
+                    v.library,
+                    recordsMapping[v.library][v.recordKey],
+                    v.attribute,
+                    {value: v.value, version},
+                    infos
+                );
+            })
+        );
+    };
+
+    const _saveTreeContent = async (treeId, treeNodes, parent, recordsMapping) => {
+        for (const node of treeNodes) {
+            const treeElem = {id: recordsMapping[node.library][node.recordKey], library: node.library};
+            await treeDomain.addElement(treeId, treeElem, parent);
+
+            if (node.children.length) {
+                await _saveTreeContent(treeId, node.children, treeElem, recordsMapping);
+            }
+        }
+    };
+
+    return {
+        async import(filepath: string, clear: boolean): Promise<void> {
+            const startTime = now();
+            const infos: IQueryInfos = {
+                userId: 1
+            };
+
+            if (clear) {
+                await _clearDb();
+            }
+
+            // Run DB migration before doing anything
+            await dbUtils.migrate(depsManager);
+
+            const fileContent = await fs.readFile(filepath, {encoding: 'utf8'});
+            const data = JSON.parse(fileContent);
+
+            // Create libraries
+            console.info('Processing libraries...');
+            const libs = await _processLibraries(data.libraries, infos);
+            console.info(`Processed ${libs.length} libraries`);
+
+            // Create trees
+            console.info('Processing trees...');
+            const trees = await _processTrees(data.trees, infos);
+            console.info(`Processed ${trees.length} trees`);
+
+            // Create attributes
+            console.info('Processing attributes...');
+            const attributes = await _processAttributes(data.attributes, infos);
+
+            const attrsById = attributes.reduce((attrs, a: any) => {
+                attrs[a.id] = a;
+                return attrs;
+            }, {});
+            console.info(`Processed ${attributes.length} attributes`);
+
+            console.info('Processing libraries attributes...');
+            await _processLibrariesAttributes(data.libraries, attrsById, infos);
+
+            // Create records
+            console.info('Processing records...');
+            const recordsMapping = await _processRecords(data.records);
+            const recordsCount = Object.keys(recordsMapping).reduce((count, libName) => {
+                count = count + Object.keys(recordsMapping[libName]).length;
+                return count;
+            }, 0);
+            console.info(`Processed ${recordsCount} records`);
+
+            // Create tree content
+            console.info('Processing trees content...');
+            await _processTreeContent(data.trees, recordsMapping);
+
+            // Save values
+            console.info('Processing values...');
+            const values = await _processValues(data.values, attrsById, recordsMapping, infos);
+            console.info(`Processed ${values.length} values`);
+
+            const endTime = now();
+
+            console.info(`Import done in ${(endTime - startTime) / 1000}s`);
+        }
+    };
+}
