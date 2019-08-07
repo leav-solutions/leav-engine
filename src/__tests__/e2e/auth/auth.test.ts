4e7d2b88882cbc4b0ebdd35806a8ae390c1ba13b
Teddy <t.paul@cacom.fr>
2019-08-07 14:22:09 +0200

    Add e2e test on authentication

--- src/__tests__/e2e/auth/auth.test.ts
+++ src/__tests__/e2e/auth/auth.test.ts
@@ -1,6 +1,21 @@
+import axios from 'axios';
+import {config} from '../../../config';
 import {makeGraphQlCall} from '../e2eUtils';
 
 describe('Auth', () => {
+    test('Authenticate', async () => {
+        const conf: any = await config;
+        const url = `http://${conf.server.host}:${conf.server.port}/auth/authenticate`;
+
+        const res = await axios.post(url, {
+            login: 'admin',
+            password: 'admin'
+        });
+
+        expect(res.status).toBe(200);
+        expect(res.data.token).toBeTruthy();
+    });
+
     test('Me', async () => {
         const res = await makeGraphQlCall(`{
             me {
