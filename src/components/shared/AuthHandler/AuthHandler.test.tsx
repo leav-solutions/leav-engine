701422f787582b82abb1c184101432abf588d663
Miguel Pelleterat <m.pelleterat@cacom.fr>
2019-08-08 17:36:27 +0200

    added tests for AuthHandler component

--- src/components/shared/AuthHandler/AuthHandler.test.tsx
+++ src/components/shared/AuthHandler/AuthHandler.test.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import {render} from 'enzyme';
+import {render, mount} from 'enzyme';
 import AuthHandler from './AuthHandler';
 
-describe('AuthHandler', () => {
-    test('Snapshot test', async () => {
-        const comp = render(<AuthHandler url={process.env.REACT_APP_AUTH_URL || ''} />);
+import Login from '../Login';
+import App from '../../app/App';
 
-        expect(comp).toMatchSnapshot();
-    });
+const globalAny: any = global;
+
+const localStorageMock = {
+    setItem: jest.fn()
+};
+
+globalAny.localStorage = localStorageMock;
+
+test('Snapshot test', async () => {
+    const comp = render(<AuthHandler url={process.env.REACT_APP_AUTH_URL || ''} />);
+    expect(comp).toMatchSnapshot();
+});
+
+test('renders login if no token in local storage, app otherwise', async () => {
+    globalAny.localStorage.clear();
+    const wrapper = mount(<AuthHandler url={process.env.REACT_APP_AUTH_URL || ''} />);
+    const loginComp = render(<Login />);
+    const app = render(<App />);
+    expect(wrapper.childAt(0).contains(loginComp));
+    globalAny.localStorage.setItem('accessToken', '1');
+    wrapper.update();
+    expect(wrapper.childAt(0).contains(app));
 });
