3b5d346170510572b1b9ad9e2886efdbba9563ba
Teddy <t.paul@cacom.fr>
2019-08-29 17:34:16 +0200

    Convert EditLibrary to Apollo hooks

--- src/components/libraries/EditLibrary/EditLibrary.tsx
+++ src/components/libraries/EditLibrary/EditLibrary.tsx
@@ -1,12 +1,13 @@
+import {useMutation, useQuery} from '@apollo/react-hooks';
 import {History} from 'history';
 import {i18n} from 'i18next';
 import React from 'react';
 import {withNamespaces, WithNamespaces} from 'react-i18next';
 import useLang from '../../../hooks/useLang';
 import useUserData from '../../../hooks/useUserData';
-import {getLibsQuery, LibrariesQuery} from '../../../queries/libraries/getLibrariesQuery';
-import {SaveLibMutation, saveLibQuery} from '../../../queries/libraries/saveLibMutation';
-import {GET_LIBRARIES_libraries_list} from '../../../_gqlTypes/GET_LIBRARIES';
+import {getLibsQuery} from '../../../queries/libraries/getLibrariesQuery';
+import {saveLibQuery} from '../../../queries/libraries/saveLibMutation';
+import {GET_LIBRARIES, GET_LIBRARIESVariables, GET_LIBRARIES_libraries_list} from '../../../_gqlTypes/GET_LIBRARIES';
 import {PermissionsActions} from '../../../_gqlTypes/globalTypes';
 import Loading from '../../shared/Loading';
 import EditLibraryForm from '../EditLibraryForm';
@@ -23,88 +24,83 @@ function EditLibrary({match, history}: IEditLibraryProps) {
     const userData = useUserData();
     const readOnly = !userData.permissions[PermissionsActions.admin_edit_library];
 
+    const {loading, error, data} = useQuery<GET_LIBRARIES, GET_LIBRARIESVariables>(getLibsQuery, {
+        variables: {id: libraryId, lang}
+    });
+    const [saveLibrary] = useMutation(saveLibQuery);
+
     /**
      * Retrieve EditLibraryForm, wrapped by mutation component
      * @param libToEdit
      * @param history
      */
-    const _getEditLibraryForm = (libToEdit: GET_LIBRARIES_libraries_list | null) => (
-        <SaveLibMutation mutation={saveLibQuery}>
-            {saveLibrary => {
-                const onFormSubmit = async libData => {
-                    await saveLibrary({
-                        variables: {
-                            libData: {
-                                id: libData.id,
-                                label: {
-                                    fr: libData.label.fr,
-                                    en: libData.label.en
-                                },
-                                recordIdentityConf:
-                                    libData.recordIdentityConf !== null
-                                        ? {
-                                              label: libData.recordIdentityConf.label,
-                                              preview: libData.recordIdentityConf.preview,
-                                              color: libData.recordIdentityConf.color
-                                          }
-                                        : null
-                            }
+    const _getEditLibraryForm = (libToEdit: GET_LIBRARIES_libraries_list | null) => {
+        const onFormSubmit = async libData => {
+            await saveLibrary({
+                variables: {
+                    libData: {
+                        id: libData.id,
+                        label: {
+                            fr: libData.label.fr,
+                            en: libData.label.en
                         },
-                        refetchQueries: [{query: getLibsQuery, variables: {id: libData.id}}, {query: getLibsQuery}]
-                    });
+                        recordIdentityConf:
+                            libData.recordIdentityConf !== null
+                                ? {
+                                      label: libData.recordIdentityConf.label,
+                                      preview: libData.recordIdentityConf.preview,
+                                      color: libData.recordIdentityConf.color
+                                  }
+                                : null
+                    }
+                },
+                refetchQueries: [{query: getLibsQuery, variables: {id: libData.id}}, {query: getLibsQuery}]
+            });
 
-                    history.replace({pathname: '/libraries/edit/' + libData.id});
-                };
+            history.replace({pathname: '/libraries/edit/' + libData.id});
+        };
 
-                const onPermissionsFormSubmit = async libData => {
-                    await saveLibrary({
-                        variables: {
-                            libData: {
-                                id: libData.id,
-                                permissions_conf: {
-                                    permissionTreeAttributes: libData.permissions_conf.permissionTreeAttributes,
-                                    relation: libData.permissions_conf.relation
-                                }
-                            }
-                        },
-                        refetchQueries: [{query: getLibsQuery, variables: {id: libData.id}}]
-                    });
-                };
-                return (
-                    <EditLibraryForm
-                        library={libToEdit}
-                        onSubmit={onFormSubmit}
-                        onPermsSettingsSubmit={onPermissionsFormSubmit}
-                        readOnly={readOnly}
-                    />
-                );
-            }}
-        </SaveLibMutation>
-    );
+        const onPermissionsFormSubmit = async libData => {
+            await saveLibrary({
+                variables: {
+                    libData: {
+                        id: libData.id,
+                        permissions_conf: {
+                            permissionTreeAttributes: libData.permissions_conf.permissionTreeAttributes,
+                            relation: libData.permissions_conf.relation
+                        }
+                    }
+                },
+                refetchQueries: [{query: getLibsQuery, variables: {id: libData.id}}]
+            });
+        };
+        return (
+            <EditLibraryForm
+                library={libToEdit}
+                onSubmit={onFormSubmit}
+                onPermsSettingsSubmit={onPermissionsFormSubmit}
+                readOnly={readOnly}
+            />
+        );
+    };
 
-    return libraryId ? (
-        <LibrariesQuery query={getLibsQuery} variables={{id: libraryId, lang}}>
-            {({loading, error, data}) => {
-                if (loading) {
-                    return <Loading />;
-                }
-                if (typeof error !== 'undefined') {
-                    return <p>Error: {error.message}</p>;
-                }
+    if (!libraryId) {
+        return _getEditLibraryForm(null);
+    }
 
-                if (libraryId && !data) {
-                    return 'Unknown library';
-                }
+    if (loading) {
+        return <Loading />;
+    }
 
-                const libToEdit =
-                    data!.libraries !== null && data!.libraries!.list.length ? data!.libraries!.list[0] : null;
+    if (typeof error !== 'undefined') {
+        return <p>Error: {error.message}</p>;
+    }
 
-                return _getEditLibraryForm(libToEdit);
-            }}
-        </LibrariesQuery>
-    ) : (
-        _getEditLibraryForm(null)
-    );
+    if (!data || !data.libraries || !data.libraries.list.length) {
+        return <p>Unknown library</p>;
+    }
+
+    return _getEditLibraryForm(data.libraries.list[0]);
 }
 
 export default withNamespaces()(EditLibrary);
