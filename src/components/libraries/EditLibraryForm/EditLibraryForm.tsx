a01475cd5fd61284ae387aeb9622b4dfe7fa7698
Teddy <t.paul@cacom.fr>
2019-09-02 16:56:13 +0200

    Convert EditLibraryForm to func component

--- src/components/libraries/EditLibraryForm/EditLibraryForm.tsx
+++ src/components/libraries/EditLibraryForm/EditLibraryForm.tsx
@@ -13,76 +13,63 @@ interface IEditLibraryFormProps extends WithNamespaces {
     readOnly: boolean;
 }
 
-class EditLibraryForm extends React.Component<IEditLibraryFormProps, any> {
-    constructor(props: IEditLibraryFormProps) {
-        super(props);
+function EditLibraryForm({library, onSubmit, onPermsSettingsSubmit, readOnly, t}: IEditLibraryFormProps) {
+    const label =
+        library === null
+            ? t('libraries.new')
+            : library.label !== null
+            ? library.label.fr || library.label.en || library.id
+            : library.id;
 
-        this.state = {
-            ...this.props.library
-        };
-    }
-
-    public render() {
-        const {library, onSubmit, onPermsSettingsSubmit, readOnly} = this.props;
-        const {t} = this.props;
-
-        const label =
-            library === null
-                ? t('libraries.new')
-                : library.label !== null
-                ? library.label.fr || library.label.en || library.id
-                : library.id;
-
-        let panes = [
-            {
-                key: 'infos',
-                menuItem: t('libraries.informations'),
-                render: () => (
-                    <Tab.Pane key="infos" className="grow">
-                        <EditLibraryInfosForm library={library} onSubmit={onSubmit} readonly={readOnly} />
-                    </Tab.Pane>
-                )
-            }
-        ];
-
-        if (library !== null) {
-            panes = panes.concat([
-                {
-                    key: 'permissions',
-                    menuItem: t('libraries.permissions'),
-                    render: () => {
-                        return (
-                            <Tab.Pane key="permissions" className="grow flex-col height100">
-                                <EditLibraryPermissions
-                                    library={library}
-                                    onSubmitSettings={onPermsSettingsSubmit}
-                                    readOnly={readOnly}
-                                />
-                            </Tab.Pane>
-                        );
-                    }
-                },
-                {
-                    key: 'attributes',
-                    menuItem: t('libraries.attributes'),
-                    render: () => {
-                        return (
-                            <Tab.Pane key="attributes" className="grow">
-                                <EditLibraryAttributes library={library} readOnly={readOnly} />
-                            </Tab.Pane>
-                        );
-                    }
-                }
-            ]);
+    let panes = [
+        {
+            key: 'infos',
+            menuItem: t('libraries.informations'),
+            render: () => (
+                <Tab.Pane key="infos" className="grow">
+                    <EditLibraryInfosForm library={library} onSubmit={onSubmit} readonly={readOnly} />
+                </Tab.Pane>
+            )
         }
+    ];
 
-        return (
-            <>
-                <Header className="no-grow">{label}</Header>
-                <Tab menu={{secondary: true, pointing: true}} panes={panes} className="grow flex-col height100" />
-            </>
-        );
+    if (library !== null) {
+        panes = panes.concat([
+            {
+                key: 'permissions',
+                menuItem: t('libraries.permissions'),
+                render: () => {
+                    return (
+                        <Tab.Pane key="permissions" className="grow flex-col height100">
+                            <EditLibraryPermissions
+                                library={library}
+                                onSubmitSettings={onPermsSettingsSubmit}
+                                readOnly={readOnly}
+                            />
+                        </Tab.Pane>
+                    );
+                }
+            },
+            {
+                key: 'attributes',
+                menuItem: t('libraries.attributes'),
+                render: () => {
+                    return (
+                        <Tab.Pane key="attributes" className="grow">
+                            <EditLibraryAttributes library={library} readOnly={readOnly} />
+                        </Tab.Pane>
+                    );
+                }
+            }
+        ]);
     }
+
+    return (
+        <>
+            <Header className="no-grow">{label}</Header>
+            <Tab menu={{secondary: true, pointing: true}} panes={panes} className="grow flex-col height100" />
+        </>
+    );
 }
 
 export default withNamespaces()(EditLibraryForm);
