31059d68b9e6bc34786366ed68e20fd6d765bb47
Teddy <t.paul@cacom.fr>
2019-10-10 16:11:52 +0200

    Convert TreeStructure to functional component

--- src/components/trees/TreeStructure/TreeStructure.tsx
+++ src/components/trees/TreeStructure/TreeStructure.tsx
@@ -1,6 +1,6 @@
 import {ApolloConsumer} from '@apollo/react-common';
 import {ApolloClient} from 'apollo-client';
-import React from 'react';
+import React, {useState} from 'react';
 import {withNamespaces, WithNamespaces} from 'react-i18next';
 import {
     changeNodeAtPath,
@@ -34,10 +34,26 @@ interface ITreeStructureProps extends WithNamespaces {
     withFakeRoot?: boolean;
 }
 
-interface ITreeStructureState {
-    treeData: TreeItem[];
-    loaded: boolean;
-}
+const _convertTreeRecord = (records: TREE_CONTENT_treeContent[]): TreeItem[] => {
+    return records.map(
+        (r: TREE_CONTENT_treeContent): TreeItem => {
+            const nodeTitle =
+                r.record.id !== 'root' ? (
+                    <RecordCard record={r.record.whoAmI} style={{height: '100%'}} />
+                ) : (
+                    <RootElem>{r.record.whoAmI.label}</RootElem>
+                );
+
+            return {
+                ...r.record,
+                title: nodeTitle,
+                // subtitle: r.record.library.id,
+                children: r.children ? _convertTreeRecord(r.children as TREE_CONTENT_treeContent[]) : [],
+                expanded: false
+            };
+        }
+    );
+};
 
 /* tslint:disable-next-line:variable-name */
 const RootElem = styled.div`
@@ -48,140 +64,18 @@ const RootElem = styled.div`
     justify-content: center;
 `;
 
-class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureState> {
-    constructor(props: ITreeStructureProps) {
-        super(props);
-
-        const fakeRootData = [
-            {
-                record: {
-                    id: 'root',
-                    library: {id: 'root', label: null},
-                    whoAmI: {
-                        id: 'root',
-                        label: props.t('permissions.any_record'),
-                        color: 'transparent',
-                        library: {id: 'root', label: null},
-                        preview: null
-                    }
-                },
-                children: [],
-                order: 0
-            }
-        ];
-        const initTreeData = props.withFakeRoot ? this._convertTreeRecord(fakeRootData) : [];
-
-        this.state = {
-            treeData: initTreeData,
-            loaded: false
-        };
-    }
-
-    public render(): JSX.Element {
-        const {treeId, readOnly, onClickNode, selection, withFakeRoot} = this.props;
-        const {treeData, loaded} = this.state;
-
-        const _handleClickNode = treeItem => {
-            // Add all parents details on selected node
-            const hydratedPath = treeItem.path.map(k => {
-                // All we have is the parent key, so we need to find matching node in tree data
-                const findRes = find({
-                    treeData,
-                    getNodeKey: getTreeNodeKey,
-                    searchQuery: k,
-                    searchMethod: d => {
-                        const [lib, id] = k.split('/');
-                        return d.node.id === id && d.node.library.id === lib;
-                    }
-                });
-
-                return findRes.matches.length ? findRes.matches[0].node : null;
-            });
-
-            return onClickNode
-                ? onClickNode({...treeItem, node: {...treeItem.node, parents: hydratedPath}})
-                : undefined;
-        };
-
-        return (
-            <ApolloConsumer>
-                {client => {
-                    // Init tree with root children
-                    if (!loaded) {
-                        const path = withFakeRoot ? ['root/root'] : undefined;
-                        this._loadChildren(client, treeId, null, path);
-                    }
-
-                    const onVisibilityToggle = ({expanded, node, path}) => {
-                        if (node.expanded || node.loaded) {
-                            return;
-                        }
-
-                        return this._loadChildren(
-                            client,
-                            treeId,
-                            {id: node.id, library: node.library.id},
-                            path,
-                            expanded
-                        );
-                    };
-
-                    const onMoveNode = moveData => this._saveMove(client, moveData);
-                    const onDeleteNode = nodeData => this._deleteNode(client, nodeData);
-
-                    return (
-                        <TreeStructureView
-                            treeData={treeData}
-                            readOnly={readOnly || false}
-                            onTreeChange={this._onTreeChange}
-                            onVisibilityToggle={onVisibilityToggle}
-                            onMoveNode={onMoveNode}
-                            onDeleteNode={onDeleteNode}
-                            onClickNode={_handleClickNode}
-                            selection={selection}
-                        />
-                    );
-                }}
-            </ApolloConsumer>
-        );
-    }
-
-    private _convertTreeRecord = (records: TREE_CONTENT_treeContent[]): TreeItem[] => {
-        return records.map(
-            (r: TREE_CONTENT_treeContent): TreeItem => {
-                const nodeTitle =
-                    r.record.id !== 'root' ? (
-                        <RecordCard record={r.record.whoAmI} style={{height: '100%'}} />
-                    ) : (
-                        <RootElem>{r.record.whoAmI.label}</RootElem>
-                    );
-
-                return {
-                    ...r.record,
-                    title: nodeTitle,
-                    // subtitle: r.record.library.id,
-                    children: r.children ? this._convertTreeRecord(r.children as TREE_CONTENT_treeContent[]) : [],
-                    expanded: false
-                };
-            }
-        );
-    }
-
-    private _onTreeChange = treeData => this.setState({treeData});
-
+function TreeStructure({treeId, readOnly, onClickNode, selection, withFakeRoot, t}: ITreeStructureProps) {
     /**
      * Retrieve node children.
      * We retrieve current node children + first level children to know if we must display "expand" button
      *
      * @param client
-     * @param treeId
      * @param parent	No parent = root
      * @param path      No path = root
      * @param expand    Should expand node?
      */
-    private _loadChildren = async (
+    const _loadChildren = async (
         client: ApolloClient<any>,
-        treeId: string,
         parent?: TreeElementInput | null,
         path?: Array<string | number>,
         expand: boolean = true
@@ -189,10 +83,10 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
         // Show loading spinner on node
         const withPath = !!path;
         if (withPath && parent) {
-            const node = getNodeAtPath({treeData: this.state.treeData, path: path!, getNodeKey: getTreeNodeKey});
+            const node = getNodeAtPath({treeData, path: path!, getNodeKey: getTreeNodeKey});
             if (node !== null) {
-                this.setState({
-                    treeData: this._mergeNode(
+                setTreeData(
+                    _mergeNode(
                         {
                             ...node.node,
                             loading: true,
@@ -201,64 +95,62 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
                         },
                         path!
                     )
-                });
+                );
             }
         }
 
         // Retrieve data
-        const variables: TREE_CONTENTVariables = {
-            treeId,
-            startAt: parent || null
-        };
-
         const data = await client.query<TREE_CONTENT, TREE_CONTENTVariables>({
             query: getTreeContentQuery,
-            variables
+            variables: {
+                treeId,
+                startAt: parent || null
+            }
         });
 
-        const convertedRecords = data.data.treeContent ? this._convertTreeRecord(data.data.treeContent) : [];
+        const convertedRecords = data.data.treeContent ? _convertTreeRecord(data.data.treeContent) : [];
 
         // Update tree node with fetched data
         // We must get fresh node data from in case its state has changed during loading (expand/collapse...)
-        const nodeToUpdate = getNodeAtPath({treeData: this.state.treeData, path: path!, getNodeKey: getTreeNodeKey});
-        const newState: Partial<ITreeStructureState> = {
-            loaded: true
-        };
+        const nodeToUpdate = getNodeAtPath({treeData, path: path!, getNodeKey: getTreeNodeKey});
 
+        let newTreeData;
         if (!withPath) {
-            newState.treeData = convertedRecords;
+            newTreeData = convertedRecords;
         } else if (nodeToUpdate !== null) {
-            newState.treeData = this._mergeNode(
+            newTreeData = _mergeNode(
                 {
                     ...nodeToUpdate.node,
                     loading: false,
                     loaded: true,
-                    children: convertedRecords
+                    children: convertedRecords,
+                    expanded: expand
                 },
                 path!
             );
         }
 
-        this.setState(newState as ITreeStructureState);
-    }
+        setLoaded(true);
+        setTreeData(newTreeData);
+    };
 
-    private _saveMove = async (
+    const _saveMove = async (
         client: ApolloClient<any>,
         moveData: NodeData & FullTree & OnMovePreviousAndNextLocation
     ) => {
-        const element: TreeElementInput = this._nodeToTreeElement(moveData.node);
+        const element: TreeElementInput = _nodeToTreeElement(moveData.node);
 
         // Parent node in tree
         const parentNode = moveData.nextParentNode;
 
         // Parent element to save
-        const parentTo: TreeElementInput | null = parentNode !== null ? this._nodeToTreeElement(parentNode) : null;
+        const parentTo: TreeElementInput | null = parentNode !== null ? _nodeToTreeElement(parentNode) : null;
 
         // Get new element position
         let position = moveData.treeIndex;
         if (parentNode !== null) {
             const parentNodeAtPath = getNodeAtPath({
-                treeData: this.state.treeData,
+                treeData,
                 path: moveData.nextPath.slice(0, -1),
                 getNodeKey: getTreeNodeKey
             });
@@ -269,7 +161,7 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
         await client.mutate<MOVE_TREE_ELEMENT, MOVE_TREE_ELEMENTVariables>({
             mutation: moveTreeElementQuery,
             variables: {
-                treeId: this.props.treeId,
+                treeId,
                 element,
                 parentTo,
                 order: position
@@ -277,16 +169,16 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
         });
 
         // Update positions for all siblings in destination
-        const siblings = parentNode !== null ? parentNode.children : this.state.treeData;
+        const siblings = parentNode !== null ? parentNode.children : treeData;
         if (siblings && siblings.length) {
             await Promise.all(
                 siblings.map((s, i) => {
-                    const siblingElement = this._nodeToTreeElement(s);
+                    const siblingElement = _nodeToTreeElement(s);
                     return getTreeNodeKey({node: s}) !== getTreeNodeKey(moveData) // Skip moved element
                         ? client.mutate<MOVE_TREE_ELEMENT, MOVE_TREE_ELEMENTVariables>({
                               mutation: moveTreeElementQuery,
                               variables: {
-                                  treeId: this.props.treeId,
+                                  treeId,
                                   element: siblingElement,
                                   parentTo,
                                   order: i
@@ -296,13 +188,13 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
                 })
             );
         }
-    }
+    };
 
-    private _deleteNode = async (client: ApolloClient<any>, node: ExtendedNodeData) => {
-        const element: TreeElementInput = this._nodeToTreeElement(node.node);
+    const _deleteNode = async (client: ApolloClient<any>, node: ExtendedNodeData) => {
+        const element: TreeElementInput = _nodeToTreeElement(node.node);
 
         const variables: DELETE_TREE_ELEMENTVariables = {
-            treeId: this.props.treeId,
+            treeId,
             element
         };
 
@@ -312,23 +204,102 @@ class TreeStructure extends React.Component<ITreeStructureProps, ITreeStructureS
         });
 
         const updatedTree = removeNodeAtPath({
-            treeData: this.state.treeData,
+            treeData,
             path: node.path,
             getNodeKey: getTreeNodeKey
         });
-        this.setState({treeData: updatedTree});
-    }
+        setTreeData(updatedTree);
+    };
 
-    private _mergeNode(nodeData: TreeItem, path: Array<string | number>) {
-        return changeNodeAtPath({treeData: this.state.treeData, path, newNode: nodeData, getNodeKey: getTreeNodeKey});
-    }
+    const _mergeNode = (nodeData: TreeItem, path: Array<string | number>) => {
+        return changeNodeAtPath({treeData, path, newNode: nodeData, getNodeKey: getTreeNodeKey});
+    };
 
-    private _nodeToTreeElement(node: TreeItem): TreeElementInput {
+    const _nodeToTreeElement = (node: TreeItem): TreeElementInput => {
         return {
             id: node.id,
             library: node.library.id
         };
-    }
+    };
+
+    const fakeRootData = [
+        {
+            record: {
+                id: 'root',
+                library: {id: 'root', label: null},
+                whoAmI: {
+                    id: 'root',
+                    label: t('permissions.any_record'),
+                    color: 'transparent',
+                    library: {id: 'root', label: null},
+                    preview: null
+                }
+            },
+            children: [],
+            order: 0
+        }
+    ];
+
+    const initTreeData = withFakeRoot ? _convertTreeRecord(fakeRootData) : [];
+
+    const [treeData, setTreeData] = useState<TreeItem[]>(initTreeData);
+    const [loaded, setLoaded] = useState<boolean>(false);
+
+    const _handleClickNode = treeItem => {
+        // Add all parents details on selected node
+        const hydratedPath = treeItem.path.map(k => {
+            // All we have is the parent key, so we need to find matching node in tree data
+            const findRes = find({
+                treeData,
+                getNodeKey: getTreeNodeKey,
+                searchQuery: k,
+                searchMethod: d => {
+                    const [lib, id] = k.split('/');
+                    return d.node.id === id && d.node.library.id === lib;
+                }
+            });
+
+            return findRes.matches.length ? findRes.matches[0].node : null;
+        });
+
+        return onClickNode ? onClickNode({...treeItem, node: {...treeItem.node, parents: hydratedPath}}) : undefined;
+    };
+
+    return (
+        <ApolloConsumer>
+            {client => {
+                // Init tree with root children
+                if (!loaded) {
+                    const path = withFakeRoot ? ['root/root'] : undefined;
+                    _loadChildren(client, null, path);
+                }
+
+                const onVisibilityToggle = ({expanded, node, path}) => {
+                    if (node.expanded || node.loaded) {
+                        return;
+                    }
+
+                    return _loadChildren(client, {id: node.id, library: node.library.id}, path, expanded);
+                };
+
+                const onMoveNode = moveData => _saveMove(client, moveData);
+                const onDeleteNode = nodeData => _deleteNode(client, nodeData);
+
+                return (
+                    <TreeStructureView
+                        treeData={treeData}
+                        readOnly={readOnly || false}
+                        onTreeChange={setTreeData}
+                        onVisibilityToggle={onVisibilityToggle}
+                        onMoveNode={onMoveNode}
+                        onDeleteNode={onDeleteNode}
+                        onClickNode={_handleClickNode}
+                        selection={selection}
+                    />
+                );
+            }}
+        </ApolloConsumer>
+    );
 }
 
 export default withNamespaces()(TreeStructure);
