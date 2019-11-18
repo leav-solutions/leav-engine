b23ff0880b6da8d068917aaad4e13bc3c55f86cf
Teddy <t.paul@cacom.fr>
2019-11-18 17:43:36 +0100

    Fix URL for e2e test in docker env

--- config/test.js
+++ config/test.js
@@ -1,9 +1,10 @@
 module.exports = {
     server: {
+        host: '0.0.0.0',
         port: 7357
     },
     db: {
-        url: 'http://root:@localhost:8529',
+        url: 'http://root:@arangodb:8529',
         name: 'leav_test'
     }
 };
