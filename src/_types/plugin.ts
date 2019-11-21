431c9e0499ca83caa5ac5f473d8cff92735083a1
Teddy <t.paul@cacom.fr>
2019-11-21 12:32:35 +0100

    Add type for plugins

--- src/_types/plugin.ts
+++ src/_types/plugin.ts
@@ -1,6 +1,12 @@
+import {IExtensionPoints} from './shared';
+
 export interface IPlugin {
     name: string;
     description: string;
     version: string;
     author: string;
+}
+
+export interface IPluginInitModule {
+    init: (extensionPoints: IExtensionPoints) => void;
 }
