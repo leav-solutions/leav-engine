4b03ac3818cb4e0efb3dd1fee1a37b2e6df3e1e7
Teddy <t.paul@cacom.fr>
2020-02-10 11:33:20 +0100

    Fix bug on cache refresh when deleting library

--- src/components/libraries/DeleteLibrary/DeleteLibrary.tsx
+++ src/components/libraries/DeleteLibrary/DeleteLibrary.tsx
@@ -4,7 +4,7 @@ import {useTranslation} from 'react-i18next';
 import useUserData from '../../../hooks/useUserData';
 import {DeleteLibMutation, deleteLibQuery} from '../../../queries/libraries/deleteLibMutation';
 import {getLibsQuery} from '../../../queries/libraries/getLibrariesQuery';
-import {GET_LIBRARIES_libraries_list} from '../../../_gqlTypes/GET_LIBRARIES';
+import {GET_LIBRARIES, GET_LIBRARIESVariables, GET_LIBRARIES_libraries_list} from '../../../_gqlTypes/GET_LIBRARIES';
 import {PermissionsActions} from '../../../_gqlTypes/globalTypes';
 import ConfirmedButton from '../../shared/ConfirmedButton';
 import DeleteButton from '../../shared/DeleteButton';
@@ -18,10 +18,20 @@ const DeleteLibrary = ({library}: IDeleteLibraryProps): JSX.Element | null => {
     const {t} = useTranslation();
     const userData = useUserData();
     const _updateCache = (cache: DataProxy, {data: {deleteLibrary}}: any) => {
-        const cacheData: any = cache.readQuery({query: getLibsQuery});
-        cache.writeQuery({
+        const cacheData = cache.readQuery<GET_LIBRARIES, GET_LIBRARIESVariables>({query: getLibsQuery});
+
+        if (!cacheData) {
+            return;
+        }
+
+        // Remove lib from cached list
+        const newCount = cacheData.libraries?.totalCount ? cacheData.libraries?.totalCount - 1 : 0;
+        const newList = cacheData.libraries?.list
+            ? cacheData.libraries.list.filter(l => l.id !== deleteLibrary.id)
+            : [];
+        cache.writeQuery<GET_LIBRARIES, GET_LIBRARIESVariables>({
             query: getLibsQuery,
-            data: {libraries: cacheData.libraries.filter(l => l.id !== deleteLibrary.id)}
+            data: {libraries: {...cacheData.libraries, totalCount: newCount, list: newList}}
         });
     };
 
