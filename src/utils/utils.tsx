f40b4692efad1cc112c97f7dfa6d0edaefb2144a
Teddy <t.paul@cacom.fr>
2020-03-20 12:03:51 +0100

    Cleanup

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -13,23 +13,6 @@ import {IS_ALLOWED_isAllowed} from '../_gqlTypes/IS_ALLOWED';
 import {IErrorByField} from '../_types/errors';
 import {IGenericValue, ILinkValue, ITreeLinkValue, IValue} from '../_types/records';
 
-/**
- * Return label matching user language (or default language) from an object containing all languages labels
- *
- * @param labels
- * @param i18next
- */
-// export const localizedLabel = (labels: any, i18next: i18n): string => {
-//     if (labels === null) {
-//         return '';
-//     }
-
-//     const userLang = i18next.language;
-//     const fallbackLang = i18next.options.fallbackLng ? i18next.options.fallbackLng[0] : '';
-
-//     return labels[userLang] || labels[fallbackLang] || labels[Object.keys(labels)[0]] || '';
-// };
-
 export const localizedLabel = (labels: any, availableLanguages: AvailableLanguage[]): string => {
     if (labels === null) {
         return '';
