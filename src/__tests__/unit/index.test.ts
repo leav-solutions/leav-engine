c30a5afbace45109b227a8a26c87332397d5ffda
Jérémy Bel <j.bel@cacom.fr>
2020-04-10 12:20:04 +0200

    scan filesystem test done

--- src/__tests__/unit/index.test.ts
+++ src/__tests__/unit/index.test.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import {resolve} from 'path';
+import fs from 'fs';
 import config from '../../config';
 import {Config} from '../../_types/config';
 import * as rmq from '../../rmq';
@@ -21,7 +22,16 @@ beforeAll(async () => {
     }
 });
 
+afterAll(() => {
+    fs.rmdirSync(`${cfg.filesystem.absolutePath}/dir`, {recursive: true});
+});
+
 describe('unit tests sync-scan', () => {
+    test('check filesystem is empty', () => {
+        expect.assertions(1);
+        return expect(scan.filesystem(cfg.filesystem.absolutePath)).resolves.toHaveLength(0);
+    });
+
     test('RMQ initialization', async () => {
         try {
             expect.assertions(5);
@@ -45,8 +55,34 @@ describe('unit tests sync-scan', () => {
 
     test('Scan filesystem', async () => {
         try {
-            expect.assertions(1);
+            expect.assertions(5);
             await expect(scan.filesystem('wrong path')).rejects.toEqual('Wrong filesystem absolute path');
+            await expect(scan.filesystem(cfg.filesystem.absolutePath)).resolves.toHaveLength(0);
+
+            fs.mkdirSync(`${cfg.filesystem.absolutePath}/dir`);
+            fs.writeFileSync(`${cfg.filesystem.absolutePath}/dir/file`, '');
+
+            const res = await scan.filesystem(cfg.filesystem.absolutePath);
+            expect(res.length).toBe(2);
+
+            expect({...res[0]}).toStrictEqual({
+                ...fs.statSync(`${cfg.filesystem.absolutePath}/dir`),
+                name: 'dir',
+                type: 'directory',
+                path: '.',
+                level: 0,
+                trt: false
+            });
+
+            expect({...res[1]}).toStrictEqual({
+                ...fs.statSync(`${cfg.filesystem.absolutePath}/dir/file`),
+                name: 'file',
+                type: 'file',
+                hash: 'd41d8cd98f00b204e9800998ecf8427e',
+                path: 'dir',
+                level: 1,
+                trt: false
+            });
         } catch (e) {
             console.error(e);
         }
