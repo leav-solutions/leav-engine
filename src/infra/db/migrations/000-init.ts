e5020ed82f1cc1cfafe208f8357d202f63c3286e
Teddy <t.paul@cacom.fr>
2020-07-24 11:31:56 +0200

    Refactoring migration 000-init

--- src/infra/db/migrations/000-init.ts
+++ src/infra/db/migrations/000-init.ts
@@ -1,8 +1,8 @@
 import moment from 'moment';
+import {IUtils} from 'utils/utils';
 import {IMigration} from '_types/migration';
-import {ActionsListEvents} from '../../../_types/actionsList';
 import {AttributeFormats, AttributeTypes} from '../../../_types/attribute';
-import {IAttributeRepo} from '../../attribute/attributeRepo';
+import {IAttributeForRepo, IAttributeRepo} from '../../attribute/attributeRepo';
 import {ILibraryRepo} from '../../library/libraryRepo';
 import {collectionTypes, IDbService} from '../dbService';
 
@@ -10,167 +10,103 @@ interface IDeps {
     'core.infra.db.dbService'?: IDbService;
     'core.infra.library'?: ILibraryRepo;
     'core.infra.attribute'?: IAttributeRepo;
+    'core.utils'?: IUtils;
 }
 
 export default function({
     'core.infra.db.dbService': dbService = null,
     'core.infra.library': libraryRepo = null,
-    'core.infra.attribute': attributeRepo = null
+    'core.infra.attribute': attributeRepo = null,
+    'core.utils': utils = null
 }: IDeps = {}): IMigration {
     return {
         async run(ctx) {
             if (!(await dbService.collectionExists('core_attributes'))) {
                 await dbService.createCollection('core_attributes');
+            }
 
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'id',
-                        system: true,
-                        type: AttributeTypes.SIMPLE,
-                        format: AttributeFormats.TEXT,
-                        label: {fr: 'Identifiant', en: 'Identifier'},
-                        multiple_values: false
-                    },
-                    ctx
-                });
+            // Create attributes
+            const commonAttributeData = {
+                system: true,
+                multiple_values: false
+            };
+            const attributesToCreate: IAttributeForRepo[] = [
+                {
+                    ...commonAttributeData,
+                    id: 'id',
+                    type: AttributeTypes.SIMPLE,
+                    format: AttributeFormats.TEXT,
+                    label: {fr: 'Identifiant', en: 'Identifier'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'created_by',
+                    linked_library: 'users',
+                    type: AttributeTypes.SIMPLE_LINK,
+                    label: {fr: 'Créé par', en: 'Created by'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'created_at',
+                    type: AttributeTypes.SIMPLE,
+                    format: AttributeFormats.DATE,
+                    label: {fr: 'Date de création', en: 'Creation date'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'modified_by',
+                    linked_library: 'users',
+                    type: AttributeTypes.SIMPLE_LINK,
+                    label: {fr: 'Modifié par', en: 'Modified by'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'modified_at',
+                    type: AttributeTypes.SIMPLE,
+                    format: AttributeFormats.DATE,
+                    label: {fr: 'Date de modification', en: 'Modification date'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'login',
+                    type: AttributeTypes.SIMPLE,
+                    format: AttributeFormats.TEXT,
+                    label: {fr: 'Login', en: 'Login'}
+                },
+                {
+                    ...commonAttributeData,
+                    id: 'password',
+                    type: AttributeTypes.SIMPLE,
+                    format: AttributeFormats.ENCRYPTED,
+                    label: {fr: 'Mot de passe', en: 'Password'}
+                }
+            ];
 
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'created_by',
-                        linked_library: 'users',
-                        system: true,
-                        type: AttributeTypes.SIMPLE_LINK,
-                        label: {fr: 'Créé par', en: 'Created by'},
-                        multiple_values: false
-                    },
-                    ctx
-                });
-
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'created_at',
-                        system: true,
-                        type: AttributeTypes.SIMPLE,
-                        format: AttributeFormats.NUMERIC,
-                        label: {fr: 'Date de création', en: 'Creation date'},
-                        multiple_values: false,
-                        actions_list: {
-                            [ActionsListEvents.GET_VALUE]: [
-                                {
-                                    id: 'formatDate',
-                                    name: 'Format Date',
-                                    is_system: false
-                                }
-                            ],
-                            [ActionsListEvents.SAVE_VALUE]: [
-                                {
-                                    id: 'validateFormat',
-                                    name: 'Validate Format',
-                                    is_system: true
-                                }
-                            ],
-                            [ActionsListEvents.DELETE_VALUE]: []
-                        }
-                    },
-                    ctx
-                });
-
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'modified_by',
-                        linked_library: 'users',
-                        system: true,
-                        type: AttributeTypes.SIMPLE_LINK,
-                        label: {fr: 'Modifié par', en: 'Modified by'},
-                        multiple_values: false
-                    },
-                    ctx
-                });
-
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'modified_at',
-                        system: true,
-                        type: AttributeTypes.SIMPLE,
-                        format: AttributeFormats.NUMERIC,
-                        label: {fr: 'Date de modification', en: 'Modification date'},
-                        actions_list: {
-                            [ActionsListEvents.GET_VALUE]: [
-                                {
-                                    id: 'formatDate',
-                                    name: 'Format Date',
-                                    is_system: false
-                                }
-                            ],
-                            [ActionsListEvents.SAVE_VALUE]: [
-                                {
-                                    id: 'validateFormat',
-                                    name: 'Validate Format',
-                                    is_system: true
-                                }
-                            ],
-                            [ActionsListEvents.DELETE_VALUE]: []
+            for (const attribute of attributesToCreate) {
+                // Check if attribute already exists
+                const attributeFromDb = await attributeRepo.getAttributes({
+                    params: {
+                        filters: {
+                            id: attribute.id
                         },
-                        multiple_values: false
+                        strictFilters: true,
+                        withCount: false
                     },
                     ctx
                 });
 
-                await attributeRepo.createAttribute({
-                    attrData: {
-                        id: 'login',
-                        system: true,
-                        type: AttributeTypes.SIMPLE,
-                        format: AttributeFormats.TEXT,
-                        label: {fr: 'Login', en: 'Login'},
-                        actions_list: {
-                            [ActionsListEvents.GET_VALUE]: [],
-                            [ActionsListEvents.SAVE_VALUE]: [
-                                {
-                                    id: 'validateFormat',
-                                    name: 'Validate Format',
-                                    is_system: true
-                                }
-                            ],
-                            [ActionsListEvents.DELETE_VALUE]: []
-                        },
-                        multiple_values: false
-                    },
-                    ctx
-                });
+                // It already exists, move on
+                if (attributeFromDb.list.length) {
+                    continue;
+                }
 
+                // Let's create it
                 await attributeRepo.createAttribute({
                     attrData: {
-                        id: 'password',
-                        system: true,
-                        type: AttributeTypes.SIMPLE,
-                        format: AttributeFormats.ENCRYPTED,
-                        label: {fr: 'Mot de passe', en: 'Password'},
-                        actions_list: {
-                            [ActionsListEvents.SAVE_VALUE]: [
-                                {
-                                    id: 'validateFormat',
-                                    name: 'Validate Format',
-                                    is_system: true
-                                },
-                                {
-                                    id: 'encrypt',
-                                    name: 'Encrypt',
-                                    is_system: true
-                                }
-                            ],
-                            [ActionsListEvents.GET_VALUE]: [
-                                {
-                                    id: 'maskValue',
-                                    name: 'Mask Value',
-                                    is_system: true
-                                }
-                            ],
-                            [ActionsListEvents.DELETE_VALUE]: []
-                        },
-                        multiple_values: false
+                        ...attribute,
+                        actions_list: utils.getDefaultActionsList(attribute)
                     },
-                    ctx
+                    ctx: {}
                 });
             }
 
