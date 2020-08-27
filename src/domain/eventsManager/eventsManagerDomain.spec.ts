a538beb240cc207c62fa7de88ee9c4573761156a
Jérémy Bel <j.bel@cacom.fr>
2020-08-27 16:25:42 +0200

    events manager domain tests

---
+++ src/domain/eventsManager/eventsManagerDomain.spec.ts
@@ -0,0 +1,40 @@
+import * as amqp from 'amqplib';
+import {getConfig} from '../../config';
+import * as Config from '_types/config';
+import amqpService, {IAmqpService} from '../../infra/amqp/amqpService';
+import eventsManager from '././eventsManagerDomain';
+import {IQueryInfos} from '_types/queryInfos';
+import {EventType} from '../../_types/event';
+
+const mockAmqpChannel: Mockify<amqp.ConfirmChannel> = {
+    consume: jest.fn(),
+    publish: jest.fn(),
+    prefetch: jest.fn(),
+    checkExchange: jest.fn(),
+    waitForConfirms: jest.fn()
+};
+
+const ctx: IQueryInfos = {
+    userId: '1',
+    queryId: 'eventsManagerDomainTest'
+};
+
+describe('Events Manager', () => {
+    test('send amqp message', async () => {
+        const conf = await getConfig();
+
+        const amqpServ = amqpService({
+            'core.infra.amqp': {connection: null, channel: mockAmqpChannel as amqp.ConfirmChannel},
+            config: conf as Config.IConfig
+        });
+
+        const events = eventsManager({
+            config: conf as Config.IConfig,
+            'core.infra.amqp.amqpService': amqpServ as IAmqpService
+        });
+
+        await events.send({type: EventType.LIBRARY_SAVE, data: {id: 'test'}}, ctx);
+
+        expect(mockAmqpChannel.publish).toBeCalledTimes(1);
+    });
+});
