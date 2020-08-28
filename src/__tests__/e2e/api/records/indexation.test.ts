6e6286703a45ed2bdf70d24d15ce5d3fe723df42
Jérémy Bel <j.bel@cacom.fr>
2020-08-28 15:23:09 +0200

    tests e2e indexRecords

---
+++ src/__tests__/e2e/api/records/indexation.test.ts
@@ -0,0 +1,36 @@
+import {Client} from '@elastic/elasticsearch';
+import {makeGraphQlCall} from '../e2eUtils';
+import {getConfig} from '../../../../config';
+
+describe('Search', () => {
+    const testLibName = 'indexation_library_test';
+    const testLibNameType = 'indexationLibraryTest';
+    let record1;
+    let record2;
+
+    beforeAll(async () => {
+        await makeGraphQlCall(`mutation {
+            saveLibrary(library: {id: "${testLibName}", label: {fr: "Test lib"}}) { id }
+        }`);
+
+        await makeGraphQlCall('mutation { refreshSchema }');
+
+        const res = await makeGraphQlCall(`mutation {
+            c1: createRecord(library: "${testLibName}") { id },
+            c2: createRecord(library: "${testLibName}") { id }
+        }`);
+
+        record1 = res.data.data.c1.id;
+        record2 = res.data.data.c2.id;
+    });
+
+    test('index all libraries', async () => {
+        const res = await makeGraphQlCall('mutation { indexRecords }');
+        expect(res.status).toBe(200);
+    });
+
+    test('index some records', async () => {
+        const res = await makeGraphQlCall(`mutation { indexRecords(records: ["${record1}", "${record2}"]) }`);
+        expect(res.status).toBe(200);
+    });
+});
