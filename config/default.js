1391c18a453b8a3597f0fc5d52b3849a4b1f0092
Teddy <t.paul@cacom.fr>
2020-10-26 16:02:04 +0100

    Makes it easier to run in a non-dev env

--- config/default.js
+++ config/default.js
@@ -2,22 +2,22 @@ module.exports = {
     graphql: {
         uri: process.env.GRAPHQL_URI,
         token: process.env.GRAPHQL_TOKEN,
-        treeId: process.env.GRAPHQL_TREE_ID
+        treeId: process.env.GRAPHQL_TREE_ID || 'files_tree'
     },
     filesystem: {
         absolutePath: process.env.FILESYSTEM_ABSOLUTE_PATH
     },
     rmq: {
         connOpt: {
-            protocol: process.env.RMQ_CONN_PROTOCOL,
+            protocol: process.env.RMQ_CONN_PROTOCOL || 'amqp',
             hostname: process.env.RMQ_CONN_HOSTNAME,
             username: process.env.RMQ_CONN_USERNAME,
             password: process.env.RMQ_CONN_PASSWORD
         },
-        queue: process.env.RMQ_QUEUE,
-        exchange: process.env.RMQ_EXCHANGE,
-        routingKey: process.env.RMQ_ROUTING_KEY,
-        rootKey: process.env.RMQ_ROOT_KEY,
+        queue: process.env.RMQ_QUEUE || 'files_events',
+        exchange: process.env.RMQ_EXCHANGE || 'leav_core',
+        routingKey: process.env.RMQ_ROUTING_KEY || 'files.event',
+        rootKey: process.env.RMQ_ROOT_KEY || 'files1',
         type: 'direct'
     }
 };
