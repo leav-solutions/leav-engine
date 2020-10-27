7d5fab1585beb37239c977f0b7172128137afb23
Teddy <t.paul@cacom.fr>
2020-10-27 11:57:48 +0100

    Improve config handling to ease usage in prod

--- config/development.js
+++ config/development.js
@@ -1,21 +1,3 @@
 module.exports = {
-    inputRootPath: '/files',
-    outputRootPath: '/results',
-    ICCPath: '/app/profile/',
-    amqp: {
-        hostname: 'message_broker',
-        username: 'guest',
-        password: 'guest',
-        consume: {
-            queue: 'files_preview_request',
-            exchange: 'leav_core',
-            routingKey: 'files.previewRequest',
-        },
-        publish: {
-            queue: 'files_preview_response',
-            exchange: 'leav_core',
-            routingKey: 'files.previewResponse',
-        },
-    },
     verbose: true,
 };
