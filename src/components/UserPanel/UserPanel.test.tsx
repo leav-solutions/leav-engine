a9d6556afad037eefc19ddb94e4beb0a79f5d70b
Mathieu <mathieu.25565@gmail.com>
2020-10-27 15:23:33 +0100

    Move setting to UserPanel

--- src/components/UserPanel/UserPanel.test.tsx
+++ src/components/UserPanel/UserPanel.test.tsx
@@ -1,10 +1,15 @@
 import {mount} from 'enzyme';
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import UserPanel from './UserPanel';
 
 describe('UserPanel', () => {
     test('should be a sidebar', async () => {
-        const comp = mount(<UserPanel userPanelVisible={true} hideUserPanel={jest.fn()} />);
+        const comp = mount(
+            <BrowserRouter>
+                <UserPanel userPanelVisible={true} hideUserPanel={jest.fn()} />
+            </BrowserRouter>
+        );
 
         expect(comp.find('Drawer')).toHaveLength(1);
     });
