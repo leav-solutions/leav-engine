1a24547403ee51b00bab07bcfe37ae826ebac6d4
Teddy <t.paul@cacom.fr>
2021-02-05 14:12:50 +0100

    [preview-generator] Fix unit test Mock actual call to command

--- apps/preview-generator/src/processPreview/processPreview.spec.ts
+++ apps/preview-generator/src/processPreview/processPreview.spec.ts
@@ -1,9 +1,10 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {handleCheck} from './../check/handleCheck';
-import {IConfig} from '../types/types';
 import {ConsumeMessage} from 'amqplib';
+import {generatePreview} from '../generatePreview/generatePreview';
+import {IConfig} from '../types/types';
+import {handleCheck} from './../check/handleCheck';
 import {processPreview} from './processPreview';
 
 describe('processPreview', () => {
@@ -33,9 +34,11 @@ describe('processPreview', () => {
 
     test('process preview', async () => {
         (handleCheck as jest.FunctionLike) = global.__mockPromise();
+        (generatePreview as jest.FunctionLike) = global.__mockPromise();
 
         await processPreview(msg as ConsumeMessage, config as IConfig);
 
         expect(handleCheck).toBeCalledWith(JSON.parse(msg.content.toString()), config);
+        expect(generatePreview).toBeCalled();
     });
 });
