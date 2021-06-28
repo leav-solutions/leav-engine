8819f85731cfebfedd28d3aa4ced34acb331750b
Teddy <t.paul@cacom.fr>
2021-06-28 12:15:43 +0200

    [automate-scan] Fix crashes with special chars Crashed badly during init if filename had an accent or a quote for example

--- apps/automate-scan/src/index.ts
+++ apps/automate-scan/src/index.ts
@@ -3,19 +3,19 @@
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {startWatch} from './setupWatcher/setupWatcher';
 
-process.on('exit', code => {
-    console.info('Process exit event with code:', code);
-});
-
 process.on('uncaughtException', err => {
     console.error('1 - There was an uncaught error', err);
-    process.exit(1); // mandatory (as per the Node.js docs)
+    process.exit(1);
+});
+
+process.on('unhandledRejection', err => {
+    console.error('1 - There was an unhandled rejection', err);
+    process.exit(1);
 });
 
 // handle CTRL + C
 process.on('SIGINT', () => {
-    console.info();
-    console.info('0 - User stop the app');
+    console.info('0 - User stopped the app');
     process.exit(0);
 });
 
--- apps/automate-scan/src/redis/redis.ts
+++ apps/automate-scan/src/redis/redis.ts
@@ -5,6 +5,16 @@ import {Tedis} from 'redis-typescript';
 
 let client: Tedis;
 
+const _slugifyPath = (path: string): string => {
+    return path
+        .toString()
+        .trim()
+        .normalize('NFD') // split accents and base letter
+        .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents to leave only base letters
+        .replace(/[^a-zA-Z0-9\-_\/\.]/g, '_') // keep only letters, numbers, underscores, dashes, points and slashes
+        .replace(/_{2,}/g, '_'); // replace all __, ___, ... with a single _
+};
+
 export const createClient = (host: string, port: number) => {
     client = new Tedis({
         host,
@@ -21,28 +31,28 @@ export const createClient = (host: string, port: number) => {
     return client;
 };
 
-export const initRedis = (path: string, inode: number) => {
-    return client.set(path, inode.toString());
+export const initRedis = async (path: string, inode: number) => {
+    return client.set(_slugifyPath(path), inode.toString());
 };
 
 export const updateData = async (path: string, inode: number, oldPath?: string) => {
     if (oldPath) {
-        client.del(oldPath);
+        client.del(_slugifyPath(oldPath));
     }
 
-    return client.set(path, inode.toString());
+    return client.set(_slugifyPath(path), inode.toString());
 };
 
 export const deleteData = (path: string) => {
-    return client.del(path);
+    return client.del(_slugifyPath(path));
 };
 
 export const getInode = async (path: string) => {
-    const value = await client.get(path);
+    const value = await client.get(_slugifyPath(path));
     if (value) {
         return parseInt(value.toString(), 10);
     } else {
-        console.error(path + ' not found in redis');
+        console.error(_slugifyPath(path) + ' not found in redis');
         return 0;
     }
 };
