4f9d8a0aa6c32c5769e5d44250ed645678313ac1
JrmyB <9062561+JrmyB@users.noreply.github.com>
2022-01-28 10:56:04 +0100

    replace hapi with express

--- apps/core/src/domain/actionsList/actionsListDomain.ts
+++ apps/core/src/domain/actionsList/actionsListDomain.ts
@@ -1,7 +1,7 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import * as Joi from '@hapi/joi';
+import Joi from 'joi';
 import {AwilixContainer} from 'awilix';
 import {partialRight} from 'lodash';
 import {IUtils} from '../../utils/utils';
