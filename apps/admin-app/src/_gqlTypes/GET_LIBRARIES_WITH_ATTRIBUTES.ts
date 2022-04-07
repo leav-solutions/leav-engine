2325ff9d848c8687da574249f69d7dec45fcf6ba
Teddy <TdyP@users.noreply.github.com>
2022-04-07 16:36:09 +0200

    [admin] Add libraries selection from attribute

---
+++ apps/admin-app/src/_gqlTypes/GET_LIBRARIES_WITH_ATTRIBUTES.ts
@@ -0,0 +1,40 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GET_LIBRARIES_WITH_ATTRIBUTES
+// ====================================================
+
+export interface GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list_gqlNames {
+    query: string;
+    type: string;
+    list: string;
+    filter: string;
+    searchableFields: string;
+}
+
+export interface GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list_attributes {
+    id: string;
+    label: SystemTranslation | null;
+}
+
+export interface GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list {
+    id: string;
+    label: SystemTranslation | null;
+    gqlNames: GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list_gqlNames;
+    attributes: GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list_attributes[] | null;
+}
+
+export interface GET_LIBRARIES_WITH_ATTRIBUTES_libraries {
+    totalCount: number;
+    list: GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list[];
+}
+
+export interface GET_LIBRARIES_WITH_ATTRIBUTES {
+    libraries: GET_LIBRARIES_WITH_ATTRIBUTES_libraries | null;
+}
--- apps/admin-app/src/components/attributes/EditAttribute/EditAttributeTabs/InfosTab/InfosForm/AttributeLibraries/AttributeLibraries.tsx
+++ apps/admin-app/src/components/attributes/EditAttribute/EditAttributeTabs/InfosTab/InfosForm/AttributeLibraries/AttributeLibraries.tsx
@@ -1,19 +1,98 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import useLang from 'hooks/useLang';
-import React from 'react';
-import {localizedLabel} from 'utils';
+import {useMutation, useQuery} from '@apollo/client';
+import ErrorDisplay from 'components/shared/ErrorDisplay';
+import FormFieldWrapper from 'components/shared/FormFieldWrapper';
+import Loading from 'components/shared/Loading';
+import difference from 'lodash/difference';
+import {getLibrariesWithAttributesQuery} from 'queries/libraries/getLibrariesWithAttributesQuery';
+import {saveLibAttributesMutation} from 'queries/libraries/saveLibAttributesMutation';
+import React, {SyntheticEvent, useMemo} from 'react';
+import {DropdownProps, FormDropdownProps} from 'semantic-ui-react';
 import {GET_ATTRIBUTE_BY_ID_attributes_list} from '_gqlTypes/GET_ATTRIBUTE_BY_ID';
+import {
+    GET_LIBRARIES_WITH_ATTRIBUTES,
+    GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list
+} from '_gqlTypes/GET_LIBRARIES_WITH_ATTRIBUTES';
+import {SAVE_LIBRARY_ATTRIBUTES, SAVE_LIBRARY_ATTRIBUTESVariables} from '_gqlTypes/SAVE_LIBRARY_ATTRIBUTES';
+import AttributeLibrariesField from './AttributeLibrariesField';
 
-interface IAttributeLibrariesProps {
+export type AttributeLibrariesOnChange = (
+    e: SyntheticEvent<HTMLElement>,
+    fieldData: DropdownProps & {value: GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list[]}
+) => void;
+
+interface IAttributeLibrariesProps extends Omit<FormDropdownProps, 'onChange' | 'value'> {
     attribute: GET_ATTRIBUTE_BY_ID_attributes_list;
 }
 
-function AttributeLibraries({attribute}: IAttributeLibrariesProps): JSX.Element {
-    const {lang} = useLang();
+function AttributeLibraries({attribute, onChange, ...fieldProps}: IAttributeLibrariesProps): JSX.Element {
+    const {loading, error, data} = useQuery<GET_LIBRARIES_WITH_ATTRIBUTES>(getLibrariesWithAttributesQuery);
+    const [saveLibrary, {loading: loadingSave, error: saveError}] = useMutation<
+        SAVE_LIBRARY_ATTRIBUTES,
+        SAVE_LIBRARY_ATTRIBUTESVariables
+    >(saveLibAttributesMutation, {
+        onError: e => undefined // To prevent unhandled rejection, error is managed with error variable
+    });
 
-    return <div>{(attribute?.libraries ?? []).map(l => localizedLabel(l.label, lang))}</div>;
+    const libraries = data?.libraries?.list ?? [];
+    const librariesById: Record<string, GET_LIBRARIES_WITH_ATTRIBUTES_libraries_list> = useMemo(
+        () =>
+            libraries.reduce((libs, lib) => {
+                libs[lib.id] = lib;
+                return libs;
+            }, {}),
+        [libraries]
+    );
+
+    const _handleChange = async (e: SyntheticEvent<HTMLElement>, fieldData: DropdownProps) => {
+        const attributeLibrariesIds = attribute.libraries.map(lib => lib.id);
+        const addedValues = difference(fieldData.value as string[], attributeLibrariesIds);
+        const removedValues = difference(attributeLibrariesIds, fieldData.value as string[]);
+
+        // Added libraries
+        await Promise.all([
+            ...addedValues.map(libraryId =>
+                saveLibrary({
+                    variables: {
+                        libId: libraryId,
+                        attributes: [...librariesById[libraryId].attributes.map(a => a.id), attribute.id]
+                    }
+                })
+            ),
+            ...removedValues.map(libraryId =>
+                saveLibrary({
+                    variables: {
+                        libId: libraryId,
+                        attributes: [
+                            ...librariesById[libraryId].attributes.filter(a => a.id !== attribute.id).map(a => a.id)
+                        ]
+                    }
+                })
+            )
+        ]);
+    };
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <ErrorDisplay message={error.message} />;
+    }
+
+    return (
+        <FormFieldWrapper error={saveError?.message}>
+            <AttributeLibrariesField
+                libraries={libraries}
+                {...fieldProps}
+                onChange={_handleChange}
+                loading={loadingSave}
+                defaultValue={attribute.libraries.map(l => l.id)}
+            />
+        </FormFieldWrapper>
+    );
 }
 
 export default AttributeLibraries;
