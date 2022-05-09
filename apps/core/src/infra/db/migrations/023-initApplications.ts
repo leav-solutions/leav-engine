73b723e5822f1a92546886a49fe6fd3290429856
Teddy <TdyP@users.noreply.github.com>
2022-05-09 16:56:06 +0200

    [core] Add default applications in DB migration

--- apps/core/src/infra/db/migrations/023-initApplications.ts
+++ apps/core/src/infra/db/migrations/023-initApplications.ts
@@ -1,18 +1,81 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {aql} from 'arangojs';
 import {IDbService} from '../dbService';
 
 interface IDeps {
     'core.infra.db.dbService'?: IDbService;
 }
 
-export default function({'core.infra.db.dbService': dbService = null}: IDeps = {}) {
+export default function ({'core.infra.db.dbService': dbService = null}: IDeps = {}) {
     return {
-        async run() {
+        async run(ctx) {
             if (!(await dbService.collectionExists('core_applications'))) {
                 await dbService.createCollection('core_applications');
             }
+
+            const appsToInsert = [
+                {
+                    _key: 'admin',
+                    system: true,
+                    module: 'admin-app',
+                    label: {fr: 'Administration', en: 'Administration'},
+                    description: {fr: "Application d'administration", en: 'Administration app'},
+                    endpoint: 'admin',
+                    libraries: [],
+                    trees: []
+                },
+                {
+                    _key: 'explorer',
+                    system: false,
+                    module: 'explorer',
+                    label: {fr: 'Explorateur', en: 'Explorer'},
+                    description: {fr: 'Explorateur générique', en: 'Generic explorer'},
+                    endpoint: 'explorer',
+                    libraries: [],
+                    trees: []
+                },
+                {
+                    _key: 'login',
+                    system: true,
+                    module: 'login',
+                    label: {fr: 'Login', en: 'Login'},
+                    description: {fr: "Application d'authentification", en: 'Authentication app'},
+                    endpoint: 'login',
+                    libraries: [],
+                    trees: []
+                },
+                {
+                    _key: 'portal',
+                    system: true,
+                    module: 'portal',
+                    label: {fr: 'Portail', en: 'Portal'},
+                    description: {fr: "Portail d'accès à toutes les applications", en: 'All applications portal'},
+                    endpoint: 'portal',
+                    libraries: [],
+                    trees: []
+                }
+            ];
+
+            await Promise.all(
+                appsToInsert.map(async app => {
+                    // Check if app already exists
+                    const existingApp = await dbService.execute({
+                        query: aql`
+                            FOR app IN core_applications
+                                FILTER app._key == ${app._key}
+                                RETURN app
+                        `,
+                        ctx
+                    });
+
+                    // If not, create it
+                    if (!existingApp.length) {
+                        await dbService.execute({query: aql`INSERT ${app} INTO core_applications`, ctx});
+                    }
+                })
+            );
         }
     };
 }
