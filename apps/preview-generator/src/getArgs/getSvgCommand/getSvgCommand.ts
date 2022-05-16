447002433e0009aa33ce6f571974650af89c8e02
Teddy <TdyP@users.noreply.github.com>
2022-05-16 16:20:28 +0200

    [preview-generator] Fix SVG files

--- apps/preview-generator/src/getArgs/getSvgCommand/getSvgCommand.ts
+++ apps/preview-generator/src/getArgs/getSvgCommand/getSvgCommand.ts
@@ -4,7 +4,7 @@
 export const getSvgCommand = (input: string, output: string, size: number) => {
     const command = 'inkscape';
 
-    const args = ['-z', '-e', output, '-w', size.toString(), '-h', size.toString(), input];
+    const args = ['-o', output, '-w', size.toString(), '-h', size.toString(), input];
 
     return {
         command,
