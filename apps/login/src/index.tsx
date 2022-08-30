a61180c8a79fe91fa1430d0b451de68f37f256e8
Teddy <TdyP@users.noreply.github.com>
2022-08-30 17:28:22 +0200

    [login] Fix webpack warning during build

--- apps/login/src/index.tsx
+++ apps/login/src/index.tsx
@@ -1,7 +1,7 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import App from 'components/App';
 import 'index.css';
 import React from 'react';
