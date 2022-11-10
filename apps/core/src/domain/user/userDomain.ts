01018aac86d492c0e5465a38d0f76819f70ea72e
Jérémy <9062561+jrmyb@users.noreply.github.com>
2022-11-10 10:50:58 +0100

    email attribute in user library. admin login/pwd in docker-compose. rename userDataDomain to userDomain

---
+++ apps/core/src/domain/user/userDomain.ts
@@ -0,0 +1,104 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {IPermissionDomain} from 'domain/permission/permissionDomain';
+import {readFile} from 'fs/promises';
+import {IMailerService} from 'infra/mailer/mailerService';
+import {IUserDataRepo} from 'infra/userData/userDataRepo';
+import {IQueryInfos} from '_types/queryInfos';
+import {IUserData} from '_types/userData';
+import PermissionError from '../../errors/PermissionError';
+import {AdminPermissionsActions, PermissionTypes} from '../../_types/permissions';
+import handlebars from 'handlebars';
+import * as Config from '_types/config';
+import {i18n} from 'i18next';
+
+export interface IUserDomain {
+    saveUserData(key: string, value: any, global: boolean, ctx: IQueryInfos): Promise<IUserData>;
+    getUserData(keys: string[], global: boolean, ctx: IQueryInfos): Promise<IUserData>;
+    sendResetPasswordEmail(mail: string, token: string, login: string, browser: string, os: string): Promise<void>;
+}
+
+interface IDeps {
+    config?: Config.IConfig;
+    'core.domain.permissions'?: IPermissionDomain;
+    'core.infra.userData'?: IUserDataRepo;
+    'core.domain.permission'?: IPermissionDomain;
+    'core.infra.mailer.mailerService'?: IMailerService;
+    translator?: i18n;
+}
+
+export default function ({
+    config = null,
+    'core.infra.userData': userDataRepo = null,
+    'core.domain.permission': permissionDomain = null,
+    'core.infra.mailer.mailerService': mailerService = null,
+    translator = null
+}: IDeps = {}): IUserDomain {
+    return {
+        async sendResetPasswordEmail(
+            email: string,
+            token: string,
+            login: string,
+            browser: string,
+            os: string
+        ): Promise<void> {
+            const html = await readFile(__dirname + `/resetPassword_${config.lang.default}.html`, {encoding: 'utf-8'});
+            const template = handlebars.compile(html);
+
+            const htmlWithData = template({
+                login,
+                resetPasswordUrl: `${config.server.publicUrl}/reset-password?token=${token}`, // FIXME: right url to render static html page
+                supportEmail: config.server.supportEmail,
+                browser,
+                os
+            });
+
+            await mailerService.sendEmail({
+                to: email,
+                subject: translator.t('mailer.reset_password_subject', {lng: config.lang.default}),
+                html: htmlWithData
+            });
+        },
+        async saveUserData(key: string, value: any, global: boolean, ctx: IQueryInfos): Promise<IUserData> {
+            if (
+                global &&
+                !(await permissionDomain.isAllowed({
+                    type: PermissionTypes.ADMIN,
+                    action: AdminPermissionsActions.MANAGE_GLOBAL_PREFERENCES,
+                    userId: ctx.userId,
+                    ctx
+                }))
+            ) {
+                throw new PermissionError(AdminPermissionsActions.MANAGE_GLOBAL_PREFERENCES);
+            }
+
+            return userDataRepo.saveUserData(key, value, global, ctx);
+        },
+        async getUserData(keys: string[], global: boolean = false, ctx: IQueryInfos): Promise<IUserData> {
+            const isAllowed = await permissionDomain.isAllowed({
+                type: PermissionTypes.ADMIN,
+                action: AdminPermissionsActions.MANAGE_GLOBAL_PREFERENCES,
+                userId: ctx.userId,
+                ctx
+            });
+
+            if (global && !isAllowed) {
+                throw new PermissionError(AdminPermissionsActions.MANAGE_GLOBAL_PREFERENCES);
+            }
+
+            const res = await userDataRepo.getUserData(keys, global, ctx);
+
+            if (isAllowed && !global) {
+                for (const k of keys) {
+                    if (typeof res.data[k] === 'undefined') {
+                        const globalData = (await userDataRepo.getUserData([k], true, ctx)).data;
+                        res.data[k] = globalData ? globalData[k] : null;
+                    }
+                }
+            }
+
+            return res;
+        }
+    };
+}
