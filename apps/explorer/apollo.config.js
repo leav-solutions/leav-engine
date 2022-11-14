ceec8c4f82afae0b62a1eb1aebc60310c7998016
Teddy <TdyP@users.noreply.github.com>
2022-11-14 17:08:30 +0100

    Handle API key in types generation scripts

--- apps/explorer/apollo.config.js
+++ apps/explorer/apollo.config.js
@@ -1,18 +1,14 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-const token = require('./apollo.token');
+const apiKey = require('./apollo.apiKey');
 
 const apiUrl = 'http://core.leav.localhost';
-
 module.exports = {
     client: {
         service: {
             name: 'default',
-            url: `${apiUrl}/graphql`,
-            headers: {
-                authorization: token
-            }
+            url: `${apiUrl}/graphql?key=${apiKey}`
         }
     }
 };
