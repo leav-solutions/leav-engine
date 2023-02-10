086d3f4e9f3148d7a1fb8648867543442fc285d0
Teddy <TdyP@users.noreply.github.com>
2023-02-10 12:49:11 +0100

    Improve vite config

--- apps/portal/vite.config.js
+++ apps/portal/vite.config.js
@@ -3,29 +3,13 @@
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 /** @type {import('vite').UserConfig} */
 
-import react from '@vitejs/plugin-react';
-import path from 'path';
 import {defineConfig} from 'vite';
+import {commonConfig} from '../../vite-config-common';
 
 export default () => {
     const base = process.env.VITE_ENDPOINT ? '/' + process.env.VITE_ENDPOINT + '/' : '/app/portal/';
     return defineConfig({
-        plugins: [react()],
-        resolve: {
-            alias: {
-                components: path.resolve(__dirname, './src/components'),
-                context: path.resolve(__dirname, './src/context'),
-                hooks: path.resolve(__dirname, './src/hooks'),
-                queries: path.resolve(__dirname, './src/queries'),
-                _gqlTypes: path.resolve(__dirname, './src/_gqlTypes'),
-                _tests: path.resolve(__dirname, './src/_tests'),
-                __mocks__: path.resolve(__dirname, './src/__mocks__')
-            }
-        },
-        server: {
-            port: 3000,
-            host: true
-        },
+        ...commonConfig(__dirname),
         base
     });
 };
