2cc0de37d2fa6260d2d39052d7b9d5f8f37e069e
jrmyb <jeremy.bel@proton.me>
2023-03-28 14:30:21 +0200

    fixes review

--- apps/login/src/setupTests.ts
+++ apps/login/src/setupTests.ts
@@ -6,19 +6,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
-import React from 'react';
-
-// To prevent warnings
-React.useLayoutEffect = React.useEffect;
-
-window.matchMedia = query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-});
