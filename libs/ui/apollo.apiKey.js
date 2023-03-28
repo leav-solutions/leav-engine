e45184046a9f211f8845e8b92caf31f46aa1fa46
jrmyb <jeremy.bel@proton.me>
2023-03-28 14:45:18 +0200

    delete apollo.apiKey file

--- libs/ui/apollo.apiKey.js
+++
@@ -1,7 +0,0 @@
-/*
-Copyright LEAV Solutions 2017
-This file is released under LGPL V3
-License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-*/
-// Copy this file to apollo.token.js and put any valid token in order to use apollo CLI tools like types generation
-module.exports = 'MTQ4ODUxMnw5ZTA4YzA0MS1mOTIwLTRiMDMtODFhMi1iNGQ2NmZlNTI5YTE';
