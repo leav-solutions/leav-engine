68bd03230ffa006a096059d1bdd1ab5746e942a7
Teddy <TdyP@users.noreply.github.com>
2023-04-24 17:09:25 +0200

    Add AttributesPicker

--- libs/ui/src/components/AttributePicker/AttributesList/AttributesList.tsx
+++ libs/ui/src/components/AttributePicker/AttributesList/AttributesList.tsx
@@ -1,18 +1,15 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {PlusOutlined} from '@ant-design/icons';
 import {useApolloClient} from '@apollo/client';
 import {localizedTranslation, Override} from '@leav/utils';
-import {Button, Input, Table, TableColumnsType} from 'antd';
+import {Input, Table, TableColumnsType} from 'antd';
 import {Key, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
 import {PreviewSize} from '../../../constants';
 import {useLang} from '../../../hooks';
-import {GetLibrariesQuery, SaveLibraryMutation, useGetLibrariesQuery} from '../../../_gqlTypes';
-import {getLibrariesQuery} from '../../../_queries/libraries/getLibrariesQuery';
-import {EditLibraryModal} from '../../EditLibraryModal';
+import {GetAttributesQuery, useGetAttributesQuery} from '../../../_gqlTypes';
 import {EntityCard, IEntityData} from '../../EntityCard';
 import {ErrorDisplay} from '../../ErrorDisplay';
 import {Loading} from '../../Loading';
@@ -28,22 +25,27 @@ const HeaderWrapper = styled.div`
     }
 `;
 
-type LibraryType = Override<GetLibrariesQuery['libraries']['list'][number], {label: string}> & {key: string};
+type AttributeType = Override<GetAttributesQuery['attributes']['list'][number], {label: string}> & {key: string};
 
-interface ILibrariesListProps {
-    onSelect: (selectedLibraries: string[]) => void;
+interface IAttributesListProps {
+    onSelect: (selectedAttributes: string[]) => void;
     selected?: string[];
     multiple?: boolean;
     canCreate?: boolean;
 }
 
-function LibrariesList({onSelect, canCreate = true, selected = [], multiple = true}: ILibrariesListProps): JSX.Element {
+function AttributesList({
+    onSelect,
+    canCreate = true,
+    selected = [],
+    multiple = true
+}: IAttributesListProps): JSX.Element {
     const {t} = useTranslation('shared');
     const {lang} = useLang();
-    const {loading, error, data} = useGetLibrariesQuery();
+    const {loading, error, data} = useGetAttributesQuery();
     const [selectedRowKeys, setSelectedRowKeys] = useState<Key[]>([]);
     const [search, setSearch] = useState('');
-    const [isNewLibraryModalOpen, setIsNewLibraryModalOpen] = useState(false);
+    const [isNewAttributeModalOpen, setIsNewAttributeModalOpen] = useState(false);
     const client = useApolloClient();
 
     const _handleSelectionChange = (selection: Key[]) => {
@@ -51,7 +53,7 @@ function LibrariesList({onSelect, canCreate = true, selected = [], multiple = tr
         onSelect(selection as string[]);
     };
 
-    const _handleRowClick = (record: LibraryType) => {
+    const _handleRowClick = (record: AttributeType) => {
         let newSelection;
 
         if (selectedRowKeys.indexOf(record.key) === -1) {
@@ -68,29 +70,29 @@ function LibrariesList({onSelect, canCreate = true, selected = [], multiple = tr
         setSearch(e.currentTarget.value);
     };
 
-    const _handleClickNewLibrary = () => {
-        setIsNewLibraryModalOpen(true);
+    const _handleClickNewAttribute = () => {
+        setIsNewAttributeModalOpen(true);
     };
-    const _handleCloseNewLibrary = () => setIsNewLibraryModalOpen(false);
+    const _handleCloseNewAttribute = () => setIsNewAttributeModalOpen(false);
 
-    const _handlePostCreate = async (newLibrary: SaveLibraryMutation['saveLibrary']) => {
-        const allLibrariesData = client.readQuery<GetLibrariesQuery>({query: getLibrariesQuery});
+    // const _handlePostCreate = async (newLibrary: SaveLibraryMutation['saveLibrary']) => {
+    //     const allLibrariesData = client.readQuery<GetLibrariesQuery>({query: getLibrariesQuery});
 
-        if (allLibrariesData) {
-            client.writeQuery({
-                query: getLibrariesQuery,
-                data: {
-                    libraries: {
-                        ...allLibrariesData.libraries,
-                        list: [newLibrary, ...allLibrariesData.libraries.list]
-                    }
-                }
-            });
-        }
-        const newSelection = [...selectedRowKeys, newLibrary.id];
-        onSelect(newSelection as string[]);
-        setSelectedRowKeys(newSelection);
-    };
+    //     if (allLibrariesData) {
+    //         client.writeQuery({
+    //             query: getLibrariesQuery,
+    //             data: {
+    //                 libraries: {
+    //                     ...allLibrariesData.libraries,
+    //                     list: [newLibrary, ...allLibrariesData.libraries.list]
+    //                 }
+    //             }
+    //         });
+    //     }
+    //     const newSelection = [...selectedRowKeys, newLibrary.id];
+    //     onSelect(newSelection as string[]);
+    //     setSelectedRowKeys(newSelection);
+    // };
 
     if (loading) {
         return <Loading />;
@@ -100,50 +102,50 @@ function LibrariesList({onSelect, canCreate = true, selected = [], multiple = tr
         return <ErrorDisplay message={error.message} />;
     }
 
-    const columns: TableColumnsType<LibraryType> = [
+    const columns: TableColumnsType<AttributeType> = [
         {
-            title: t('libraries.library'),
-            key: 'library',
-            render: (_, library) => {
-                const libraryIdentity: IEntityData = {
-                    label: library.label,
-                    subLabel: library.id,
+            title: t('attributes.attribute'),
+            key: 'attribute',
+            render: (_, attribute) => {
+                const attributeIdentity: IEntityData = {
+                    label: attribute.label,
+                    subLabel: attribute.id,
                     preview: null,
                     color: null
                 };
-                return <EntityCard entity={libraryIdentity} size={PreviewSize.small} />;
+                return <EntityCard entity={attributeIdentity} size={PreviewSize.small} />;
             }
         }
     ];
 
-    const tableData: LibraryType[] = ([...data?.libraries?.list] ?? [])
-        .filter(lib => {
+    const tableData: AttributeType[] = ([...data?.attributes?.list] ?? [])
+        .filter(attribute => {
             // Do not display already selected libraries
-            if (selected.find(selectedLib => selectedLib === lib.id)) {
+            if (selected.find(selectedAttribute => selectedAttribute === attribute.id)) {
                 return false;
             }
 
             // Filter based on search
-            const label = localizedTranslation(lib.label, lang);
+            const label = localizedTranslation(attribute.label, lang);
             const searchStr = search.toLowerCase();
 
             // Search on id or label
-            return lib.id.toLowerCase().includes(searchStr) || label.toLowerCase().includes(searchStr);
+            return attribute.id.toLowerCase().includes(searchStr) || label.toLowerCase().includes(searchStr);
         })
-        .map(lib => ({
-            ...lib,
-            key: lib.id,
-            label: localizedTranslation(lib.label, lang)
+        .map(attribute => ({
+            ...attribute,
+            key: attribute.id,
+            label: localizedTranslation(attribute.label, lang)
         }));
 
     const tableHeader = (
         <HeaderWrapper>
-            <Input.Search onChange={_handleSearchChange} placeholder={t('libraries.search') + '...'} allowClear />
-            {canCreate && (
-                <Button type="primary" icon={<PlusOutlined />} onClick={_handleClickNewLibrary}>
+            <Input.Search onChange={_handleSearchChange} placeholder={t('global.search') + '...'} allowClear />
+            {/* {canCreate && (
+                <Button type="primary" icon={<PlusOutlined />} onClick={_handleClickNewAttribute}>
                     {t('libraries.new_library')}
                 </Button>
-            )}
+            )} */}
         </HeaderWrapper>
     );
 
@@ -166,16 +168,16 @@ function LibrariesList({onSelect, canCreate = true, selected = [], multiple = tr
                     onClick: () => _handleRowClick(record)
                 })}
             />
-            {isNewLibraryModalOpen && (
+            {/* {isNewAttributeModalOpen && (
                 <EditLibraryModal
-                    open={isNewLibraryModalOpen}
-                    onClose={_handleCloseNewLibrary}
+                    open={isNewAttributeModalOpen}
+                    onClose={_handleCloseNewAttribute}
                     onPostCreate={_handlePostCreate}
                     width={790}
                 />
-            )}
+            )} */}
         </>
     );
 }
 
-export default LibrariesList;
+export default AttributesList;
