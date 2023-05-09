f587fb96c6d6fceb01aba4d256d4fc7a75ce15d0
jrmyb <9062561+jrmyb@users.noreply.github.com>
2023-05-09 10:01:46 +0200

    migration file to del install field and rename admin-app on apps

---
+++ apps/core/src/infra/db/migrations/002-delInstallApps.ts
@@ -0,0 +1,36 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {IMigration} from '_types/migration';
+import {IDbService} from '../dbService';
+import {IConfig} from '_types/config';
+import {aql} from 'arangojs';
+import {IQueryInfos} from '_types/queryInfos';
+
+interface IDeps {
+    'core.infra.db.dbService'?: IDbService;
+}
+
+export default function ({'core.infra.db.dbService': dbService = null}: IDeps = {}): IMigration {
+    const _deleteInstallApps = async (ctx: IQueryInfos) => {
+        const appsCollec = dbService.db.collection('core_applications');
+
+        const query = aql`
+            FOR doc IN ${appsCollec}
+                UPDATE doc WITH { 
+                    install: null, 
+                    module: doc.module == 'admin-app' ? 'admin' : doc.module 
+                } IN ${appsCollec}
+                OPTIONS { keepNull: false }
+            RETURN NEW
+        `;
+
+        await dbService.execute({query, ctx});
+    };
+
+    return {
+        async run(ctx) {
+            await _deleteInstallApps(ctx);
+        }
+    };
+}
