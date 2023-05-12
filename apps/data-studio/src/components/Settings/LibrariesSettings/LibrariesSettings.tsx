9e86b6bea0420ec604fb395dbe9c3e8235854615
Teddy <TdyP@users.noreply.github.com>
2023-05-12 11:43:18 +0200

    Fixes after merge

--- apps/data-studio/src/components/Settings/LibrariesSettings/LibrariesSettings.tsx
+++ apps/data-studio/src/components/Settings/LibrariesSettings/LibrariesSettings.tsx
@@ -1,8 +1,13 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {Loading, SubmitStateNotifier, SubmitStateNotifierStates, useSaveApplicationMutation} from '@leav/ui';
-import ErrorDisplay from 'components/shared/ErrorDisplay';
+import {
+    ErrorDisplay,
+    Loading,
+    SubmitStateNotifier,
+    SubmitStateNotifierStates,
+    useSaveApplicationMutation
+} from '@leav/ui';
 import {useApplicationContext} from 'context/ApplicationContext';
 import {useApplicationLibraries} from 'hooks/useApplicationLibraries';
 import {useEffect, useState} from 'react';
--- libs/ui/src/components/EditApplication/_types.ts
+++ libs/ui/src/components/EditApplication/_types.ts
@@ -6,7 +6,6 @@ import {CSSObject} from 'styled-components';
 
 export interface IEditApplicationProps {
     applicationId?: string;
-    appsBaseUrl: string;
     onSetSubmitFunction?: (submitFunction: () => Promise<void>) => void;
     activeTab?: 'info' | string;
     tabContentStyle?: CSSObject;
