203a50bcc02c37bfa48d31c5876d222f2e28697e
Teddy <TdyP@users.noreply.github.com>
2023-06-08 10:42:51 +0200

    [admin] Fix library create

--- apps/admin/src/components/libraries/EditLibrary/EditLibrary.test.tsx
+++ apps/admin/src/components/libraries/EditLibrary/EditLibrary.test.tsx
@@ -1,27 +1,89 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {MockedProvider} from '@apollo/client/testing';
-import {render} from 'enzyme';
+import {mockLibrary} from '__mocks__/libraries';
+import {PermissionsActions} from '_gqlTypes/globalTypes';
+import {render, screen} from '_tests/testUtils';
+import {getLibByIdQuery} from 'queries/libraries/getLibraryById';
 import React from 'react';
-import MockedLangContextProvider from '../../../__mocks__/MockedLangContextProvider';
-import MockedUserContextProvider from '../../../__mocks__/MockedUserContextProvider';
 import EditLibrary from './EditLibrary';
 
+jest.mock('./EditLibraryTabs', () => {
+    return function EditLibraryTabs() {
+        return <div>EditLibraryTabs</div>;
+    };
+});
+
 describe('EditLibrary', () => {
-    test('Snapshot test', async () => {
+    test('Render tabs when editing library', async () => {
+        const mocks = [
+            {
+                request: {
+                    query: getLibByIdQuery,
+                    variables: {
+                        id: ['test']
+                    }
+                },
+                result: {
+                    data: {
+                        libraries: {
+                            list: [
+                                {
+                                    ...mockLibrary
+                                }
+                            ]
+                        }
+                    }
+                }
+            }
+        ];
         const mockMatch: any = {params: {id: 'test'}};
+        render(<EditLibrary match={mockMatch} />, {apolloMocks: mocks});
 
-        const comp = render(
-            <MockedProvider>
-                <MockedLangContextProvider>
-                    <MockedUserContextProvider>
-                        <EditLibrary match={mockMatch} />
-                    </MockedUserContextProvider>
-                </MockedLangContextProvider>
-            </MockedProvider>
-        );
+        expect(screen.getByText(/loading/)).toBeInTheDocument();
 
-        expect(comp).toMatchSnapshot();
+        expect(await screen.findByText('EditLibraryTabs')).toBeInTheDocument();
+    });
+
+    test('Render tabs when creating library', async () => {
+        const mockMatch: any = {params: {}};
+        render(<EditLibrary match={mockMatch} />);
+
+        expect(screen.getByText('EditLibraryTabs')).toBeInTheDocument();
+    });
+
+    test('Display error if not allowed to create', async () => {
+        const mockMatch: any = {params: {}};
+        render(<EditLibrary match={mockMatch} />, {
+            userPermissions: {[PermissionsActions.admin_create_library]: false}
+        });
+
+        expect(screen.getByText('errors.access_denied')).toBeInTheDocument();
+    });
+
+    test('Display error if unknown library', async () => {
+        const mocks = [
+            {
+                request: {
+                    query: getLibByIdQuery,
+                    variables: {
+                        id: ['test']
+                    }
+                },
+                result: {
+                    data: {
+                        libraries: {
+                            list: []
+                        }
+                    }
+                }
+            }
+        ];
+        const mockMatch: any = {params: {id: 'test'}};
+        render(<EditLibrary match={mockMatch} />, {apolloMocks: mocks});
+
+        expect(screen.getByText(/loading/)).toBeInTheDocument();
+
+        expect(await screen.findByText(/unknown_library/)).toBeInTheDocument();
     });
 });
