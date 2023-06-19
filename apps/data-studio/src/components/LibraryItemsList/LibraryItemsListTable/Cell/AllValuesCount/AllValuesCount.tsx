5f68361a01fb4211c25cb454baaf3ef447cab583
Teddy <TdyP@users.noreply.github.com>
2023-06-19 17:23:48 +0200

    [data-studio] Fix display of other links

--- apps/data-studio/src/components/LibraryItemsList/LibraryItemsListTable/Cell/AllValuesCount/AllValuesCount.tsx
+++ apps/data-studio/src/components/LibraryItemsList/LibraryItemsListTable/Cell/AllValuesCount/AllValuesCount.tsx
@@ -2,16 +2,18 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {RecordCard, themeVars} from '@leav/ui';
+import {AttributeType} from '_gqlTypes/globalTypes';
+import {PreviewSize} from '_types/types';
 import {Modal, Tooltip} from 'antd';
 import List from 'components/shared/List';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
 import {stopEvent} from 'utils';
-import {PreviewSize} from '_types/types';
 
 interface IAllValuesCountProps {
     values: any[];
+    attributeType: AttributeType;
 }
 
 const MoreValuesCount = styled.span`
@@ -27,12 +29,13 @@ const MoreValuesCount = styled.span`
     cursor: pointer;
 `;
 
-function AllValuesCount({values}: IAllValuesCountProps): JSX.Element {
+function AllValuesCount({values, attributeType}: IAllValuesCountProps): JSX.Element {
     const {t} = useTranslation();
+    const [modal, contextHolder] = Modal.useModal();
 
     const _handleShowMoreClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         stopEvent(e);
-        Modal.info({
+        modal.info({
             icon: null,
             content: (
                 <List
@@ -47,9 +50,22 @@ function AllValuesCount({values}: IAllValuesCountProps): JSX.Element {
         });
     };
 
+    const otherValuesData = values
+        .slice(0, 5)
+        .map(val => {
+            const whoAmI = attributeType === AttributeType.tree ? val?.record?.whoAmI : val?.whoAmI;
+
+            if (!whoAmI) {
+                return null;
+            }
+
+            return whoAmI.label ?? whoAmI.id;
+        })
+        .filter(val => !!val); // To make sure we don't have null values. This crashes the List component
+
     const otherValuesList = (
         <List
-            dataSource={values.slice(0, 5).map(val => val?.whoAmI?.label ?? val?.whoAmI?.id)}
+            dataSource={otherValuesData}
             size="small"
             maxHeight="50vh"
             footer={values.length > 5 && <a onClick={_handleShowMoreClick}>{t('items_list.show_more_links')}</a>}
@@ -63,6 +79,7 @@ function AllValuesCount({values}: IAllValuesCountProps): JSX.Element {
                     {values.length}
                 </MoreValuesCount>
             </Tooltip>
+            {contextHolder}
         </>
     );
 }
