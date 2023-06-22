c739eea2db6f328021b4656ee7a21e3dfddc96a9
Teddy <TdyP@users.noreply.github.com>
2023-06-22 14:41:27 +0200

    Improve handling of Excel calculation error

--- apps/core/src/i18nextInit.ts
+++ apps/core/src/i18nextInit.ts
@@ -1,10 +1,11 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {IConfig} from '_types/config';
 import i18next, {i18n} from 'i18next';
 import Backend from 'i18next-fs-backend';
 
-export default async (config: any): Promise<i18n> => {
+export default async (config: IConfig): Promise<i18n> => {
     await i18next.use(Backend).init({
         lng: config.lang.default,
         fallbackLng: config.lang.default,
