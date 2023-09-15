8f8c8f91aa0c9ef5c8da24b0d6b3d82e1d88f9e0
k.legras <k.legras@aristid.com>
2023-09-15 10:49:08 +0200

    Update CI

--- scripts/splitTestFile.js
+++ scripts/splitTestFile.js
@@ -8,22 +8,16 @@ const exec = util.promisify(require('child_process').exec);
  */
 (async () => {
     try {
-
-        const {stdout, stderr}  = await exec('npx jest --listTests --json');
-        const listOfProject = ['libs/ui','apps/data-studio','apps/admin','apps/core'];
-        const final = [];
-
-        for(const filePath of stdout.trim(1).slice(0, -1).split(',')){
-            for(const project of listOfProject){
-                if(filePath.includes(project)){
-                    if(!final[project]){
-                        final[project] =[];
-                    }
-                    final[project].push(filePath.replaceAll('"',''));
-                }
-            }
-        }
-    return final;
+        const {stdout, stderr}  = await exec('yarn workspaces list --json');
+        const workspacesArray = stdout.split('\n');
+        workspacesArray.shift();
+        workspacesArray.pop();
+        let workspaceFilter;
+        workspacesArray.forEach((workspace) => {
+            workspace = JSON.parse(workspace);
+            workspaceFilter.push(workspace.location);
+        });
+        return workspaceFilter;
     } catch (e) {
         console.error(e);
         process.exit(1);
