3485775db553f94126000dd6539f064cbdda2d5f
k.legras <k.legras@aristid.com>
2023-09-20 11:17:06 +0200

    Remove change on script

--- scripts/testChangedProjects.js
+++ scripts/testChangedProjects.js
@@ -30,7 +30,7 @@ const getChangedProjects = require('./helpers/getChangedProjects');
         // Run tests
         const testRunProcess = spawn(
             'yarn',
-            ['workspaces', 'foreach', '-v', ...includeCmd, '--exclude', 'leav-monorepo', 'run', 'test', '--debug', '--maxWorkers=2'],
+            ['workspaces', 'foreach', '-v', ...includeCmd, '--exclude', 'leav-monorepo', 'run', 'test'],
             {
                 stdio: 'inherit'
             }
