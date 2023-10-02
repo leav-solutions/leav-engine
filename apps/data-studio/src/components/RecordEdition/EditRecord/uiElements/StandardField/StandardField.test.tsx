84d04a3c0b9fbb548731ebfcc57f0aa720e1218e
Teddy <TdyP@users.noreply.github.com>
2023-10-02 09:59:00 +0200

    Cleanup

--- apps/data-studio/src/components/RecordEdition/EditRecord/uiElements/StandardField/StandardField.test.tsx
+++ apps/data-studio/src/components/RecordEdition/EditRecord/uiElements/StandardField/StandardField.test.tsx
@@ -400,7 +400,7 @@ describe('StandardField', () => {
             userEvent.click(screen.getByRole('button', {name: 'global.submit'}));
         });
         expect(mockHandleSubmit).toHaveBeenCalled();
-    }, 10000);
+    });
 
     test('Display error message', async () => {
         const onSubmitFail: SubmitValueFunc = jest.fn().mockReturnValue({
@@ -417,7 +417,7 @@ describe('StandardField', () => {
         });
 
         expect(screen.getByText('ERROR_MESSAGE')).toBeInTheDocument();
-    }, 10000);
+    });
 
     test('Delete value', async () => {
         render(<StandardField element={mockFormElementInput} {...baseProps} />);
@@ -477,7 +477,7 @@ describe('StandardField', () => {
         });
 
         expect(baseProps.onDeleteMultipleValues).toBeCalled();
-    }, 10000);
+    });
 
     describe('Values list', () => {
         const mockFormElementWithValuesList: FormElement<{}> = {
@@ -513,7 +513,7 @@ describe('StandardField', () => {
             expect(screen.getByText('Other value')).toBeInTheDocument();
 
             expect(screen.queryByRole('button', {name: 'global.submit'})).not.toBeInTheDocument();
-        }, 10000);
+        });
 
         test('Filters list when typing', async () => {
             render(<StandardField element={{...mockFormElementWithValuesList, values: []}} {...baseProps} />);
@@ -529,7 +529,7 @@ describe('StandardField', () => {
 
             expect(screen.queryByText('My value')).not.toBeInTheDocument();
             expect(screen.getByText('Other value')).toBeInTheDocument();
-        }, 10000);
+        });
 
         test('On click on a value, save it', async () => {
             render(<StandardField element={{...mockFormElementWithValuesList, values: []}} {...baseProps} />);
@@ -544,7 +544,7 @@ describe('StandardField', () => {
                 [{idValue: null, value: 'My value', attribute: mockFormElementWithValuesList.attribute}],
                 null
             );
-        }, 10000);
+        });
 
         test('On Enter, first matching value is selected', async () => {
             render(<StandardField element={{...mockFormElementWithValuesList, values: []}} {...baseProps} />);
@@ -567,7 +567,7 @@ describe('StandardField', () => {
                 ],
                 null
             );
-        }, 10000);
+        });
 
         test('If no match, display a message', async () => {
             render(<StandardField element={{...mockFormElementWithValuesList, values: []}} {...baseProps} />);
@@ -582,7 +582,7 @@ describe('StandardField', () => {
                 userEvent.type(editingInputElem, 'zzz');
             });
             expect(screen.getByText('record_edition.no_matching_value')).toBeInTheDocument();
-        }, 10000);
+        });
 
         test('If open values list, display submit button', async () => {
             render(<StandardField element={{...mockFormElementWithValuesListOpen, values: []}} {...baseProps} />);
@@ -590,7 +590,7 @@ describe('StandardField', () => {
             userEvent.click(screen.getByRole('textbox'));
 
             expect(screen.queryByRole('button', {name: 'global.submit'})).toBeInTheDocument();
-        }, 10000);
+        });
 
         test('If open values list, can copy a value from the list and edit it', async () => {
             await act(async () => {
@@ -609,7 +609,7 @@ describe('StandardField', () => {
             });
 
             expect(screen.getByRole('textbox')).toHaveValue('My value');
-        }, 10000);
+        });
 
         test('If open values list, current value appears on the list', async () => {
             await act(async () => {
@@ -624,6 +624,6 @@ describe('StandardField', () => {
             userEvent.type(editingInputElem, 'Some new value');
 
             expect(screen.getByText(/Some new value/)).toBeInTheDocument();
-        }, 10000);
+        });
     });
 });
