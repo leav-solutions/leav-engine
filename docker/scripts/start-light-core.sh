b6f67c0b4f9c3d1d05fb5994468cfd0a8e16f9c1
Steven <steven.f.13@gmail.com>
2023-11-13 10:23:04 +0000

    Add prebuild files and Github action script to auto build core and vite apps (#352)

---
+++ docker/scripts/start-light-core.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Install dependencies"
+# If no dependencies have been update, you can comment this line
+# to use up to 30sec in load time
+yarn install
+
+# Migrate database if needed
+yarn run db:migrate
+
+echo "Download artifact and extract it (contains dist folders)"
+node ./scripts/preload.js
+
+echo "Unzip dist-artifact-**.zip"
+unzip -o dist-artifact-**.zip -q
+rm -rf dist-artifact-**.zip
+
+echo "mkdir plugin if does not exist"
+mkdir -p /app/apps/core/dist/plugins
+
+echo "start server"
+yarn run start --server
