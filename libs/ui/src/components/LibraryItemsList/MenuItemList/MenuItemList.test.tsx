1fa3bfc38a64448db00cb20e553346c1ec1cf7aa
Jules HABLOT <j.hablot-ext@aristid.com>
2024-02-21 16:10:47 +0100

    feat(@leav/ui): add refresh and success notification on new element creation inside library

--- libs/ui/src/components/LibraryItemsList/MenuItemList/MenuItemList.test.tsx
+++ libs/ui/src/components/LibraryItemsList/MenuItemList/MenuItemList.test.tsx
@@ -5,32 +5,32 @@ import {render, screen} from '_ui/_tests/testUtils';
 import MockSearchContextProvider from '_ui/__mocks__/common/mockSearchContextProvider';
 import {mockGetLibraryDetailExtendedElement} from '_ui/__mocks__/mockQuery/mockGetLibraryDetailExtendedQuery';
 import MenuItemList from './MenuItemList';
+import {LibraryBehavior} from '_ui/_gqlTypes';
+import userEvent from '@testing-library/user-event';
 
-jest.mock(
-    '../MenuView',
-    () =>
-        function MenuView() {
-            return <div>MenuView</div>;
-        }
-);
+jest.mock('../MenuView', () => () => <div>MenuView</div>);
 
-jest.mock('../MenuSelection', () => {
-    return function MenuSelection() {
-        return <div>MenuSelection</div>;
-    };
-});
+jest.mock('../MenuSelection', () => () => <div>MenuSelection</div>);
 
-jest.mock('../SearchItems', () => {
-    return function SearchItems() {
-        return <div>SearchItems</div>;
-    };
-});
+jest.mock('../SearchItems', () => () => <div>SearchItems</div>);
 
-jest.mock('../DisplayOptions', () => {
-    return function DisplayOptions() {
-        return <div>DisplayOptions</div>;
-    };
-});
+jest.mock('../DisplayOptions', () => () => <div>DisplayOptions</div>);
+
+jest.mock('_ui/components', () => ({
+    EditRecordModal: ({afterCreate}) => afterCreate?.(),
+    UploadFiles: ({onCompleted}) => onCompleted?.(),
+    CreateDirectory: ({onCompleted}) => onCompleted?.()
+}));
+
+const kitNotificationMock = {
+    success: jest.fn()
+};
+jest.mock('aristid-ds', () => ({
+    ...jest.requireActual('aristid-ds'),
+    useKitNotification: () => ({
+        kitNotification: kitNotificationMock
+    })
+}));
 
 describe('MenuItemList', () => {
     test('should have MenuView', async () => {
@@ -71,5 +71,32 @@ describe('MenuItemList', () => {
         );
 
         expect(screen.getByText('DisplayOptions')).toBeInTheDocument();
+    });
+
+    describe('refetch after creation', () => {
+        beforeEach(() => {
+            kitNotificationMock.success.mockClear();
+        });
+
+        test.each([LibraryBehavior.standard, LibraryBehavior.files, LibraryBehavior.directories])(
+            'refetch callback and success notification are called when props are called in modal %s',
+            async behavior => {
+                const refetch = jest.fn();
+                render(
+                    <MockSearchContextProvider>
+                        <MenuItemList refetch={refetch} library={{...mockGetLibraryDetailExtendedElement, behavior}} />
+                    </MockSearchContextProvider>
+                );
+
+                await userEvent.click(screen.getByRole('button', {name: 'plus items_list.new'}));
+
+                expect(refetch).toHaveBeenCalledTimes(1);
+                expect(kitNotificationMock.success).toHaveBeenCalledTimes(1);
+                expect(kitNotificationMock.success).toHaveBeenCalledWith({
+                    message: 'items_list.created_in_success.message',
+                    description: ''
+                });
+            }
+        );
     });
 });
