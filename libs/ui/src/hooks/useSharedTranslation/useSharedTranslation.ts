55a43c7bcd2f2655bf2a1a2773d7e7a88ab34937
Teddy <TdyP@users.noreply.github.com>
2024-02-22 11:26:13 +0100

    [libs/ui] Fix handling of user lang

--- libs/ui/src/hooks/useSharedTranslation/useSharedTranslation.ts
+++ libs/ui/src/hooks/useSharedTranslation/useSharedTranslation.ts
@@ -2,7 +2,6 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import {useContext, useMemo} from 'react';
 import {initReactI18next, useTranslation} from 'react-i18next';
 import {LangContext} from '../../contexts';
@@ -14,28 +13,26 @@ const useSharedTranslation = () => {
 
     const i18n = useMemo(
         () =>
-            i18next
-                .use(initReactI18next)
-                .use(LanguageDetector)
-                .createInstance(
-                    {
-                        fallbackLng: langContext.defaultLang,
-                        ns: ['shared'],
-                        defaultNS: 'shared',
-                        resources: {
-                            fr: {shared: fr},
-                            en: {shared: en}
-                        },
-                        react: {
-                            useSuspense: true
-                        }
+            i18next.use(initReactI18next).createInstance(
+                {
+                    lng: langContext.lang[0],
+                    fallbackLng: langContext.defaultLang,
+                    ns: ['shared'],
+                    defaultNS: 'shared',
+                    resources: {
+                        fr: {shared: fr},
+                        en: {shared: en}
                     },
-                    (err, t) => {
-                        if (err) {
-                            return console.error('Something went wrong loading', err);
-                        }
+                    react: {
+                        useSuspense: true
                     }
-                ),
+                },
+                (err, t) => {
+                    if (err) {
+                        return console.error('Something went wrong loading', err);
+                    }
+                }
+            ),
         [langContext.lang]
     );
 
