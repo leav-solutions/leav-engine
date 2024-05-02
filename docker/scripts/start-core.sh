76d97e5f579ea45dc64c5828a0ab40fc13ac35e3
Teddy <TdyP@users.noreply.github.com>
2024-05-02 09:08:56 +0200

    fixup

--- docker/scripts/start-core.sh
+++ docker/scripts/start-core.sh
@@ -1,15 +1,17 @@
 # Install dependencies
+echo "Install apps dependencies"
 yarn install
 
 # Install plugins dependencies
 for plugin in ./src/plugins/*/; do
+  echo "Install dependencies for $plugin"
   yarn --cwd "$plugin" install
 done
 
-# Start the server
 yarn run db:migrate:dev
 
 # To avoid permissions issues later on, in particular during applications install
 chown -R node:node /app
 
+# Start the server
 yarn run start:watch
