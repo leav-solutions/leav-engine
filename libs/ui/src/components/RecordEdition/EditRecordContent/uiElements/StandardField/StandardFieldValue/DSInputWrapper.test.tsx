9d0deb984fdc07d96cf22626ff1ce378b3aeb941
Arno <arno.tgh@gmail.com>
2024-05-03 17:23:37 +0200

    feat(@leav/ui): manage inherit and override on simple input

--- libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/DSInputWrapper.test.tsx
+++ libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/DSInputWrapper.test.tsx
@@ -5,6 +5,7 @@ import {render, screen} from '_ui/_tests/testUtils';
 import {DSInputWrapper} from './DSInputWrapper';
 import {FieldScope} from '../../../_types';
 import {
+    InheritedFlags,
     IStandardFieldReducerState,
     StandardFieldValueState
 } from '../../../reducers/standardFieldReducer/standardFieldReducer';
@@ -12,9 +13,7 @@ import {mockRecord} from '_ui/__mocks__/common/record';
 import {mockFormElementInput} from '_ui/__mocks__/common/form';
 import {mockAttributeLink} from '_ui/__mocks__/common/attribute';
 import userEvent from '@testing-library/user-event';
-import {Form} from 'antd';
-import {RecordFormElementsValueStandardValue} from '_ui/hooks/useGetRecordForm';
-import {useForm} from 'antd/lib/form/Form';
+import {AntForm} from 'aristid-ds';
 
 const label = 'label';
 const idValue = '123';
@@ -66,23 +65,57 @@ const getInitialState = (required: boolean): IStandardFieldReducerState => ({
     isInheritedValue: false
 });
 
+const inheritedValues = [
+    {
+        isInherited: null,
+        value: 'override value',
+        raw_value: 'override value'
+    },
+    {
+        isInherited: true,
+        value: 'inherited value',
+        raw_value: 'inherited value'
+    }
+];
+
+const inheritedNotOverrideValue: InheritedFlags = {
+    isInheritedValue: true,
+    isInheritedOverrideValue: false,
+    isInheritedNotOverrideValue: true,
+    inheritedValue: {raw_value: inheritedValues[1].raw_value}
+};
+
+const inheritedOverrideValue: InheritedFlags = {
+    isInheritedValue: true,
+    isInheritedOverrideValue: true,
+    isInheritedNotOverrideValue: false,
+    inheritedValue: {raw_value: inheritedValues[1].raw_value}
+};
+
 describe('DSInputWrapper', () => {
     const mockHandleSubmit = jest.fn();
+    const mockOnChange = jest.fn();
     let user!: ReturnType<typeof userEvent.setup>;
 
     beforeEach(() => {
         user = userEvent.setup({});
+        mockOnChange.mockReset();
         mockHandleSubmit.mockReset();
     });
 
     test('Should submit empty value if field is not required', async () => {
         const state = getInitialState(false);
         render(
-            <Form>
-                <Form.Item>
-                    <DSInputWrapper state={state} infoButton="" handleSubmit={mockHandleSubmit} />
-                </Form.Item>
-            </Form>
+            <AntForm>
+                <AntForm.Item>
+                    <DSInputWrapper
+                        state={state}
+                        infoButton=""
+                        handleSubmit={mockHandleSubmit}
+                        onChange={mockOnChange}
+                    />
+                </AntForm.Item>
+            </AntForm>
         );
 
         const input = screen.getByRole('textbox');
@@ -90,17 +123,23 @@ describe('DSInputWrapper', () => {
         await user.tab();
 
         expect(mockHandleSubmit).toHaveBeenCalledWith('', state.attribute.id);
+        expect(mockOnChange).toHaveBeenCalled();
     });
 
-    describe('With required input', () => {
+    describe('With required input and no inheritance', () => {
         test('Should submit the value if field is not empty', async () => {
             const state = getInitialState(true);
             render(
-                <Form>
-                    <Form.Item>
-                        <DSInputWrapper state={state} infoButton="" handleSubmit={mockHandleSubmit} />
-                    </Form.Item>
-                </Form>
+                <AntForm>
+                    <AntForm.Item>
+                        <DSInputWrapper
+                            state={state}
+                            infoButton=""
+                            handleSubmit={mockHandleSubmit}
+                            onChange={mockOnChange}
+                        />
+                    </AntForm.Item>
+                </AntForm>
             );
 
             const text = 'text';
@@ -110,26 +149,119 @@ describe('DSInputWrapper', () => {
             await user.tab();
 
             expect(mockHandleSubmit).toHaveBeenCalledWith(text, state.attribute.id);
+            expect(mockOnChange).toHaveBeenCalled();
         });
 
         test('Should submit the default value if field is empty', async () => {
             const state = getInitialState(true);
             render(
-                <Form>
-                    <Form.Item>
-                        <DSInputWrapper state={state} infoButton="" handleSubmit={mockHandleSubmit} />
-                    </Form.Item>
-                </Form>
+                <AntForm>
+                    <AntForm.Item>
+                        <DSInputWrapper
+                            state={state}
+                            infoButton=""
+                            handleSubmit={mockHandleSubmit}
+                            onChange={mockOnChange}
+                            value={mockValue.originRawValue}
+                        />
+                    </AntForm.Item>
+                </AntForm>
             );
 
             const input = screen.getByRole('textbox');
             await user.click(input);
             await user.tab();
 
-            expect(mockHandleSubmit).toHaveBeenCalledWith(
-                (state.formElement.values[0] as RecordFormElementsValueStandardValue).raw_value,
-                state.attribute.id
+            expect(mockHandleSubmit).toHaveBeenCalledWith(mockValue.originRawValue, state.attribute.id);
+        });
+    });
+
+    describe('With inheritance', () => {
+        test("Should display the inherited value by default and not save if we don't change it", async () => {
+            let state = getInitialState(false);
+            state = {
+                ...state,
+                ...inheritedNotOverrideValue,
+                formElement: {...state.formElement, values: inheritedValues}
+            };
+            render(
+                <AntForm>
+                    <AntForm.Item>
+                        <DSInputWrapper
+                            state={state}
+                            infoButton=""
+                            handleSubmit={mockHandleSubmit}
+                            onChange={mockOnChange}
+                            value={inheritedValues[1].raw_value}
+                        />
+                    </AntForm.Item>
+                </AntForm>
             );
+            const input = screen.getByRole('textbox');
+            expect(input).toHaveValue(inheritedValues[1].raw_value);
+            const clearButton = screen.queryByRole('button');
+            expect(clearButton).not.toBeInTheDocument();
+
+            await user.click(input);
+            await user.tab();
+
+            expect(mockHandleSubmit).not.toHaveBeenCalled();
+        });
+
+        test('Should display the override value in the input and inherited value under it', async () => {
+            let state = getInitialState(false);
+            state = {
+                ...state,
+                ...inheritedOverrideValue,
+                formElement: {...state.formElement, values: inheritedValues}
+            };
+
+            render(
+                <AntForm>
+                    <AntForm.Item>
+                        <DSInputWrapper
+                            state={state}
+                            infoButton=""
+                            handleSubmit={mockHandleSubmit}
+                            onChange={mockOnChange}
+                            value={inheritedValues[0].raw_value}
+                        />
+                    </AntForm.Item>
+                </AntForm>
+            );
+
+            const input = screen.getByRole('textbox');
+            const helperText = screen.getByText(/inherited value/);
+            expect(input).toHaveValue(inheritedValues[0].raw_value);
+            expect(helperText).toBeInTheDocument();
+        });
+
+        test("Should allow to clear input when it's override", async () => {
+            let state = getInitialState(false);
+            state = {
+                ...state,
+                ...inheritedOverrideValue,
+                formElement: {...state.formElement, values: inheritedValues}
+            };
+            render(
+                <AntForm>
+                    <AntForm.Item>
+                        <DSInputWrapper
+                            state={state}
+                            infoButton=""
+                            handleSubmit={mockHandleSubmit}
+                            onChange={mockOnChange}
+                            value={inheritedValues[0].raw_value}
+                        />
+                    </AntForm.Item>
+                </AntForm>
+            );
+            const clearButton = screen.getByRole('button');
+            expect(clearButton).toBeInTheDocument();
+
+            await user.click(clearButton);
+
+            expect(mockHandleSubmit).toHaveBeenCalledWith('', 'my_attribute');
         });
     });
 });
