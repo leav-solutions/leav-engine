d5bffb2417b81347261ddf8f44456240364041f1
Arno <arno.tgh@gmail.com>
2024-05-03 17:23:37 +0200

    feat(@leav/ui): manage inherit and override on simple input

--- libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/DSInputWrapper.tsx
+++ libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/DSInputWrapper.tsx
@@ -1,18 +1,30 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {KitInput, KitInputWrapper} from 'aristid-ds';
-import {FocusEvent, FunctionComponent, ReactNode} from 'react';
+import {KitInput} from 'aristid-ds';
+import {ChangeEvent, FocusEvent, FunctionComponent, ReactNode, useState} from 'react';
 import {IStandardFieldReducerState} from '../../../reducers/standardFieldReducer/standardFieldReducer';
 import {Form, InputProps} from 'antd';
-import {RecordFormElementsValueStandardValue} from '_ui/hooks/useGetRecordForm';
 import {IProvidedByAntFormItem} from '_ui/components/RecordEdition/EditRecordContent/_types';
+import styled from 'styled-components';
+import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
 
 interface IDSInputWrapperProps extends IProvidedByAntFormItem<InputProps> {
     state: IStandardFieldReducerState;
     infoButton: ReactNode;
     handleSubmit: (value: string, id?: string) => void;
 }
+
+const KitInputStyled = styled(KitInput)<{$shouldHighlightColor: boolean}>`
+    color: ${({$shouldHighlightColor}) =>
+        $shouldHighlightColor ? 'var(--general-colors-primary-primary400)' : 'initial'};
+
+    .kit-input-wrapper-helper {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+`;
 
 export const DSInputWrapper: FunctionComponent<IDSInputWrapperProps> = ({
     state,
@@ -21,30 +33,60 @@ export const DSInputWrapper: FunctionComponent<IDSInputWrapperProps> = ({
     onChange,
     handleSubmit
 }) => {
+    const {t} = useSharedTranslation();
     const {errors} = Form.Item.useStatus();
     const form = Form.useFormInstance();
+    const [hasChanged, setHasChanged] = useState(false);
 
-    const isRequired = state.formElement.settings.required;
+    const _resetToInheritedValue = () => {
+        setHasChanged(false);
+        form.setFieldValue(state.attribute.id, state.inheritedValue.raw_value);
+        form.validateFields();
+        handleSubmit('', state.attribute.id);
+    };
 
-    const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
-        let valuetoSubmit = e.target.value;
-        if (isRequired && valuetoSubmit === '' && state.formElement.values[0]) {
-            valuetoSubmit = (state.formElement.values[0] as RecordFormElementsValueStandardValue).raw_value;
-            form.setFieldValue(state.attribute.id, valuetoSubmit);
-            form.validateFields();
+    const _handleOnBlur = (event: FocusEvent<HTMLInputElement>) => {
+        const valueToSubmit = event.target.value;
+        if (state.formElement.settings.required && valueToSubmit === '' && state.inheritedValue) {
+            _resetToInheritedValue();
+            return;
         }
-        handleSubmit(valuetoSubmit, state.attribute.id);
+        if (hasChanged || !state.inheritedValue) {
+            handleSubmit(valueToSubmit, state.attribute.id);
+        }
+        onChange(event);
+    };
+
+    const _handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setHasChanged(true);
+        const inputValue = event.target.value;
+        if (state.isInheritedValue && inputValue === '' && event.type === 'click') {
+            _resetToInheritedValue();
+            return;
+        }
+        onChange(event);
     };
 
     return (
-        <KitInputWrapper
+        <KitInputStyled
             label={state.formElement.settings.label}
-            required={isRequired}
+            required={state.formElement.settings.required}
             status={errors.length > 0 ? 'error' : undefined}
             infoIcon={infoButton}
             onInfoClick={Boolean(infoButton) ? () => void 0 : undefined}
-        >
-            <KitInput value={value} onChange={onChange} disabled={state.isReadOnly} onBlur={handleBlur} />
-        </KitInputWrapper>
+            helper={
+                state.isInheritedOverrideValue
+                    ? t('record_edition.inherited_input_helper', {
+                          inheritedValue: state.inheritedValue.raw_value
+                      })
+                    : undefined
+            }
+            value={value}
+            disabled={state.isReadOnly}
+            allowClear={!state.isInheritedNotOverrideValue}
+            onBlur={_handleOnBlur}
+            onChange={_handleOnChange}
+            $shouldHighlightColor={state.isInheritedNotOverrideValue}
+        />
     );
 };
