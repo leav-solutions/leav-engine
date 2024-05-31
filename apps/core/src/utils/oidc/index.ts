1d31f0b97739c5bc2ff5535bb92f48947bf2979f
Arno <arno.tgh@gmail.com>
2024-05-31 17:16:29 +0200

    --wip-- [skip ci]

--- apps/core/src/utils/oidc/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright LEAV Solutions 2017
-// This file is released under LGPL V3
-// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import crypto from 'crypto';
-
-export const generateCodeChallenge = (codeVerifier: string) =>
-    crypto.createHash('sha256').update(codeVerifier).digest('base64url');
