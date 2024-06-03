506c96abac6f1897a36781efad5e20cc5ab7a18c
Arno <arno.tgh@gmail.com>
2024-06-03 12:01:56 +0200

    refacto: adding openid-client for login

---
+++ apps/core/src/infra/oidc/oidcClient.ts
@@ -0,0 +1,17 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {BaseClient, Issuer} from 'openid-client';
+import {IConfig} from '../../_types/config';
+
+export type OidcClient = BaseClient;
+
+export const initOIDCClient = async (config: IConfig): Promise<OidcClient> => {
+    // TODO passer l'url depuis la config
+    const oidcIssuer = await Issuer.discover('http://keycloak:8080/realms/Generic/.well-known/openid-configuration');
+
+    return new oidcIssuer.Client({
+        client_id: config.auth.oidc.clientId,
+        token_endpoint_auth_method: 'none' // see doc here https://backstage.forgerock.com/docs/am/7/oidc1-guide/oidc-client-auth.html
+    });
+};
