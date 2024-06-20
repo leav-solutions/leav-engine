bcd6501d9d8b818ee8b526c6e164de7bd0ad31da
Jules HABLOT <j.hablot-ext@aristid.com>
2024-06-20 11:48:09 +0200

    fix(@leav/ui): avoid reload loop due to oidc

--- libs/ui/src/hooks/useRefreshToken/useRefreshToken.ts
+++ libs/ui/src/hooks/useRefreshToken/useRefreshToken.ts
@@ -1,6 +1,11 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {isDevelopmentHelper} from '_ui/_utils/isDevelopmentHelper';
+
+const _redirectToLogin = () =>
+    window.location.replace(`${window.location.origin}/app/login/?dest=${window.location.pathname}`);
+
 export default function useRefreshToken() {
     const REFRESH_TOKEN_KEY = 'refreshToken';
 
@@ -18,9 +23,12 @@ export default function useRefreshToken() {
             });
 
             // If we didn't get a 2xx response, reload the page and let the back handle auth, unless we explicitly
-            // ask not to reload (eg. in login app)
+            // ask not to reload (e.g. in login app)
             if (!res.ok) {
                 if (autoReload) {
+                    if (isDevelopmentHelper()) {
+                        return _redirectToLogin();
+                    }
                     return window.location.reload();
                 }
 
