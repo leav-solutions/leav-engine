5a758bf1fd7b65d842f26ab675c60244feec7a71
jrmyb <9062561+jrmyb@users.noreply.github.com>
2024-07-22 09:52:56 +0200

    add header leav

--- apps/core/src/infra/db/_constants.ts
+++ apps/core/src/infra/db/_constants.ts
@@ -1,2 +1,5 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 export const ARANGODB_WRITEWRITE_CONFLICT_CODE = 1200;
 export const ARANGODB_HTTP_CONFLICT_CODE = 409;
--- libs/ui/src/components/RecordEdition/EditRecordContent/shared/ValueDetailsBtn/useValueDetailsButton.tsx
+++ libs/ui/src/components/RecordEdition/EditRecordContent/shared/ValueDetailsBtn/useValueDetailsButton.tsx
@@ -1,3 +1,6 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {isTypeStandard} from '@leav/utils';
 import {IRecordPropertyStandard, RecordProperty} from '_ui/_queries/records/getRecordPropertiesQuery';
 import {EditRecordReducerActionsTypes} from '_ui/components/RecordEdition/editRecordReducer/editRecordReducer';
