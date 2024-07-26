2f22d815657013376f7aa1115f20eadbc4af53b3
Jules HABLOT <j.hablot-ext@aristid.com>
2024-07-26 11:00:13 +0200

    chore: add licence header

--- libs/ui/src/components/LibraryItemsList/hooks/useSearchReducer/searchReducer.ts
+++ libs/ui/src/components/LibraryItemsList/hooks/useSearchReducer/searchReducer.ts
@@ -1,3 +1,6 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {GraphQLErrors} from '@apollo/client/errors';
 import getFieldsFromView from '_ui/components/LibraryItemsList/helpers/getFieldsFromView';
 import {IAttribute, IField, IFilter, ISelectedRecord, SearchMode, SidebarContentType} from '_ui/types/search';
