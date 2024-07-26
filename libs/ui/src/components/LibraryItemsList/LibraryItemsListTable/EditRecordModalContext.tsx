2f320880bb434c30e9adb0088e8f1ee036c84a7e
Jules HABLOT <j.hablot-ext@aristid.com>
2024-07-26 11:00:13 +0200

    chore: add licence header

--- libs/ui/src/components/LibraryItemsList/LibraryItemsListTable/EditRecordModalContext.tsx
+++ libs/ui/src/components/LibraryItemsList/LibraryItemsListTable/EditRecordModalContext.tsx
@@ -1,3 +1,6 @@
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {createContext, FunctionComponent, useCallback, useContext, useMemo, useState} from 'react';
 import {EditRecordModal, IEditRecordModalProps} from '_ui/components/RecordEdition/EditRecordModal/EditRecordModal';
 
