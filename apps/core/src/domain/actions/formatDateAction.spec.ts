8d6d98cfe6d9346ece804688b1dbc783129ee9cf
Jules HABLOT <j.hablot-ext@aristid.com>
2024-08-12 16:03:51 +0200

    feat(core): allow options for action date & date range

--- apps/core/src/domain/actions/formatDateAction.spec.ts
+++ apps/core/src/domain/actions/formatDateAction.spec.ts
@@ -9,31 +9,122 @@ describe('formatDateAction', () => {
     const attrText: IAttribute = {id: 'test_attr', format: AttributeFormats.DATE, type: AttributeTypes.SIMPLE};
     const ctx = {attribute: attrText};
 
-    test('formatDate', async () => {
-        expect(
-            (await action([{value: 2119477320}], {auto: 'false', format: 'D/M/YY HH:mm'}, ctx)).values[0].value
-        ).toBeTruthy();
-        expect((await action([{value: 2119477320}], {auto: 'false'}, ctx)).values[0].value).toBeTruthy();
-        expect((await action([{value: 'aaaa'}], {auto: 'false'}, ctx)).values[0].value).toBe('');
-        expect((await action([{value: null}], {auto: 'false'}, ctx)).values[0].value).toBe(null);
+    const testingDate = 2119477320;
+
+    describe('Localized format', () => {
+        test('with options', async () => {
+            const localized = `{
+                "year": "2-digit",
+                "month": "long",
+                "day": "2-digit",
+                "hour": "2-digit"
+            }`;
+
+            expect(
+                (
+                    await action(
+                        [{value: testingDate}],
+                        {localized},
+                        {
+                            ...ctx,
+                            lang: 'en-GB'
+                        }
+                    )
+                ).values[0].value
+            ).toBe('28 February 37 at 23');
+            expect(
+                (
+                    await action(
+                        [{value: testingDate}],
+                        {localized},
+                        {
+                            ...ctx,
+                            lang: 'fr-FR'
+                        }
+                    )
+                ).values[0].value
+            ).toBe('28 février 37 à 23 h');
+            expect(
+                (
+                    await action(
+                        [{value: testingDate}],
+                        {localized},
+                        {
+                            ...ctx,
+                            lang: 'ko-KR'
+                        }
+                    )
+                ).values[0].value
+            ).toBe('37년 2월 28일 오후 11시');
+        });
+
+        // TODO: rise error to inform user without break app
+        test.skip.each(['{', '{withoutDoubleQuote: true}', '', '{"params1": "long", "param2": "too many coma",}'])(
+            'auto should print default on invalid json format: `%s`',
+            async localized => {
+                const result = await action(
+                    [{value: testingDate}],
+                    {localized},
+                    {
+                        ...ctx,
+                        lang: 'en-EN'
+                    }
+                );
+                expect(result.values[0].value).toBe('2/28/2037, 11:42:00 PM');
+                expect(result.errors[0]).toEqual({
+                    attributeValue: {value: localized},
+                    errorType: 'FORMAT_ERROR',
+                    message: 'Params "localized" of FormatDateAction are invalid JSON. Use `{}` empty option instead.'
+                });
+            }
+        );
     });
 
-    test('auto', async () => {
-        expect((await action([{value: 2119477320}], {auto: 'true'}, {...ctx, lang: 'ko-KR'})).values[0].value).toBe(
-            '2037. 2. 28. 오후 11:42:00'
-        );
-        expect((await action([{value: 2119477320}], {auto: 'true'}, {...ctx, lang: 'fr-FR'})).values[0].value).toBe(
-            '28/02/2037 23:42:00'
-        );
-        expect((await action([{value: 2119477320}], {auto: 'true'}, {...ctx, lang: 'ar-EG'})).values[0].value).toBe(
-            '٢٨‏/٢‏/٢٠٣٧، ١١:٤٢:٠٠ م'
-        );
+    test('Universal format', async () => {
+        expect(
+            (
+                await action(
+                    [{value: testingDate}],
+                    {
+                        universal: 'D/MMMM-YY HH:mm'
+                    },
+                    ctx
+                )
+            ).values[0].value
+        ).toBe('28/February-37 23:42');
     });
 
-    test('auto override formatDate', async () => {
+    describe('edge cases', () => {
+        test('should fallback to empty localized param when neither params provided', async () => {
+            const resultWithoutParams = await action([{value: testingDate}], {}, ctx);
+            const resultWithEmptyLocalizedParam = await action([{value: testingDate}], {localized: '{}'}, ctx);
+            expect(resultWithoutParams).toEqual(resultWithEmptyLocalizedParam);
+        });
+        test('should return empty string on non numerical value in DB', async () => {
+            expect((await action([{value: 'aaaa'}], {}, ctx)).values[0].value).toBe('');
+        });
+        test('should return null on null value in DB', async () => {
+            expect((await action([{value: null}], {}, ctx)).values[0].value).toBe(null);
+        });
+    });
+
+    test('localized override universal format', async () => {
         expect(
-            (await action([{value: 2119477320}], {format: 'D/M/YY', auto: 'true'}, {...ctx, lang: 'fr-FR'})).values[0]
-                .value
-        ).toBe('28/02/2037 23:42:00');
+            (
+                await action(
+                    [{value: testingDate}],
+                    {
+                        universal: 'D/MMMM/YY',
+                        localized: `{
+                            "weekday": "long",
+                            "era": "short",
+                            "month": "narrow",
+                            "day": "numeric"
+                        }`
+                    },
+                    {...ctx, lang: 'fr-FR'}
+                )
+            ).values[0].value
+        ).toBe('ap. J.-C. samedi 28 F');
     });
 });
