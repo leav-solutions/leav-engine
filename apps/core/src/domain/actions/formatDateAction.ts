69df25f9d1ac4fe9a35b0d8f5f3c1cc5702a7dd5
Jules HABLOT <j.hablot-ext@aristid.com>
2024-08-12 16:03:51 +0200

    feat(core): allow options for action date & date range

--- apps/core/src/domain/actions/formatDateAction.ts
+++ apps/core/src/domain/actions/formatDateAction.ts
@@ -2,9 +2,17 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import moment from 'moment';
-import {ActionsListIOTypes, IActionsListFunction} from '../../_types/actionsList';
+import {ActionsListIOTypes, IActionsListFunction, IActionsListFunctionResult} from '../../_types/actionsList';
+import {Errors} from '../../_types/errors';
 
-export default function (): IActionsListFunction<{auto: true; format: false}> {
+const defaultValueLocalizedParam = `{
+  "weekday": "long",
+  "month": "long",
+  "day": "numeric",
+  "year": "numeric"
+}`;
+
+export default function (): IActionsListFunction<{localized: false; universal: false}> {
     return {
         id: 'formatDate',
         name: 'Format Date',
@@ -13,41 +21,59 @@ export default function (): IActionsListFunction<{auto: true; format: false}> {
         output_types: [ActionsListIOTypes.STRING],
         params: [
             {
-                name: 'auto',
-                type: 'boolean',
-                description: 'Adapt format to current language',
-                required: true,
-                default_value: 'false'
-            },
-            {
-                name: 'format',
+                name: 'localized',
                 type: 'string',
                 description:
-                    'Date format. Available format: https://momentjs.com/docs/#/displaying/format/. If "auto" is flagged to true, this parameter is ignored.',
+                    'Adapt format to current language. Available options: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString#options.',
+                required: false,
+                default_value: defaultValueLocalizedParam
+            },
+            {
+                name: 'universal',
+                type: 'string',
+                description:
+                    'Date format for every languages. If "localized" parameter is defined, this parameter is ignored. Available formats: https://momentjs.com/docs/#/displaying/format/.',
                 required: false,
                 default_value: 'DD/MM/YYYY HH:mm:ss'
             }
         ],
-        action: (values, {auto, format}, {lang}) => {
-            const computedValues = values.map(elementValue => {
+        action: (values, {localized, universal}, {lang}) => {
+            const errors: IActionsListFunctionResult['errors'] = [];
+
+            const formattedValues = values.map(elementValue => {
                 if (elementValue.value === null) {
                     return elementValue;
                 }
                 const numberVal = Number(elementValue.value);
 
-                let newValue = '';
-
-                if (!isNaN(numberVal)) {
-                    newValue =
-                        auto === 'true'
-                            ? new Date(numberVal * 1_000).toLocaleString(lang)
-                            : moment.unix(numberVal).format(format ?? '');
+                if (isNaN(numberVal)) {
+                    elementValue.value = '';
+                    return elementValue;
                 }
-                elementValue.value = newValue;
+
+                if ((localized === null || localized === undefined) && universal) {
+                    elementValue.value = moment.unix(numberVal).format(universal); // TODO: replace moment by dayjs
+                    return elementValue;
+                }
+
+                let options: Intl.DateTimeFormatOptions = {};
+                try {
+                    options = JSON.parse(localized ?? {});
+                } catch (e) {
+                    // TODO: rise error to inform user without break app
+                    errors.push({
+                        errorType: Errors.FORMAT_ERROR,
+                        attributeValue: {value: localized},
+                        message:
+                            'Params "localized" of FormatDateAction are invalid JSON. Use `{}` empty option instead.'
+                    });
+                }
+
+                elementValue.value = new Date(numberVal * 1_000).toLocaleString(lang, options);
                 return elementValue;
             });
 
-            return {values: computedValues, errors: []};
+            return {values: formattedValues, errors: []};
         }
     };
 }
--- apps/core/src/domain/actions/formatDateRangeAction.spec.ts
+++ apps/core/src/domain/actions/formatDateRangeAction.spec.ts
@@ -1,7 +1,6 @@
 // Copyright LEAV Solutions 2017
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {IActionsListFunctionResult} from '_types/actionsList';
 import {AttributeFormats, AttributeTypes, IAttribute} from '../../_types/attribute';
 import formatDateRangeAction from './formatDateRangeAction';
 
@@ -10,64 +9,114 @@ describe('formatDateRangeAction', () => {
     const attrText: IAttribute = {id: 'test_attr', format: AttributeFormats.DATE_RANGE, type: AttributeTypes.SIMPLE};
     const ctx = {attribute: attrText};
 
-    test('formatDateRange', async () => {
-        const formattedDateResult = action(
-            [{value: {from: '2119477320', to: '2119477380'}}],
-            {auto: false, format: 'D/M/YY HH:mm'},
-            ctx
-        ) as IActionsListFunctionResult;
-        const formattedDate = formattedDateResult.values[0].value as {
+    const testingRangeDate = {from: '2119477320', to: '2119477380'};
+
+    describe('Localized format', () => {
+        test('with options', async () => {
+            const localized = `{
+                "year": "2-digit",
+                "month": "long",
+                "day": "2-digit",
+                "hour": "2-digit",
+                "minute": "2-digit"
+            }`;
+
+            expect(
+                (
+                    await action(
+                        [{value: testingRangeDate}],
+                        {localized},
+                        {
+                            ...ctx,
+                            lang: 'en-GB'
+                        }
+                    )
+                ).values[0].value
+            ).toEqual({from: '28 February 37 at 23:42', to: '28 February 37 at 23:43'});
+            expect(
+                (
+                    await action(
+                        [{value: testingRangeDate}],
+                        {localized},
+                        {
+                            ...ctx,
+                            lang: 'ko-KR'
+                        }
+                    )
+                ).values[0].value
+            ).toEqual({from: '37년 2월 28일 오후 11:42', to: '37년 2월 28일 오후 11:43'});
+        });
+
+        test.skip.each(['{', '{withoutDoubleQuote: true}', '', '{"params1": "long", "param2": "too many coma",}'])(
+            'auto should print default on invalid json format: `%s`',
+            async localized => {
+                const result = await action(
+                    [{value: testingRangeDate}],
+                    {localized},
+                    {
+                        ...ctx,
+                        lang: 'en-EN'
+                    }
+                );
+                expect(result.values[0].value).toEqual({from: '2/28/2037, 11:42:00 PM', to: '2/28/2037, 11:43:00 PM'});
+                expect(result.errors[0]).toEqual({
+                    attributeValue: {value: localized},
+                    errorType: 'FORMAT_ERROR',
+                    message: 'Params "localized" of FormatDateAction are invalid JSON. Use `{}` empty option instead.'
+                });
+            }
+        );
+    });
+
+    test('Universal format', async () => {
+        const result = await action([{value: testingRangeDate}], {universal: 'D/MMMM-YY HH:mm'}, ctx);
+        const formattedRangeDate = result.values[0].value as {
             from: string;
             to: string;
         };
-        expect(formattedDate.from).toBeTruthy();
-        expect(formattedDate.to).toBeTruthy();
-
-        const formattedDateNoFormatResult = action(
-            [{value: {from: '2119477320', to: '2119477380'}}],
-            {auto: false, format: 'D/M/YY HH:mm'},
-            ctx
-        ) as IActionsListFunctionResult;
-        const formattedDateNoFormat = formattedDateNoFormatResult.values[0].value as {
-            from: string;
-            to: string;
-        };
-        expect(formattedDateNoFormat.from).toBeTruthy();
-        expect(formattedDateNoFormat.to).toBeTruthy();
-        expect((await action([{value: 'aaaa'}], {auto: false, format: null}, ctx)).values[0].value).toBe(null);
-        expect((await action([{value: {from: '2119477320'}}], {auto: false, format: null}, ctx)).values[0].value).toBe(
-            null
-        );
-        expect((await action([{value: {to: '2119477320'}}], {auto: false, format: null}, ctx)).values[0].value).toBe(
-            null
-        );
-        expect((await action([{value: null}], {auto: false, format: null}, ctx)).values[0].value).toBe(null);
+        expect(formattedRangeDate.from).toBe('28/February-37 23:42');
+        expect(formattedRangeDate.to).toBe('28/February-37 23:43');
     });
 
-    test('auto', async () => {
-        expect(
-            (await action([{value: {from: '2119477320', to: '2119477380'}}], {auto: 'true'}, {...ctx, lang: 'ko-KR'}))
-                .values[0].value
-        ).toEqual({from: '2037. 2. 28. 오후 11:42:00', to: '2037. 2. 28. 오후 11:43:00'});
-        expect(
-            (await action([{value: {from: '2119477320', to: '2119477380'}}], {auto: 'true'}, {...ctx, lang: 'fr-FR'}))
-                .values[0].value
-        ).toEqual({from: '28/02/2037 23:42:00', to: '28/02/2037 23:43:00'});
-        expect(
-            (await action([{value: {from: '2119477320', to: '2119477380'}}], {auto: 'true'}, {...ctx, lang: 'ar-EG'}))
-                .values[0].value
-        ).toEqual({from: '٢٨‏/٢‏/٢٠٣٧، ١١:٤٢:٠٠ م', to: '٢٨‏/٢‏/٢٠٣٧، ١١:٤٣:٠٠ م'});
+    describe('edge cases', () => {
+        test('should return null value if properties are omitted', async () => {
+            expect((await action([{value: 'aaaa'}], {}, ctx)).values[0].value).toBe(null);
+            expect((await action([{value: {}}], {}, ctx)).values[0].value).toBe(null);
+            expect((await action([{value: {unknownProperty: null}}], {}, ctx)).values[0].value).toBe(null);
+            expect((await action([{value: {from: '2119477320'}}], {}, ctx)).values[0].value).toBe(null);
+            expect((await action([{value: {to: '2119477320'}}], {}, ctx)).values[0].value).toBe(null);
+            expect((await action([{value: null}], {}, ctx)).values[0].value).toBe(null);
+        });
+        test('should return empty string couple on non numerical value in DB', async () => {
+            expect((await action([{value: {from: 'aaaa', to: '2119477320'}}], {}, ctx)).values[0].value).toEqual([
+                '',
+                ''
+            ]);
+            expect((await action([{value: {from: '2119477320', to: 'aaaa'}}], {}, ctx)).values[0].value).toEqual([
+                '',
+                ''
+            ]);
+        });
     });
 
-    test('auto override formatDate', async () => {
+    test('localized override universal format', async () => {
         expect(
             (
                 await action(
                     [{value: {from: '2119477320', to: '2119477380'}}],
-                    {format: 'D/M/YY', auto: 'true'},
+                    {
+                        universal: 'D/MMMM/YY',
+                        localized: `{
+                            "weekday": "long",
+                            "era": "short",
+                            "month": "narrow",
+                            "day": "numeric",
+                            "minute": "2-digit"
+                        }`
+                    },
                     {...ctx, lang: 'fr-FR'}
                 )
             ).values[0].value
-        ).toEqual({from: '28/02/2037 23:42:00', to: '28/02/2037 23:43:00'});
+        ).toEqual({from: 'ap. J.-C. samedi 28 F 42', to: 'ap. J.-C. samedi 28 F 43'});
     });
 });
