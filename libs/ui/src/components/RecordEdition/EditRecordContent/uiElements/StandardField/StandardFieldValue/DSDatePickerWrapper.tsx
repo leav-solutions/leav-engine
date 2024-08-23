b02a3efe3b8db50415b0f0a9c16f1d806768f22b
Renaud AMSELLEM <r.amsellem-ext@aristid.com>
2024-08-23 15:59:42 +0200

    fix(leav-ui): adapt datepicker to design system

---
+++ libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/DSDatePickerWrapper.tsx
@@ -0,0 +1,103 @@
+import {KitDatePicker} from 'aristid-ds';
+import {FunctionComponent, ReactNode} from 'react';
+import {
+    IStandardFieldReducerState,
+    IStandardFieldValue
+} from '../../../reducers/standardFieldReducer/standardFieldReducer';
+import {Form} from 'antd';
+import dayjs from 'dayjs';
+import styled from 'styled-components';
+import {IProvidedByAntFormItem, StandardValueTypes} from '../../../_types';
+import {DatePickerProps} from 'antd/lib/date-picker';
+import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
+import {RecordFormAttributeFragment} from '_ui/_gqlTypes';
+import {useValueDetailsButton} from '_ui/components/RecordEdition/EditRecordContent/shared/ValueDetailsBtn/useValueDetailsButton';
+import {useLang} from '_ui/hooks';
+import {localizedTranslation} from '@leav/utils';
+
+interface IDSDatePickerWrapperProps extends IProvidedByAntFormItem<DatePickerProps> {
+    state: IStandardFieldReducerState;
+    attribute: RecordFormAttributeFragment;
+    fieldValue: IStandardFieldValue;
+    shouldShowValueDetailsButton?: boolean;
+    handleSubmit: (value: StandardValueTypes, id?: string) => void;
+}
+
+const KitDatePickerStyled = styled(KitDatePicker)<{$shouldHighlightColor: boolean}>`
+    color: ${({$shouldHighlightColor}) => ($shouldHighlightColor ? 'var(--general-colors-primary-400)' : 'initial')};
+    .kit-input-wrapper-helper {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+`;
+
+export const DSDatePickerWrapper: FunctionComponent<IDSDatePickerWrapperProps> = ({
+    value,
+    onChange,
+    state,
+    attribute,
+    fieldValue,
+    shouldShowValueDetailsButton = false,
+    handleSubmit
+}) => {
+    const {t} = useSharedTranslation();
+    const {lang: availableLangs} = useLang();
+    const {errors} = Form.Item.useStatus();
+    const {onValueDetailsButtonClick, infoIconWithTooltip} = useValueDetailsButton({
+        value: fieldValue?.value,
+        attribute
+    });
+
+    const _handleDateChange: (datePickerDate: dayjs.Dayjs | null, antOnChangeParams: string | string[]) => void = (
+        datePickerDate,
+        ...antOnChangeParams
+    ) => {
+        if (state.isInheritedValue && datePickerDate === null) {
+            onChange(dayjs.unix(Number(state.inheritedValue.raw_value)), state.inheritedValue.raw_value);
+            handleSubmit('', state.attribute.id);
+            return;
+        }
+
+        onChange(datePickerDate, ...antOnChangeParams);
+
+        // TODO : validate form with await form.validateFields(state.attribute.id)
+        if (state.formElement.settings.required && datePickerDate === null) {
+            return;
+        }
+
+        let dateToSave = null;
+        if (datePickerDate !== null) {
+            dateToSave = String(datePickerDate.unix());
+        }
+
+        handleSubmit(dateToSave, state.attribute.id);
+    };
+
+    const label = localizedTranslation(state.formElement.settings.label, availableLangs);
+
+    return (
+        <KitDatePickerStyled
+            value={value}
+            onChange={_handleDateChange}
+            label={label}
+            required={state.formElement.settings.required}
+            disabled={state.isReadOnly}
+            allowClear={!state.isInheritedNotOverrideValue}
+            status={errors.length > 0 ? 'error' : undefined}
+            infoIcon={shouldShowValueDetailsButton ? infoIconWithTooltip : null}
+            onInfoClick={shouldShowValueDetailsButton ? onValueDetailsButtonClick : null}
+            helper={
+                state.isInheritedOverrideValue
+                    ? t('record_edition.inherited_input_helper', {
+                          inheritedValue: t('record_edition.date_range_from_to', {
+                              from: state.inheritedValue.value.from,
+                              to: state.inheritedValue.value.to
+                          })
+                      })
+                    : undefined
+            }
+            $shouldHighlightColor={state.isInheritedNotOverrideValue}
+        />
+    );
+};
