bcdd6121262d280f36f3bbdd6075b833f666cab2
Teddy <t.paul@cacom.fr>
2024-10-16 12:05:09 +0200

    review fix

--- libs/ui/src/_utils/typeguards.ts
+++ libs/ui/src/_utils/typeguards.ts
@@ -1,3 +1,3 @@
 import {WithTypename} from '@leav/utils';
 
-export const hasTypename = <T>(value: any): value is WithTypename<T> => !!value.__typename;
+export const hasTypename = <T>(value: any): value is WithTypename<T> => '__typename' in value;
