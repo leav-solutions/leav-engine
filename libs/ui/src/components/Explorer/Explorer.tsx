4bc3a565d36719f5f3cca4dd09a3051c366c0880
Jules HABLOT <j.hablot-ext@aristid.com>
2024-10-21 11:40:51 +0200

    feat(@leav/ui): split behavior in explorer

--- libs/ui/src/components/Explorer/Explorer.tsx
+++ libs/ui/src/components/Explorer/Explorer.tsx
@@ -1,5 +1,18 @@
-import {FunctionComponent} from 'react';
+// Copyright LEAV Solutions 2017
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {FunctionComponent, useState} from 'react';
+import {IDataGroupedFilteredSorted} from '_ui/components/Explorer/types';
+import {DataView} from './DataView';
 
-export const Explorer: FunctionComponent<{library: string}> = ({library}) => (
-    <div>This is the Explorer! on library {library}</div>
-);
+export const Explorer: FunctionComponent<{library: string}> = ({library}) => {
+    // TODO: make API call
+    const [data, setData] = useState<IDataGroupedFilteredSorted[] | null>(null);
+
+    return (
+        <div>
+            This is the Explorer! on library {library}
+            {data === null ? 'Loading...' : <DataView data={data} />}
+        </div>
+    );
+};
