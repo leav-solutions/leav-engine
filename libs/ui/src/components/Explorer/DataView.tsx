24f63f42957b8ca7d425579bf5d43273f13ed537
Jules HABLOT <j.hablot-ext@aristid.com>
2024-10-24 11:07:37 +0200

    feat(explorer): display data as table from DS

--- libs/ui/src/components/Explorer/DataView.tsx
+++ libs/ui/src/components/Explorer/DataView.tsx
@@ -2,22 +2,39 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {FunctionComponent} from 'react';
-import {KitIdCard, KitTag} from 'aristid-ds';
-import {IDataGroupedFilteredSorted} from './types';
+import {IDataGroupedFilteredSorted} from '_ui/components/Explorer/types';
+import {KitIdCard, KitTable} from 'aristid-ds';
+import {KitTableColumnType} from 'aristid-ds/dist/Kit/DataDisplay/Table/types';
+import {IKitAvatar} from 'aristid-ds/dist/Kit/DataDisplay/Avatar/types';
 
-export const DataView: FunctionComponent<{dataGroupedFilteredSorted: IDataGroupedFilteredSorted<'whoAmI'>}> = ({
-    dataGroupedFilteredSorted
-}) => (
-    <ul>
-        {dataGroupedFilteredSorted.map(({itemId, value}) => (
-            <li key={itemId} style={{display: 'flex', gap: '16px', alignItems: 'center', marginBottom: '8px'}}>
-                <KitTag idCardProps={{description: itemId}} /> |
-                <KitIdCard
-                    avatarProps={{label: value.label ?? '-'}}
-                    title={value.label ?? '-'}
-                    description={value.subLabel ?? '-'}
-                />
-            </li>
-        ))}
-    </ul>
+const _getIdCard = ({
+    id,
+    label,
+    library,
+    preview,
+    subLabel
+}: IDataGroupedFilteredSorted<'whoAmI'>[number]['value']): ReturnType<typeof KitIdCard> => {
+    const avatarProps: IKitAvatar = {label: label ?? undefined};
+
+    if (preview) {
+        avatarProps.src = preview.small;
+    }
+
+    return <KitIdCard avatarProps={avatarProps} title={label ?? id} description={subLabel ?? library.id} />;
+};
+
+const _getColumns = (
+    attributesToDisplay: string[]
+): Array<KitTableColumnType<IDataGroupedFilteredSorted<'whoAmI'>[number]>> =>
+    attributesToDisplay.map(attributeName => ({
+        title: attributeName,
+        dataIndex: attributeName,
+        render: (text, {value}) => (attributeName === 'whoAmI' ? _getIdCard(value) : text)
+    }));
+
+export const DataView: FunctionComponent<{
+    dataGroupedFilteredSorted: IDataGroupedFilteredSorted<'whoAmI'>;
+    attributesToDisplay: string[];
+}> = ({dataGroupedFilteredSorted, attributesToDisplay}) => (
+    <KitTable columns={_getColumns(attributesToDisplay)} pagination={false} dataSource={dataGroupedFilteredSorted} />
 );
