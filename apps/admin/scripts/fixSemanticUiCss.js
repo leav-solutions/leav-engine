6c28fbf123aba0b542743aede2ca932d55282827
Teddy <t.paul@cacom.fr>
2024-11-04 16:20:40 +0100

    chore: update license header

--- apps/admin/scripts/fixSemanticUiCss.js
+++ apps/admin/scripts/fixSemanticUiCss.js
@@ -1,4 +1,4 @@
-// Copyright LEAV Solutions 2017
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 // Remove a double semicolon in the Semantic UI CSS which makes build in webpack 5 fail
--- apps/core/src/__tests__/mocks/library.ts
+++ apps/core/src/__tests__/mocks/library.ts
@@ -1,4 +1,4 @@
-// Copyright LEAV Solutions 2017
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {ILibrary} from '_types/library';
