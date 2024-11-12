1028fabbcfbfb9b8b604014219b2e4102d35be13
Renaud AMSELLEM <r.amsellem-ext@aristid.com>
2024-11-12 14:34:53 +0100

    feat(leav/ui): opened values list selection

--- libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/ValuesList/MonoValueSelect.test.tsx
+++ libs/ui/src/components/RecordEdition/EditRecordContent/uiElements/StandardField/StandardFieldValue/ValuesList/MonoValueSelect.test.tsx
@@ -8,7 +8,7 @@ import {mockAttributeSimple, mockFormAttribute} from '_ui/__mocks__/common/attri
 import {mockRecord} from '_ui/__mocks__/common/record';
 import {AntForm} from 'aristid-ds';
 import userEvent from '@testing-library/user-event';
-import {RecordFormAttributeStandardAttributeFragment, SortOrder} from '_ui/_gqlTypes';
+import {RecordFormAttributeStandardAttributeFragment} from '_ui/_gqlTypes';
 import {VersionFieldScope} from '_ui/components/RecordEdition/EditRecordContent/_types';
 import {
     CalculatedFlags,
@@ -28,12 +28,14 @@ describe('<MonoValueSelect />', () => {
         permissions: {access_attribute: true, edit_value: true}
     };
 
+    const valuesList = {
+        enable: true,
+        values: ['green', 'yellow', 'foudre']
+    };
+
     const attribute = {
         ...commonAttribute,
-        values_list: {
-            enable: true,
-            values: ['green', 'yellow', 'foudre']
-        }
+        values_list: valuesList
     } satisfies RecordFormAttributeStandardAttributeFragment;
 
     const state = {
@@ -80,40 +82,42 @@ describe('<MonoValueSelect />', () => {
         handleBlurMock.mockClear();
     });
 
-    it('should throw an error when displayed not inside an Ant Form', async () => {
-        expectToThrow(
-            () =>
-                render(
-                    <MonoValueSelect
-                        attribute={commonAttribute}
-                        state={state}
-                        fieldValue={fieldValue}
-                        handleSubmit={handleSubmitMock}
-                        handleBlur={handleBlurMock}
-                    />
-                ),
-            'MonoValueSelect should be used inside a antd Form.Item'
-        );
-    });
+    describe('errors', () => {
+        it('should throw an error when displayed not inside an Ant Form', async () => {
+            expectToThrow(
+                () =>
+                    render(
+                        <MonoValueSelect
+                            attribute={commonAttribute}
+                            state={state}
+                            fieldValue={fieldValue}
+                            handleSubmit={handleSubmitMock}
+                            handleBlur={handleBlurMock}
+                        />
+                    ),
+                'MonoValueSelect should be used inside a antd Form.Item'
+            );
+        });
 
-    it('should throw an error when displayed without values_list in attribute', async () => {
-        expectToThrow(
-            () =>
-                render(
-                    <AntForm name="name">
-                        <AntForm.Item name="chartreuse">
-                            <MonoValueSelect
-                                attribute={commonAttribute}
-                                state={state}
-                                fieldValue={fieldValue}
-                                handleSubmit={handleSubmitMock}
-                                handleBlur={handleBlurMock}
-                            />
-                        </AntForm.Item>
-                    </AntForm>
-                ),
-            'MonoValueSelect should have a values list'
-        );
+        it('should throw an error when displayed without values_list in attribute', async () => {
+            expectToThrow(
+                () =>
+                    render(
+                        <AntForm name="name">
+                            <AntForm.Item name="chartreuse">
+                                <MonoValueSelect
+                                    attribute={commonAttribute}
+                                    state={state}
+                                    fieldValue={fieldValue}
+                                    handleSubmit={handleSubmitMock}
+                                    handleBlur={handleBlurMock}
+                                />
+                            </AntForm.Item>
+                        </AntForm>
+                    ),
+                'MonoValueSelect should have a values list'
+            );
+        });
     });
 
     it('should call handleSubmit on clear', async () => {
@@ -147,170 +151,204 @@ describe('<MonoValueSelect />', () => {
         expect(handleSubmitMock).toHaveBeenCalledWith('', attribute.id);
     });
 
-    it.each`
-        calculatedValue | inheritedValue | displayedValue
-        ${'calculated'} | ${null}        | ${'calculated'}
-        ${null}         | ${'inherited'} | ${'inherited'}
-        ${'calculated'} | ${'inherited'} | ${'inherited'}
-    `(
-        'should display calculated / inherited value in helper',
-        ({
-            calculatedValue,
-            inheritedValue,
-            displayedValue
-        }: {
-            calculatedValue: string | null;
-            inheritedValue: string | null;
-            displayedValue: string;
-        }) => {
-            const stateWithInheritedValue: InheritedFlags = {
-                inheritedValue: {raw_value: inheritedValue},
-                isInheritedValue: true,
-                isInheritedNotOverrideValue: false,
-                isInheritedOverrideValue: true
-            };
-            const stateWithCalculatedValue: CalculatedFlags = {
-                calculatedValue: {raw_value: calculatedValue},
-                isCalculatedValue: true,
-                isCalculatedNotOverrideValue: false,
-                isCalculatedOverrideValue: true
-            };
+    describe('calulated & inherited values', () => {
+        it.each`
+            calculatedValue | inheritedValue | displayedValue
+            ${'calculated'} | ${null}        | ${'calculated'}
+            ${null}         | ${'inherited'} | ${'inherited'}
+            ${'calculated'} | ${'inherited'} | ${'inherited'}
+        `(
+            'should display calculated / inherited value in helper',
+            ({
+                calculatedValue,
+                inheritedValue,
+                displayedValue
+            }: {
+                calculatedValue: string | null;
+                inheritedValue: string | null;
+                displayedValue: string;
+            }) => {
+                const stateWithInheritedValue: InheritedFlags = {
+                    inheritedValue: {raw_value: inheritedValue},
+                    isInheritedValue: true,
+                    isInheritedNotOverrideValue: false,
+                    isInheritedOverrideValue: true
+                };
+                const stateWithCalculatedValue: CalculatedFlags = {
+                    calculatedValue: {raw_value: calculatedValue},
+                    isCalculatedValue: true,
+                    isCalculatedNotOverrideValue: false,
+                    isCalculatedOverrideValue: true
+                };
 
-            let newState = state;
-            if (inheritedValue) {
-                newState = {...newState, ...stateWithInheritedValue} as any;
+                let newState = state;
+                if (inheritedValue) {
+                    newState = {...newState, ...stateWithInheritedValue} as any;
+                }
+
+                if (calculatedValue) {
+                    newState = {...newState, ...stateWithCalculatedValue} as any;
+                }
+
+                render(
+                    <AntForm name="name">
+                        <AntForm.Item name="chartreuse">
+                            <MonoValueSelect
+                                attribute={attribute}
+                                state={newState}
+                                fieldValue={{...fieldValue, isEditing: true}}
+                                handleSubmit={handleSubmitMock}
+                                handleBlur={handleBlurMock}
+                            />
+                        </AntForm.Item>
+                    </AntForm>
+                );
+
+                expect(screen.getByText(new RegExp(`${displayedValue}`))).toBeVisible();
             }
-
-            if (calculatedValue) {
-                newState = {...newState, ...stateWithCalculatedValue} as any;
-            }
-
-            render(
-                <AntForm name="name">
-                    <AntForm.Item name="chartreuse">
-                        <MonoValueSelect
-                            attribute={attribute}
-                            state={newState}
-                            fieldValue={{...fieldValue, isEditing: true}}
-                            handleSubmit={handleSubmitMock}
-                            handleBlur={handleBlurMock}
-                        />
-                    </AntForm.Item>
-                </AntForm>
-            );
-
-            expect(screen.getByText(new RegExp(`${displayedValue}`))).toBeVisible();
-        }
-    );
-
-    it.each`
-        calculatedValue | inheritedValue | displayedValue
-        ${'calculated'} | ${null}        | ${'calculated'}
-        ${null}         | ${'inherited'} | ${'inherited'}
-        ${'calculated'} | ${'inherited'} | ${'inherited'}
-    `(
-        'should revert to calculated / inherited value on click on clear icon',
-        async ({
-            calculatedValue,
-            inheritedValue,
-            displayedValue
-        }: {
-            calculatedValue: string | null;
-            inheritedValue: string | null;
-            displayedValue: string;
-        }) => {
-            const stateWithInheritedValue: InheritedFlags = {
-                inheritedValue: {raw_value: inheritedValue},
-                isInheritedValue: true,
-                isInheritedNotOverrideValue: true,
-                isInheritedOverrideValue: false
-            };
-            const stateWithCalculatedValue: CalculatedFlags = {
-                calculatedValue: {raw_value: calculatedValue},
-                isCalculatedValue: true,
-                isCalculatedNotOverrideValue: true,
-                isCalculatedOverrideValue: false
-            };
-
-            let newState = state;
-            if (inheritedValue) {
-                newState = {...newState, ...stateWithInheritedValue} as any;
-            }
-
-            if (calculatedValue) {
-                newState = {...newState, ...stateWithCalculatedValue} as any;
-            }
-
-            render(
-                <AntForm name="name">
-                    <AntForm.Item name="chartreuse">
-                        <MonoValueSelect
-                            attribute={attribute}
-                            state={newState}
-                            fieldValue={{...fieldValue, isEditing: true}}
-                            handleSubmit={handleSubmitMock}
-                            handleBlur={handleBlurMock}
-                        />
-                    </AntForm.Item>
-                </AntForm>
-            );
-
-            expect(screen.queryByText(displayedValue)).not.toBeInTheDocument();
-            const select = screen.getByRole('combobox');
-            await userEvent.click(select);
-
-            const green = screen.getAllByText('green').pop();
-            await userEvent.click(green);
-
-            const clearIcon = screen.getByLabelText('clear');
-            await userEvent.click(clearIcon);
-
-            expect(screen.getByTitle(displayedValue)).toBeVisible();
-        }
-    );
-
-    it('should display a specific text on search with results', async () => {
-        render(
-            <AntForm name="name">
-                <AntForm.Item name="chartreuse">
-                    <MonoValueSelect
-                        attribute={attribute}
-                        state={state}
-                        fieldValue={{...fieldValue, isEditing: true}}
-                        handleSubmit={handleSubmitMock}
-                        handleBlur={handleBlurMock}
-                    />
-                </AntForm.Item>
-            </AntForm>
         );
 
-        const select = screen.getByRole('combobox');
-        await userEvent.click(select);
-        await userEvent.type(select, 'r');
+        it.each`
+            calculatedValue | inheritedValue | displayedValue
+            ${'calculated'} | ${null}        | ${'calculated'}
+            ${null}         | ${'inherited'} | ${'inherited'}
+            ${'calculated'} | ${'inherited'} | ${'inherited'}
+        `(
+            'should revert to calculated / inherited value on click on clear icon',
+            async ({
+                calculatedValue,
+                inheritedValue,
+                displayedValue
+            }: {
+                calculatedValue: string | null;
+                inheritedValue: string | null;
+                displayedValue: string;
+            }) => {
+                const stateWithInheritedValue: InheritedFlags = {
+                    inheritedValue: {raw_value: inheritedValue},
+                    isInheritedValue: true,
+                    isInheritedNotOverrideValue: true,
+                    isInheritedOverrideValue: false
+                };
+                const stateWithCalculatedValue: CalculatedFlags = {
+                    calculatedValue: {raw_value: calculatedValue},
+                    isCalculatedValue: true,
+                    isCalculatedNotOverrideValue: true,
+                    isCalculatedOverrideValue: false
+                };
 
-        expect(screen.getByText('record_edition.press_enter_to')).toBeVisible();
-        expect(screen.getByText('record_edition.select_this_value')).toBeVisible();
+                let newState = state;
+                if (inheritedValue) {
+                    newState = {...newState, ...stateWithInheritedValue} as any;
+                }
+
+                if (calculatedValue) {
+                    newState = {...newState, ...stateWithCalculatedValue} as any;
+                }
+
+                render(
+                    <AntForm name="name">
+                        <AntForm.Item name="chartreuse">
+                            <MonoValueSelect
+                                attribute={attribute}
+                                state={newState}
+                                fieldValue={{...fieldValue, isEditing: true}}
+                                handleSubmit={handleSubmitMock}
+                                handleBlur={handleBlurMock}
+                            />
+                        </AntForm.Item>
+                    </AntForm>
+                );
+
+                expect(screen.getByTestId(attribute.id).innerHTML).not.toMatch(new RegExp(displayedValue));
+                const select = screen.getByRole('combobox');
+                await userEvent.click(select);
+
+                const green = screen.getAllByText('green').pop();
+                await userEvent.click(green);
+
+                const clearIcon = screen.getByLabelText('clear');
+                await userEvent.click(clearIcon);
+
+                expect(screen.getByTestId(attribute.id).innerHTML).toMatch(new RegExp(displayedValue));
+            }
+        );
     });
 
-    it('should display a specific text on search with 0 result', async () => {
-        render(
-            <AntForm name="name">
-                <AntForm.Item name="chartreuse">
-                    <MonoValueSelect
-                        attribute={attribute}
-                        state={state}
-                        fieldValue={{...fieldValue, isEditing: true}}
-                        handleSubmit={handleSubmitMock}
-                        handleBlur={handleBlurMock}
-                    />
-                </AntForm.Item>
-            </AntForm>
-        );
+    describe('search with result', () => {
+        it('should display a specific text on search with results', async () => {
+            render(
+                <AntForm name="name">
+                    <AntForm.Item name="chartreuse">
+                        <MonoValueSelect
+                            attribute={attribute}
+                            state={state}
+                            fieldValue={{...fieldValue, isEditing: true}}
+                            handleSubmit={handleSubmitMock}
+                            handleBlur={handleBlurMock}
+                        />
+                    </AntForm.Item>
+                </AntForm>
+            );
 
-        const select = screen.getByRole('combobox');
-        await userEvent.click(select);
-        await userEvent.type(select, 'unecouleur');
+            const select = screen.getByRole('combobox');
+            await userEvent.click(select);
+            await userEvent.type(select, 'r');
 
-        expect(screen.getByText('record_edition.search_not_found')).toBeVisible();
+            expect(screen.getByText('record_edition.press_enter_to')).toBeVisible();
+            expect(screen.getByText('record_edition.select_this_value')).toBeVisible();
+        });
+    });
+
+    describe('search without result', () => {
+        const newColor = 'une couleur';
+        it('should display a notfound text without free entry', async () => {
+            render(
+                <AntForm name="name">
+                    <AntForm.Item name="chartreuse">
+                        <MonoValueSelect
+                            attribute={attribute}
+                            state={state}
+                            fieldValue={{...fieldValue, isEditing: true}}
+                            handleSubmit={handleSubmitMock}
+                            handleBlur={handleBlurMock}
+                        />
+                    </AntForm.Item>
+                </AntForm>
+            );
+
+            const select = screen.getByRole('combobox');
+            await userEvent.click(select);
+            await userEvent.type(select, newColor);
+
+            expect(screen.queryByTitle(newColor)).not.toBeInTheDocument();
+            expect(screen.getByText('record_edition.search_not_found')).toBeVisible();
+        });
+
+        it('should display the option with free entry', async () => {
+            render(
+                <AntForm name="name">
+                    <AntForm.Item name="chartreuse">
+                        <MonoValueSelect
+                            attribute={{...attribute, values_list: {...valuesList, allowFreeEntry: true}}}
+                            state={state}
+                            fieldValue={{...fieldValue, isEditing: true}}
+                            handleSubmit={handleSubmitMock}
+                            handleBlur={handleBlurMock}
+                        />
+                    </AntForm.Item>
+                </AntForm>
+            );
+
+            const select = screen.getByRole('combobox');
+            await userEvent.click(select);
+            await userEvent.type(select, newColor);
+
+            expect(screen.queryByText('record_edition.search_not_found')).not.toBeInTheDocument();
+            const newColorOption = screen.getByText(`record_edition.select_option${newColor}`);
+            await userEvent.click(newColorOption);
+
+            expect(screen.getByTestId(attribute.id).innerHTML).toMatch(new RegExp(newColor));
+        });
     });
 });
