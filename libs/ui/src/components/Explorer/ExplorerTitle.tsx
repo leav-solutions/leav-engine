1980bb00b058f5ca829be7bab8474c736c032cb4
Teddy <t.paul@cacom.fr>
2024-11-15 09:36:32 +0100

    feat(explorer): display custom title or library label

---
+++ libs/ui/src/components/Explorer/ExplorerTitle.tsx
@@ -0,0 +1,39 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {localizedTranslation} from '@leav/utils';
+import {useExplorerLibraryDataQuery} from '_ui/_gqlTypes';
+import useLang from '_ui/hooks/useLang';
+import {AntSkeleton} from 'aristid-ds';
+import {FunctionComponent} from 'react';
+
+interface IExplorerTitleProps {
+    title?: string;
+    library: string;
+}
+
+export const ExplorerTitle: FunctionComponent<IExplorerTitleProps> = ({title, library}) => {
+    const {data, loading, error} = useExplorerLibraryDataQuery({variables: {libraryId: library}, skip: !!title});
+    const {lang} = useLang();
+
+    if (title) {
+        return <span>{title}</span>;
+    }
+
+    if (loading) {
+        return <AntSkeleton.Input style={{width: 400}} active />;
+    }
+
+    //TODO: handle error and bad library ID
+    if (error) {
+        return <span>{error.message}</span>;
+    }
+
+    const libraryData = data?.libraries?.list[0];
+
+    if (!libraryData) {
+        return <span>Unknown library</span>;
+    }
+
+    return <span>{localizedTranslation(libraryData.label, lang)}</span>;
+};
