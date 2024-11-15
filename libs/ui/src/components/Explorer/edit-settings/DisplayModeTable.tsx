c022a25e76b47c46668bbec10b583433d9217d14
Jérémy B <9062561+jrmyb@users.noreply.github.com>
2024-11-15 16:18:29 +0100

    Fix lint-staged / Add license-check CI job (#619)
    * feat: short
    
    longer
    
    * feat(test): test
    
    * feat: short
    
    * feat(test): test
    
    * feat(test): test
    
    * feat(delete tests files): test
    
    * feat(ci.yml): add license-check step
    
    * fix ci
    
    * rm test file

--- libs/ui/src/components/Explorer/edit-settings/DisplayModeTable.tsx
+++ libs/ui/src/components/Explorer/edit-settings/DisplayModeTable.tsx
@@ -1,3 +1,6 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {useDebouncedValue} from '_ui/hooks/useDebouncedValue/useDebouncedValue';
 import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
 import {KitInput, KitTypography} from 'aristid-ds';
