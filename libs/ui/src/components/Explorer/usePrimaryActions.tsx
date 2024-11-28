99c5b98c674e45bd09b24ec67569df4369864b42
Jules HABLOT <j.hablot-ext@aristid.com>
2024-11-28 16:05:03 +0100

    refactor(@leav/ui): make explicit the implicit

--- libs/ui/src/components/Explorer/usePrimaryActions.tsx
+++ libs/ui/src/components/Explorer/usePrimaryActions.tsx
@@ -10,37 +10,30 @@ import {KitButton, KitSpace} from 'aristid-ds';
  * Based on default actions and custom actions, it returns a primary with first action as "main" action and the others
  * in the dropdown accessible via a split button.
  *
- * When the creation is done, we refresh all data even if the new record will not be visible due to some filters.
- *
- * It returns also two parts : one for the call action button - one for displayed the modal required by the action.
- * It also returns the modal required for default actions (like create record).
- *
- * TODO: change params to match to signature
- * @param isEnabled - whether the action is present
- * @param library - the library's id to add new item
- * @param refetch - method to call to refresh the list. New item will be visible if it matches filters and sorts
+ * @param {Object[]} actions - list of actions to be display as one button: the primary action button
  */
 export const usePrimaryActionsButton = (actions: IPrimaryAction[]) => {
-    const [mainAction, ...dropdownActions] = actions;
+    const [firstAction, ...dropdownActions] = actions;
 
     return {
-        primaryButton: mainAction ? (
-            <KitButton
-                type="primary"
-                icon={mainAction.icon}
-                onClick={mainAction.callback}
-                items={dropdownActions.map((action, index) => ({
-                    key: index,
-                    label: (
-                        <KitSpace size={8}>
-                            {action.icon} {action.label}
-                        </KitSpace>
-                    ),
-                    onClick: action.callback
-                }))}
-            >
-                {mainAction.label}
-            </KitButton>
-        ) : null
+        primaryButton:
+            actions.length === 0 ? null : (
+                <KitButton
+                    type="primary"
+                    icon={firstAction.icon}
+                    onClick={firstAction.callback}
+                    items={dropdownActions.map((action, index) => ({
+                        key: index,
+                        label: (
+                            <KitSpace size={8}>
+                                {action.icon} {action.label}
+                            </KitSpace>
+                        ),
+                        onClick: action.callback
+                    }))}
+                >
+                    {firstAction.label}
+                </KitButton>
+            )
     };
 };
