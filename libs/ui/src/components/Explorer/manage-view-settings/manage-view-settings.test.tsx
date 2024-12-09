7f7f66797624d8f9b83de812152f45a607f7d60a
evoiron <e.voiron-levy@cacom.fr>
2024-12-09 17:05:12 +0100

    feat(@leav/ui): select filters in explorer (#654)
    * feat(@leav/ui): select filters in explorer

--- libs/ui/src/components/Explorer/manage-view-settings/manage-view-settings.test.tsx
+++ libs/ui/src/components/Explorer/manage-view-settings/manage-view-settings.test.tsx
@@ -13,7 +13,6 @@ import {useOpenViewSettings} from './open-view-settings/useOpenViewSettings';
 import {ViewSettingsContext} from './store-view-settings/ViewSettingsContext';
 import {IViewSettingsState, viewSettingsReducer} from './store-view-settings/viewSettingsReducer';
 import {viewSettingsInitialState} from './store-view-settings/viewSettingsInitialState';
-import {SortOrder} from '_ui/_gqlTypes';
 import {waitFor} from '@testing-library/react';
 
 const MockOpenEditSettings: FunctionComponent = () => {
@@ -157,6 +156,45 @@ describe('Integration tests about managing view settings feature', () => {
             await userEvent.click(within(firstActiveSort!).getByRole('button', {name: /hide/}));
 
             const [firstInactiveAttribute] = within(inactiveSorts).getAllByRole('listitem');
+
+            expect(firstInactiveAttribute).toHaveTextContent(attributesList[0].label.fr);
+            expect(firstInactiveAttribute).toBeVisible();
+        });
+    });
+
+    describe('Filter data', () => {
+        test('should be able to toggle filter activation', async () => {
+            render(
+                <EditSettingsContextProvider>
+                    <MockViewSettingsContextProvider viewMock={viewSettingsInitialState}>
+                        <MockOpenEditSettings />
+                        <SidePanel />
+                    </MockViewSettingsContextProvider>
+                </EditSettingsContextProvider>
+            );
+
+            await userEvent.click(screen.getByRole('button', {name: /settings/}));
+            await userEvent.click(screen.getByRole('button', {name: /filters/}));
+
+            const inactiveFilters = screen.getByRole('list', {name: /inactive/});
+
+            const [firstAttribute] = within(inactiveFilters).getAllByRole('listitem');
+
+            expect(firstAttribute).toHaveTextContent(attributesList[0].label.fr);
+            expect(firstAttribute).toBeVisible();
+
+            await userEvent.click(within(firstAttribute!).getByRole('button', {name: /show/}));
+
+            const activeFilters = screen.getByRole('list', {name: 'explorer.filter-list.active'});
+
+            const firstActiveFilter = within(activeFilters).getByRole('listitem');
+
+            expect(firstActiveFilter).toHaveTextContent(attributesList[0].label.fr);
+            expect(firstActiveFilter).toBeVisible();
+
+            await userEvent.click(within(firstActiveFilter!).getByRole('button', {name: /hide/}));
+
+            const [firstInactiveAttribute] = within(inactiveFilters).getAllByRole('listitem');
 
             expect(firstInactiveAttribute).toHaveTextContent(attributesList[0].label.fr);
             expect(firstInactiveAttribute).toBeVisible();
