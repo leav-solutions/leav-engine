2a96aef3a6c1667b991a79d7b055fd168651483a
Jules HABLOT <j.hablot-ext@aristid.com>
2024-12-10 11:51:04 +0100

    feat(@leav/ui): manage display multi color attribute

--- libs/ui/src/components/Explorer/TableTagGroup.tsx
+++ libs/ui/src/components/Explorer/TableTagGroup.tsx
@@ -6,6 +6,8 @@ import {IKitTag} from 'aristid-ds/dist/Kit/DataDisplay/Tag/types';
 import {FunctionComponent} from 'react';
 import styled from 'styled-components';
 
+export const multiColorTagAvatarClassName = 'multi-color-tag-avatar';
+
 const StyledTagsGroupDiv = styled.div`
     display: flex;
     column-gap: calc(var(--general-spacing-xxs) * 1px);
@@ -14,6 +16,13 @@ const StyledTagsGroupDiv = styled.div`
 
     & > span {
         margin-right: var(--general-spacing-none);
+    }
+
+    // TODO: wait DS to allow better customization on avatar
+    &&& .${multiColorTagAvatarClassName} {
+        height: calc(var(--general-spacing-s) * 1px);
+        width: calc(var(--general-spacing-s) * 1px);
+        border-radius: calc(var(--general-border-radius-xs) * 1px);
     }
 `;
 
