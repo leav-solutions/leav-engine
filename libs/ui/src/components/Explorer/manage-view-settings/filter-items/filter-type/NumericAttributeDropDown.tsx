6e762f344ebf87015024406dccd2014e88cf73a3
Jules HABLOT <j.hablot-ext@aristid.com>
2024-12-19 11:58:57 +0100

    fix(@leav/ui): allow to reset filter condition + value

--- libs/ui/src/components/Explorer/manage-view-settings/filter-items/filter-type/NumericAttributeDropDown.tsx
+++ libs/ui/src/components/Explorer/manage-view-settings/filter-items/filter-type/NumericAttributeDropDown.tsx
@@ -2,15 +2,15 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
-import {KitButton, KitDivider, KitInput, KitInputNumber, KitSelect, KitSpace} from 'aristid-ds';
+import {KitButton, KitDivider, KitInputNumber, KitSelect, KitSpace} from 'aristid-ds';
 import {ComponentProps, FunctionComponent} from 'react';
 import {FaClock, FaTrash} from 'react-icons/fa';
+import styled from 'styled-components';
 import {AttributeConditionFilter} from '_ui/types';
-import {IExplorerFilter, IFilterDropDownProps} from '_ui/components/Explorer/_types';
+import {IExplorerFilter, IFilterDropDownProps} from '../../../_types';
 import {useViewSettingsContext} from '../../store-view-settings/useViewSettingsContext';
 import {ViewSettingsActionTypes} from '../../store-view-settings/viewSettingsReducer';
 import {useConditionsOptionsByType} from './useConditionOptionsByType';
-import styled from 'styled-components';
 
 const InputNumberStyled = styled(KitInputNumber)`
     width: 100%;
@@ -35,7 +35,13 @@ export const NumericAttributeDropDown: FunctionComponent<IFilterDropDownProps> =
     const _onInputChanged: ComponentProps<typeof KitInputNumber>['onChange'] = value =>
         _updateFilter({...filter, value: value === null ? null : String(value)});
 
-    const _onResetFilter: ComponentProps<typeof KitButton>['onClick'] = () => _updateFilter({...filter, value: null});
+    const _onResetFilter: ComponentProps<typeof KitButton>['onClick'] = () =>
+        dispatch({
+            type: ViewSettingsActionTypes.RESET_FILTER,
+            payload: {
+                id: filter.id
+            }
+        });
 
     const _onDeleteFilter: ComponentProps<typeof KitButton>['onClick'] = () =>
         dispatch({
@@ -55,7 +61,7 @@ export const NumericAttributeDropDown: FunctionComponent<IFilterDropDownProps> =
             {showInput && (
                 <InputNumberStyled
                     placeholder={String(t('explorer.type-a-value'))}
-                    defaultValue={filter.value ?? undefined}
+                    value={filter.value}
                     onChange={_onInputChanged}
                 />
             )}
