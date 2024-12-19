c9a34f20c400e30305cf7727696e9cbebbc3bbab
Teddy Paul <TdyP@users.noreply.github.com>
2024-12-19 17:35:35 +0100

    test(explorer): filters display on Explorer  (#671)

--- libs/ui/src/components/Explorer/manage-view-settings/_shared/CommonFilterItem.test.tsx
+++ libs/ui/src/components/Explorer/manage-view-settings/_shared/CommonFilterItem.test.tsx
@@ -1,7 +1,7 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {render, screen, waitFor} from '_ui/_tests/testUtils';
+import {render, screen} from '_ui/_tests/testUtils';
 import userEvent from '@testing-library/user-event';
 import {IExplorerFilter} from '../../_types';
 import {CommonFilterItem} from './CommonFilterItem';
