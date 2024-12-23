aa9efaefb5b047bc7cb508f118c8e247470213e7
emile <e.voiron-levy@aristid.com>
2024-12-23 16:29:46 +0100

    feat(@leav/ui): explorer - display sort in toolbar

--- libs/ui/src/components/Explorer/Explorer.tsx
+++ libs/ui/src/components/Explorer/Explorer.tsx
@@ -23,7 +23,7 @@ import {
 import {useSearchInput} from './useSearchInput';
 import {usePagination} from './usePagination';
 import {Loading} from '../Loading';
-import {ExplorerFilterBar} from './display-view-filters/ExplorerFilterBar';
+import {ExplorerToolBar} from './display-view-filters/ExplorerToolBar';
 import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
 import {useViewSettingsReducer} from './useViewSettingsReducer';
 
@@ -120,7 +120,7 @@ export const Explorer: FunctionComponent<IExplorerProps> = ({
                         {primaryButton}
                     </KitSpace>
                 </ExplorerHeaderDivStyled>
-                <ExplorerFilterBar />
+                <ExplorerToolBar libraryId={library} />
                 {loading || viewSettingsLoading ? (
                     <Loading />
                 ) : hasNoResults ? (
