6f81e4e8e90ff09f341b3f43600ac58bd2307acf
emile <e.voiron-levy@aristid.com>
2025-03-19 14:26:22 +0100

    --wip-- [skip ci]

--- libs/ui/src/components/LibraryItemsList/shared/DeactivateRecordBtn/DeactivateRecordBtn.test.tsx
+++ libs/ui/src/components/LibraryItemsList/shared/DeactivateRecordBtn/DeactivateRecordBtn.test.tsx
@@ -5,9 +5,11 @@ import userEvent from '@testing-library/user-event';
 import {SearchContext} from '_ui/components/LibraryItemsList/hooks/useSearchReducer/searchContext';
 import {initialSearchState} from '_ui/components/LibraryItemsList/hooks/useSearchReducer/searchReducer';
 import {DeactivateRecordsDocument} from '_ui/_gqlTypes';
-import {render, screen, waitFor} from '_ui/_tests/testUtils';
+import {act, render, screen, waitFor} from '_ui/_tests/testUtils';
 import {mockRecord} from '_ui/__mocks__/common/record';
 import DeactivateRecordBtn from './DeactivateRecordBtn';
+import {KitApp} from 'aristid-ds';
+import {App} from 'antd';
 
 describe('DeactivateRecordBtn', () => {
     test('Render test', async () => {
@@ -33,16 +35,22 @@ describe('DeactivateRecordBtn', () => {
         ];
 
         render(
-            <SearchContext.Provider value={{state: initialSearchState, dispatch: jest.fn()}}>
-                <DeactivateRecordBtn record={mockRecord} />
-            </SearchContext.Provider>,
+            <App>
+                <SearchContext.Provider value={{state: initialSearchState, dispatch: jest.fn()}}>
+                    <DeactivateRecordBtn record={mockRecord} />
+                </SearchContext.Provider>
+            </App>,
             {mocks}
         );
 
-        userEvent.click(screen.getByRole('button', {name: /delete/i}));
+        await act(async () => {
+            await userEvent.click(screen.getByRole('button', {name: /delete/i}));
+        });
         expect(await screen.findByText(/confirm_one/i)).toBeInTheDocument();
 
-        userEvent.click(screen.getByText(/submit/i));
+        await act(async () => {
+            userEvent.click(screen.getByText(/submit/i));
+        });
 
         await waitFor(() => expect(mutationCalled).toBe(true));
     });
