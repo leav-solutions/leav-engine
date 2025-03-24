a8cae8c86f9e424da2f9737229a5587c83380790
Philippe Chevieux <philippe.chevieux@gmail.com>
2025-03-24 10:20:30 +0100

    fix(@leav/ui): handle tree, link and boolean properly in record sidebar
    fix(d): d

--- libs/ui/src/components/LibraryItemsList/LibraryItemsListTable/Cell/typeGuards.ts
+++ libs/ui/src/components/LibraryItemsList/LibraryItemsListTable/Cell/typeGuards.ts
@@ -1,6 +1,11 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {
+    RecordFormElementsValueLinkValue,
+    RecordFormElementsValueStandardValue,
+    RecordFormElementsValueTreeValue
+} from '_ui/hooks/useGetRecordForm';
 import {LinkCellValue, LinkCellValues, SimpleCellValue, SimpleCellValues, TreeCellValue, TreeCellValues} from './types';
 
 export const TypeGuards = {
@@ -13,5 +18,23 @@ export const TypeGuards = {
     isTreeCellValue: (value: SimpleCellValue | LinkCellValue | TreeCellValue): value is TreeCellValue =>
         'treeValue' in value,
     isTreeCellValues: (values: SimpleCellValues | LinkCellValues | TreeCellValues): values is TreeCellValues =>
-        values.length > 0 && TypeGuards.isTreeCellValue(values[0])
+        values.length > 0 && TypeGuards.isTreeCellValue(values[0]),
+    isRecordFormElementsValueStandardValue: (
+        value:
+            | RecordFormElementsValueStandardValue
+            | RecordFormElementsValueLinkValue
+            | RecordFormElementsValueTreeValue
+    ): value is RecordFormElementsValueStandardValue => value && 'value' in value,
+    isRecordFormElementsValuesLinkValue: (
+        value:
+            | RecordFormElementsValueStandardValue
+            | RecordFormElementsValueLinkValue
+            | RecordFormElementsValueTreeValue
+    ): value is RecordFormElementsValueLinkValue => value && 'linkValue' in value,
+    isRecordFormElementsValuesTreeValue: (
+        value:
+            | RecordFormElementsValueStandardValue
+            | RecordFormElementsValueLinkValue
+            | RecordFormElementsValueTreeValue
+    ): value is RecordFormElementsValueTreeValue => value && 'treeValue' in value
 };
