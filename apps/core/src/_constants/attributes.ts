09f28117d92690ceebb2644d9234147cd96203b5
jrmyb <9062561+jrmyb@users.noreply.github.com>
2025-05-12 14:44:05 +0200

    add license

--- apps/core/src/_constants/attributes.ts
+++ apps/core/src/_constants/attributes.ts
@@ -1,1 +1,4 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 export const baseAttributes = ['id', 'modified_by', 'modified_at', 'created_by', 'created_at', 'active'];
