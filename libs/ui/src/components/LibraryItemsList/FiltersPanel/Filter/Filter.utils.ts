c463f1e63acba21a8c0562c3ff4238c1ad589b08
Renaud AMSELLEM <r.amsellem-ext@aristid.com>
2025-05-22 15:02:16 +0200

    chore: remove moment

--- libs/ui/src/components/LibraryItemsList/FiltersPanel/Filter/Filter.utils.ts
+++ libs/ui/src/components/LibraryItemsList/FiltersPanel/Filter/Filter.utils.ts
@@ -2,14 +2,14 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {AttributeFormat} from '_ui/_gqlTypes';
-import moment from 'moment/moment';
+import dayjs from 'dayjs';
 
 export const getDefaultFilterValueByFormat = (format: AttributeFormat): boolean | string | number => {
     switch (format) {
         case AttributeFormat.boolean:
             return true;
         case AttributeFormat.date:
-            return moment().utcOffset(0).startOf('day').unix();
+            return dayjs.utc().startOf('day').unix();
         default:
             return '';
     }
