dd998c1a1be67474f1afb0a0dce74f0f8db96987
Philippe Chevieux <philippe.chevieux@gmail.com>
2025-06-11 17:29:21 +0200

    refactor(@leav/ui): improve code readability

--- apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/useSidePanelForm.tsx
+++ apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/useSidePanelForm.tsx
@@ -1,48 +1,57 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {ComponentProps, useRef, useState} from 'react';
+import {ComponentProps, useState} from 'react';
 import {createPortal} from 'react-dom';
 import {KitSidePanel} from 'aristid-ds';
 import {SIDE_PANEL_CONTENT_ID} from '../../../constants';
 import {EditRecordPage} from '@leav/ui';
 import {IUseIFrameMessengerOptions} from '_ui/hooks/useIFrameMessenger/types';
 
-const NO_RENDER_KEY = 0;
+const closedSidePanel = {key: 'closed'} as const;
+
+type EditRecordPageInSidePanelProps =
+    | (ComponentProps<typeof EditRecordPage> & {
+          key: number;
+      })
+    | typeof closedSidePanel;
 
 export const useSidePanelForm = () => {
-    const sidePanelPropsRef = useRef<ComponentProps<typeof KitSidePanel> | null>(null);
-    const [keyToForceRerender, setKeyToForceRerender] = useState(NO_RENDER_KEY);
+    const [editRecordPageInSidePanelProps, setEditRecordPageInSidePanelProps] =
+        useState<EditRecordPageInSidePanelProps>(closedSidePanel);
 
-    const closeSidePanelForm = () => {
-        if (sidePanelPropsRef.current && sidePanelPropsRef.current.onClose) {
-            sidePanelPropsRef.current.onClose();
-        }
-
-        sidePanelPropsRef.current = null;
-        setKeyToForceRerender(NO_RENDER_KEY);
+    const closeSidePanelForm = (onClose?: () => void) => {
+        onClose?.();
+        setEditRecordPageInSidePanelProps(closedSidePanel);
     };
 
     const openSidePanelForm: IUseIFrameMessengerOptions['handlers']['onSidePanelForm'] = data => {
-        sidePanelPropsRef.current = data;
-        setKeyToForceRerender(prev => prev + 1);
+        setEditRecordPageInSidePanelProps(({key}) => ({
+            ...data,
+            key: key === closedSidePanel.key ? 0 : key + 1
+        }));
     };
 
     return {
         openSidePanelForm,
         CustomSidePanelForm:
-            keyToForceRerender === NO_RENDER_KEY
+            editRecordPageInSidePanelProps.key === closedSidePanel.key
                 ? null
                 : createPortal(
-                      <KitSidePanel initialOpen floating closable size="m" onClose={closeSidePanelForm}>
+                      <KitSidePanel
+                          initialOpen
+                          floating
+                          closable
+                          size="m"
+                          onClose={() => closeSidePanelForm(editRecordPageInSidePanelProps.onClose)}
+                      >
                           <div style={{height: '100%'}}>
                               <EditRecordPage
-                                  key={keyToForceRerender}
                                   record={null}
                                   library={null}
-                                  {...sidePanelPropsRef.current}
+                                  {...editRecordPageInSidePanelProps}
                                   showRefreshButton={false}
-                                  onClose={closeSidePanelForm}
+                                  onClose={() => closeSidePanelForm(editRecordPageInSidePanelProps.onClose)}
                               />
                           </div>
                       </KitSidePanel>,
