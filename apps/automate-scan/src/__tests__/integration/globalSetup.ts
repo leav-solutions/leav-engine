3db7d2f3f0819491597ac43841ff47b522c6f5df
Sébastien Berthier <s.berthier@aristid.com>
2025-07-09 17:48:58 +0200

    chore(automate-scan): fix integration test

--- apps/automate-scan/src/__tests__/integration/globalSetup.ts
+++ apps/automate-scan/src/__tests__/integration/globalSetup.ts
@@ -1,9 +1,12 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import * as fs from 'fs';
+import * as path from 'path';
+
 export async function setup() {
     try {
-        // Do whatever you need to setup your integration tests
+        fs.mkdirSync(path.join(__dirname, '_fixtures'));
     } catch (e) {
         console.error(e);
     }
