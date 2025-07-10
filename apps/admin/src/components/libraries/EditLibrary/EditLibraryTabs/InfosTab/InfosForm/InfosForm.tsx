58e62f11daa142d6a3b4af20ea710d7310d75539
Jérémy B <9062561+jrmyb@users.noreply.github.com>
2025-07-10 11:22:50 +0200

    fix(admin): Can unselect tree for default color and preview in record identity library (#969)
    * fix form dropdown unselect color record identity
    
    * fix libAttributesOptions assignement

--- apps/admin/src/components/libraries/EditLibrary/EditLibraryTabs/InfosTab/InfosForm/InfosForm.tsx
+++ apps/admin/src/components/libraries/EditLibrary/EditLibraryTabs/InfosTab/InfosForm/InfosForm.tsx
@@ -106,8 +106,8 @@ const InfosForm = ({library, onSubmit, readonly, errors, onCheckIdExists}: IInfo
           }))
         : [];
 
-    const libTreeAttributesOptions = library?.attributes
-        ? library.attributes
+    const libTreeAttributesOptions = initialValues.attributes
+        ? initialValues.attributes
               .filter(a => a.type === AttributeType.tree)
               .map(a => ({
                   key: a.id,
@@ -115,7 +115,9 @@ const InfosForm = ({library, onSubmit, readonly, errors, onCheckIdExists}: IInfo
                   text: localizedLabel(a.label, lang) || a.id
               }))
         : [];
+
     libAttributesOptions.unshift({key: '', value: '', text: ''});
+    libTreeAttributesOptions.unshift({key: '', value: '', text: ''});
 
     const _handleSubmit = values => {
         onSubmit(values);
