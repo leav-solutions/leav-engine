24b3df0c671ea5f7c4c9cefd5965517f67907167
Sébastien Berthier <s.berthier@aristid.com>
2025-07-10 10:03:20 +0200

    chore(docker): plugins_install.sh yarn --production
    - For now, yarn version installed in leav/core is yarn@1, keep it for now.
    - However, plugins could embed their version of yarn, and even use another package manager like npm.
    - We need to refactor how plugins are embed in core and installed, but later.

--- docker/scripts/plugins_install.sh
+++ docker/scripts/plugins_install.sh
@@ -1,6 +1,11 @@
 pluginsFolder=/app/apps/core/dist/plugins
 
+# By default, that should be yarn@1 in base nodejs docker image
+yarn --version
+
 find $pluginsFolder -name package.json -not -path "*/node_modules/*" -exec sh -c '
   echo "🚧 Install dependencies for plugin $(basename $(dirname {}))"
-  (cd $(dirname {}) && touch "$plugin"yarn.lock && yarn install)
+  (cd $(dirname {}) && touch yarn.lock && yarn install --production --non-interactive)
 ' \;
+
+yarn cache clean
