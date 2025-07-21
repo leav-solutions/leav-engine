6da2515351137f95de054c48946597733a8837bb
Philippe <philippe.chevieux@gmail.com>
2025-07-21 10:10:45 +0200

    feat(app-studio): allow useNavigateToPanel hook to use where property

--- apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/__tests__/useNavigateToPanel.spec.ts
+++ apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/__tests__/useNavigateToPanel.spec.ts
@@ -4,24 +4,29 @@
 import {renderHook} from '_ui/_tests/testUtils';
 import * as ReactRouter from 'react-router-dom';
 import {useNavigateToPanel} from '../useNavigateToPanel';
+import {INestedPanel} from '_ui/hooks/useIFrameMessenger/types';
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useNavigate: jest.fn(),
-    useOutletContext: jest.fn()
+    useOutletContext: jest.fn(),
+    useParams: jest.fn()
 }));
 
 describe('useNavigateToPanel', () => {
     const spyUseOutletContext = jest.spyOn(ReactRouter, 'useOutletContext');
     const spyUseNavigate = jest.spyOn(ReactRouter, 'useNavigate');
+    const spyUseParamsMock = jest.spyOn(ReactRouter, 'useParams');
     const navigateMock = jest.fn();
 
     beforeEach(() => {
+        jest.clearAllMocks();
         spyUseNavigate.mockReturnValue(navigateMock);
         spyUseOutletContext.mockReturnValue({
             currentPanel: {id: 'currentPanelId'},
             currentWorkspace: {id: 'workspaceId'}
         });
+        spyUseParamsMock.mockReturnValue({panelId: 'currentPanelId'});
     });
 
     it('should provide a method to navigate to a panel', async () => {
@@ -34,24 +39,70 @@ describe('useNavigateToPanel', () => {
         expect(navigateMock).toHaveBeenCalledWith('/panelIdTest');
     });
 
-    it('should call addPanel when data includes a panel', () => {
-        const addPanelMock = jest.fn();
+    describe('when data includes a panel', () => {
+        it('should call addPanel and navigate to the panel when where is fullpage', () => {
+            const addPanelMock = jest.fn();
 
-        const {
-            result: {current}
-        } = renderHook(() => useNavigateToPanel(addPanelMock));
+            const {
+                result: {current}
+            } = renderHook(() => useNavigateToPanel(addPanelMock));
 
-        const panelData = {
-            panelId: 'panelIdTest',
-            panel: {id: 'panelIdTest', name: 'Test Panel', children: []}
-        };
+            const panelData: INestedPanel = {
+                where: 'fullpage',
+                what: {id: 'panelIdTest', name: 'Test Panel', children: []}
+            };
 
-        current.navigateToPanel(panelData);
+            current.navigateToPanel(panelData);
 
-        expect(addPanelMock).toHaveBeenCalledWith(panelData.panel, {
-            workspaceId: 'workspaceId',
-            panelId: 'currentPanelId'
+            expect(addPanelMock).toHaveBeenCalledWith(panelData.what, {
+                workspaceId: 'workspaceId',
+                panelId: 'currentPanelId'
+            });
+            expect(navigateMock).toHaveBeenCalledWith('/panelIdTest');
         });
-        expect(navigateMock).toHaveBeenCalledWith('/panelIdTest');
+
+        it('should call addPanel and navigate to the panel when where is popup', () => {
+            const addPanelMock = jest.fn();
+
+            const {
+                result: {current}
+            } = renderHook(() => useNavigateToPanel(addPanelMock));
+
+            const panelData: INestedPanel = {
+                where: 'popup',
+                what: {id: 'popupPanelIdTest', name: 'Test Panel', children: []}
+            };
+
+            current.navigateToPanel(panelData);
+
+            expect(addPanelMock).toHaveBeenCalledWith(panelData.what, {
+                workspaceId: 'workspaceId',
+                panelId: 'currentPanelId'
+            });
+
+            expect(navigateMock).toHaveBeenCalledWith('popup/popupPanelIdTest');
+        });
+
+        it('should call addPanel and navigate to the panel when where is slider', () => {
+            const addPanelMock = jest.fn();
+
+            const {
+                result: {current}
+            } = renderHook(() => useNavigateToPanel(addPanelMock));
+
+            const panelData: INestedPanel = {
+                where: 'slider',
+                what: {id: 'sliderPanelIdTest', name: 'Test Panel', children: []}
+            };
+
+            current.navigateToPanel(panelData);
+
+            expect(addPanelMock).toHaveBeenCalledWith(panelData.what, {
+                workspaceId: 'workspaceId',
+                panelId: 'currentPanelId'
+            });
+
+            expect(navigateMock).toHaveBeenCalledWith('slider/sliderPanelIdTest');
+        });
     });
 });
--- apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/useNavigateToPanel.ts
+++ apps/skeleton-app/src/modules/ApplicationRouting/custom-panel-message-handlers/useNavigateToPanel.ts
@@ -1,21 +1,34 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {generatePath, useNavigate, useOutletContext} from 'react-router-dom';
+import {generatePath, useLocation, useNavigate, useOutletContext, useParams} from 'react-router-dom';
 import {IUseIFrameMessengerOptions} from '_ui/hooks/useIFrameMessenger/types';
 import {routes} from '../routes';
 import {AddPanel, IApplicationMatchingContext} from '../types';
 
 export const useNavigateToPanel = (addPanel: AddPanel) => {
     const navigate = useNavigate();
-    const {currentPanel, currentWorkspace} = useOutletContext<IApplicationMatchingContext>();
+    const {currentWorkspace} = useOutletContext<IApplicationMatchingContext>();
+    const {panelId} = useParams();
+    const {search} = useLocation();
 
     const navigateToPanel: IUseIFrameMessengerOptions['handlers']['onNavigateToPanel'] = data => {
-        if ('panel' in data) {
-            addPanel(data.panel, {workspaceId: currentWorkspace.id, panelId: currentPanel.id});
+        const routeParamsMap: Record<string, {route: string; params: {panelId: string; [key: string]: string}}> = {};
+        let where = 'fullpage';
+
+        if ('what' in data) {
+            where = data.where;
+            routeParamsMap.popup = {route: routes.popupPanel, params: {panelId, popupPanelId: data.what.id}};
+            routeParamsMap.slider = {route: routes.sliderPanel, params: {panelId, sliderPanelId: data.what.id}};
+            routeParamsMap.fullpage = {route: routes.panel, params: {panelId: data.what.id}};
+            addPanel(data.what, {workspaceId: currentWorkspace.id, panelId});
+        } else {
+            routeParamsMap.fullpage = {route: routes.panel, params: {panelId: data.panelId}};
         }
 
-        return navigate(generatePath(routes.panel, {panelId: data.panelId}));
+        const {route, params} = routeParamsMap[where];
+
+        return navigate(generatePath(route, params) + search);
     };
 
     return {
