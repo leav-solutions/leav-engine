7e5f6f555662b63840ee6dd7d23e76782c343797
Sébastien Berthier <s.berthier@aristid.com>
2025-08-27 11:07:11 +0200

    fix(core): dataloaders in ctx should not be serializable
    Important for task params that are serialized in arango core_tasks collection, and read by worker

---
+++ apps/core/src/utils/dataloader.spec.ts
@@ -0,0 +1,57 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import DataLoader from 'dataloader';
+import {IQueryInfos} from '_types/queryInfos';
+import {dataloaderCtxKey, getOrCreateDataLoaderInCtx} from './dataloader';
+
+describe('Dataloader', () => {
+    describe('getOrCreateDataLoaderInCtx', () => {
+        test('Should not be serialized in ctx', async () => {
+            const ctx: IQueryInfos = {userId: 'user1'};
+            getOrCreateDataLoaderInCtx(ctx, 'testLoader', () => new DataLoader(async keys => keys.map(() => 41)));
+            const serialized = JSON.stringify(ctx);
+            expect(serialized).not.toContain(dataloaderCtxKey);
+        });
+
+        test('Should create a new dataloader if not present', () => {
+            const ctx: IQueryInfos = {userId: 'user2'};
+            const loader = getOrCreateDataLoaderInCtx(
+                ctx,
+                'loaderA',
+                () => new DataLoader(async keys => keys.map(() => 42))
+            );
+            expect(loader).toBeInstanceOf(DataLoader);
+        });
+
+        test('Should return the same dataloader instance on subsequent calls', () => {
+            const ctx: IQueryInfos = {userId: 'user3'};
+            const loader1 = getOrCreateDataLoaderInCtx(
+                ctx,
+                'loaderB',
+                () => new DataLoader(async keys => keys.map(() => 43))
+            );
+            const loader2 = getOrCreateDataLoaderInCtx(
+                ctx,
+                'loaderB',
+                () => new DataLoader(async keys => keys.map(() => 44))
+            );
+            expect(loader1).toBe(loader2);
+        });
+
+        test('Should create different dataloaders for different keys', () => {
+            const ctx: IQueryInfos = {userId: 'user4'};
+            const loaderA = getOrCreateDataLoaderInCtx(
+                ctx,
+                'loaderA',
+                () => new DataLoader(async keys => keys.map(() => 45))
+            );
+            const loaderB = getOrCreateDataLoaderInCtx(
+                ctx,
+                'loaderB',
+                () => new DataLoader(async keys => keys.map(() => 46))
+            );
+            expect(loaderA).not.toBe(loaderB);
+        });
+    });
+});
--- apps/core/src/utils/dataloader.ts
+++ apps/core/src/utils/dataloader.ts
@@ -4,6 +4,7 @@
 import DataLoader from 'dataloader';
 import {IQueryInfos} from '_types/queryInfos';
 
+export const dataloaderCtxKey: keyof IQueryInfos = 'dataLoaders' as const;
 /**
  * Get or create a DataLoader in the request context.
  * Each usage (not each call) of this function should use a unique name to avoid conflicts among different DataLoaders.
@@ -14,7 +15,16 @@ export function getOrCreateDataLoaderInCtx<DL extends DataLoader<unknown, unknow
     name: string,
     create: () => DL
 ): DL {
-    ctx.dataLoaders = ctx.dataLoaders || {};
+    // Important to not serialize dataLoaders to JSON for task submission for instance;
+    // ctx is in task params, saved in core_tasks collection and read by worker
+    if (ctx.dataLoaders == null) {
+        Object.defineProperty(ctx, dataloaderCtxKey, {
+            value: {},
+            enumerable: false,
+            writable: true,
+            configurable: true
+        });
+    }
     if (!ctx.dataLoaders[name]) {
         ctx.dataLoaders[name] = create();
     }
