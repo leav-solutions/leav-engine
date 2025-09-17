a8ed1513989bc1134b857019ede64cba048d77ad
Rémy Michallet <remy.michallet@gmail.com>
2025-09-17 10:38:29 +0200

    fix: mapper type

--- apps/app-studio/src/modules/ApplicationRouting/explorer-panel/mapperToExplorerProps.tsx
+++ apps/app-studio/src/modules/ApplicationRouting/explorer-panel/mapperToExplorerProps.tsx
@@ -1,9 +1,9 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {ComponentProps} from 'react';
-import {Explorer} from '@leav/ui';
-import {LibraryExplorerProps} from '../types';
+import {type ComponentProps} from 'react';
+import {type Explorer} from '@leav/ui';
+import {type LibraryExplorerProps} from '../types';
 
 const isBoolean = (val: unknown): val is boolean => 'boolean' === typeof val;
 
@@ -33,7 +33,7 @@ export const mapToLibraryExplorerProps = ({
 export const mapToCommonExplorerProps = ({
     explorerProps
 }: {
-    explorerProps: LibraryExplorerProps;
+    explorerProps: LibraryExplorerProps | undefined;
 }): Pick<ComponentProps<typeof Explorer>, CommonOverridablePropsByUser> => {
     if (!explorerProps) {
         return {iconsOnlyItemActions: true};
