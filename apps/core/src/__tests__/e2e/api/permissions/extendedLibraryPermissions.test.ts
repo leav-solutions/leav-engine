4a1417a69e5ad9aebd680a023f11f4dd1acd57a5
Sébastien Berthier <s.berthier@aristid.com>
2025-10-15 12:40:55 +0200

    test(core): add e2e ExtendedLibraryPermissions

---
+++ apps/core/src/__tests__/e2e/api/permissions/extendedLibraryPermissions.test.ts
@@ -0,0 +1,433 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {RecordPermissionsActions} from '../../../../_types/permissions';
+import {AttributeTypes} from '../../../../_types/attribute';
+import {AttributeCondition} from '../../../../_types/record';
+import {
+    gqlAddElemToTree,
+    gqlCreateRecord,
+    gqlSaveAttribute,
+    gqlSaveLibrary,
+    gqlSaveTree,
+    makeGraphQlCall
+} from '../e2eUtils';
+import {adminsGroupId} from '../../../../_constants/users';
+
+// Extended library permissions using a tree with a library for tree elements
+// particularly apps/core/src/domain/record/helpers/getAccessPermissionFilters.ts
+describe('ExtendedLibraryPermissions', () => {
+    const permNodeLibName = 'extended_library_permissions_test_node_lib'; // for tree element
+    const permTreeName = 'extended_library_permissions_test_tree';
+    const libName = 'extended_library_permissions_test_lib';
+    const libTreeAttr = 'extended_library_permissions_attribute'; // tree attribute
+    let permTreeNodeRecord1Id: string;
+    let permTreeNodeRecord2Id: string;
+    let permTreeNode1Id: string;
+    let permTreeNode2Id: string;
+    let record1Id: string;
+    let record2Id: string;
+
+    beforeAll(async () => {
+        await gqlSaveLibrary(permNodeLibName, 'Test node lib', []);
+        await gqlSaveTree(permTreeName, 'Permissions tree', [permNodeLibName]);
+
+        permTreeNodeRecord1Id = await gqlCreateRecord(permNodeLibName);
+        permTreeNodeRecord2Id = await gqlCreateRecord(permNodeLibName);
+
+        permTreeNode1Id = await gqlAddElemToTree(
+            permTreeName,
+            {id: permTreeNodeRecord1Id, library: permNodeLibName},
+            null,
+            2
+        );
+        permTreeNode2Id = await gqlAddElemToTree(
+            permTreeName,
+            {id: permTreeNodeRecord2Id, library: permNodeLibName},
+            null,
+            2
+        );
+
+        // Create library using permission tree
+        await gqlSaveAttribute({
+            id: libTreeAttr,
+            label: 'Test Attr tree record permissions',
+            type: AttributeTypes.TREE,
+            linkedTree: permTreeName,
+            multipleValues: false
+        });
+
+        await makeGraphQlCall(
+            `mutation {
+            saveLibrary(library: {
+                id: "${libName}",
+                attributes: [
+                    "${libTreeAttr}"
+                ],
+                permissions_conf: {permissionTreeAttributes: ["${libTreeAttr}"], relation: and}
+            }) {
+                id
+            }
+        }`,
+            true
+        );
+
+        record1Id = await gqlCreateRecord(libName);
+        record2Id = await gqlCreateRecord(libName);
+
+        await makeGraphQlCall(
+            `mutation {
+            saveValue(
+                library: "${libName}",
+                recordId: "${record1Id}",
+                attribute: "${libTreeAttr}",
+                value: {
+                    payload: "${permTreeNode1Id}"
+                }
+            ) {
+                id_value
+            }
+        }`,
+            true
+        );
+        await makeGraphQlCall(
+            `mutation {
+            saveValue(
+                library: "${libName}",
+                recordId: "${record2Id}",
+                attribute: "${libTreeAttr}",
+                value: {
+                    payload: "${permTreeNode2Id}"
+                }
+            ) {
+                id_value
+            }
+        }`,
+            true
+        );
+    });
+
+    afterAll(async () => {
+        await makeGraphQlCall(
+            `mutation {
+            d1: deleteRecord(library: "${permNodeLibName}", id: "${permTreeNodeRecord1Id}") { id }
+            d2: deleteRecord(library: "${permNodeLibName}", id: "${permTreeNodeRecord2Id}") { id }
+            d10: deleteLibrary(id: "${libName}") { id }
+            d11: deleteTree(id: "${permTreeName}") { id }
+            d12: deleteLibrary(id: "${permNodeLibName}") { id }
+            d20: deleteAttribute(id: "${libTreeAttr}") { id }
+        }`,
+            true
+        );
+    });
+
+    describe('no perm', () => {
+        it('record should get all of them', async () => {
+            const records = await getLibRecords();
+
+            expect(records.length).toBe(2);
+        });
+
+        it('record should get some of them with filter', async () => {
+            const records = await getLibRecords(`[
+                {
+                    condition: ${AttributeCondition.EQUAL},
+                    field: "${libTreeAttr}.${permNodeLibName}.id",
+                    value: "${permTreeNodeRecord1Id}"
+                }
+            ]`);
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record1Id);
+        });
+    });
+
+    describe('disable access_record on node 1 for everybody', () => {
+        beforeAll(async () => {
+            await makeGraphQlCall(
+                `mutation {
+                savePermission(
+                    permission: {
+                        type: record,
+                        applyTo: "${libName}",
+                        usersGroup: null,
+                        permissionTreeTarget: {
+                            tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                        },
+                        actions: [
+                            {name: ${RecordPermissionsActions.ACCESS_RECORD}, allowed: false},
+                        ]
+                    }
+                ) { 
+                    type
+                }
+            }`,
+                true
+            );
+        });
+
+        afterAll(async () => {
+            await makeGraphQlCall(
+                `mutation {
+                savePermission(
+                    permission: {
+                        type: record,
+                        applyTo: "${libName}",
+                        usersGroup: null,
+                        permissionTreeTarget: {
+                            tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                        },
+                        actions: [
+                            {name: ${RecordPermissionsActions.ACCESS_RECORD}, allowed: null},
+                        ]
+                    }
+                ) { 
+                    type
+                }
+            }`,
+                true
+            );
+        });
+
+        it('record should get only one of them', async () => {
+            const records = await getLibRecords();
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record2Id);
+        });
+
+        it('record should get none of them with filter on node 1', async () => {
+            const records = await getLibRecords(`[
+                {
+                    condition: ${AttributeCondition.EQUAL},
+                    field: "${libTreeAttr}.${permNodeLibName}.id",
+                    value: "${permTreeNodeRecord1Id}"
+                }
+            ]`);
+
+            expect(records.length).toBe(0);
+        });
+
+        it('record should get one of them with filter on node 2', async () => {
+            const records = await getLibRecords(`[
+                {
+                    condition: ${AttributeCondition.EQUAL},
+                    field: "${libTreeAttr}.${permNodeLibName}.id",
+                    value: "${permTreeNodeRecord2Id}"
+                }
+            ]`);
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record2Id);
+        });
+
+        describe('except current user group (admin)', () => {
+            beforeAll(async () => {
+                await makeGraphQlCall(
+                    `mutation {
+                    savePermission(
+                        permission: {
+                            type: record,
+                            applyTo: "${libName}",
+                            usersGroup: "${adminsGroupId}",
+                            permissionTreeTarget: {
+                                tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                            },
+                            actions: [
+                                {name: ${RecordPermissionsActions.ACCESS_RECORD}, allowed: true},
+                            ]
+                        }
+                    ) { 
+                        type
+                    }
+                }`,
+                    true
+                );
+            });
+
+            afterAll(async () => {
+                await makeGraphQlCall(
+                    `mutation {
+                    savePermission(
+                        permission: {
+                            type: record,
+                            applyTo: "${libName}",
+                            usersGroup: "${adminsGroupId}",
+                            permissionTreeTarget: {
+                                tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                            },
+                            actions: [
+                                {name: ${RecordPermissionsActions.ACCESS_RECORD}, allowed: null},
+                            ]
+                        }
+                    ) { 
+                        type
+                    }
+                }`,
+                    true
+                );
+            });
+
+            // To improve those test, would be fine to be able to make call as another user than admin
+            it('record should get all records', async () => {
+                const records = await getLibRecords();
+
+                expect(records.length).toBe(2);
+            });
+        });
+    });
+
+    describe('disable access_record_by_default on node 1 for everybody', () => {
+        beforeAll(async () => {
+            await makeGraphQlCall(
+                `mutation {
+                savePermission(
+                    permission: {
+                        type: record,
+                        applyTo: "${libName}",
+                        usersGroup: null,
+                        permissionTreeTarget: {
+                            tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                        },
+                        actions: [
+                            {name: ${RecordPermissionsActions.ACCESS_RECORD_BY_DEFAULT}, allowed: false},
+                        ]
+                    }
+                ) { 
+                    type
+                }
+            }`,
+                true
+            );
+        });
+
+        afterAll(async () => {
+            await makeGraphQlCall(
+                `mutation {
+                savePermission(
+                    permission: {
+                        type: record,
+                        applyTo: "${libName}",
+                        usersGroup: null,
+                        permissionTreeTarget: {
+                            tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                        },
+                        actions: [
+                            {name: ${RecordPermissionsActions.ACCESS_RECORD_BY_DEFAULT}, allowed: null},
+                        ]
+                    }
+                ) { 
+                    type
+                }
+            }`,
+                true
+            );
+        });
+
+        it('record should get only one of them', async () => {
+            const records = await getLibRecords();
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record2Id);
+        });
+
+        it('record should get hidden one with filter on node 1', async () => {
+            const records = await getLibRecords(`[
+                {
+                    condition: ${AttributeCondition.EQUAL},
+                    field: "${libTreeAttr}.${permNodeLibName}.id",
+                    value: "${permTreeNodeRecord1Id}"
+                }
+            ]`);
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record1Id);
+        });
+
+        it('record should get one of them with filter on node 2', async () => {
+            const records = await getLibRecords(`[
+                {
+                    condition: ${AttributeCondition.EQUAL},
+                    field: "${libTreeAttr}.${permNodeLibName}.id",
+                    value: "${permTreeNodeRecord2Id}"
+                }
+            ]`);
+
+            expect(records.length).toBe(1);
+            expect(records[0].id).toBe(record2Id);
+        });
+
+        describe('except current user group (admin)', () => {
+            beforeAll(async () => {
+                await makeGraphQlCall(
+                    `mutation {
+                    savePermission(
+                        permission: {
+                            type: record,
+                            applyTo: "${libName}",
+                            usersGroup: "${adminsGroupId}",
+                            permissionTreeTarget: {
+                                tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                            },
+                            actions: [
+                                {name: ${RecordPermissionsActions.ACCESS_RECORD_BY_DEFAULT}, allowed: true},
+                            ]
+                        }
+                    ) { 
+                        type
+                    }
+                }`,
+                    true
+                );
+            });
+
+            afterAll(async () => {
+                await makeGraphQlCall(
+                    `mutation {
+                    savePermission(
+                        permission: {
+                            type: record,
+                            applyTo: "${libName}",
+                            usersGroup: "${adminsGroupId}",
+                            permissionTreeTarget: {
+                                tree: "${permTreeName}", nodeId: "${permTreeNode1Id}"
+                            },
+                            actions: [
+                                {name: ${RecordPermissionsActions.ACCESS_RECORD_BY_DEFAULT}, allowed: null},
+                            ]
+                        }
+                    ) { 
+                        type
+                    }
+                }`,
+                    true
+                );
+            });
+
+            // To improve those test, would be fine to be able to make call as another user than admin
+            it('record should get all records', async () => {
+                const records = await getLibRecords();
+
+                expect(records.length).toBe(2);
+            });
+        });
+    });
+
+    async function getLibRecords(filters?: string) {
+        const query = `query {
+            records(
+                library: "${libName}",
+                filters: ${filters ?? '[]'},
+            ) {
+                list {
+                    id
+                }
+            }
+        }`;
+        const res = await makeGraphQlCall(query, true);
+
+        expect(res.status).toBe(200);
+
+        return res.data.data.records.list;
+    }
+});
