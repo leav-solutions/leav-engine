44274b8debdcae408288798e0363f5f1f3761434
Sébastien BERTHIER <s.berthier@aristid.com>
2025-10-28 09:11:33 +0100

    fix(core): websocketChannel imports
    !1280

--- apps/core/src/domain/notification/channels/webSocketChannel.ts
+++ apps/core/src/domain/notification/channels/webSocketChannel.ts
@@ -5,7 +5,7 @@ import {logger} from '@leav/logger';
 import {type INotification, type INotificationChannel, NotificationChannels} from '../../../_types/notification';
 import {type IQueryInfos} from '_types/queryInfos';
 import {type IEventsManagerDomain} from 'domain/eventsManager/eventsManagerDomain';
-import {type IPubSubNotificationData, TriggerNames} from '_types/eventsManager';
+import {type IPubSubNotificationData, TriggerNames} from '../../../_types/eventsManager';
 
 export interface INotificationByWebSocketChannelDeps {
     'core.domain.eventsManager': IEventsManagerDomain;
