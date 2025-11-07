01216774cff81da5057e7b2c4cac4e19202cb196
Jérémy Bel <j.bel@aristid.com>
2025-11-07 14:19:14 +0100

    feat(toLowercase): implement toLowercase action with tests

---
+++ apps/core/src/domain/actions/toLowercaseAction.spec.ts
@@ -0,0 +1,18 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {AttributeFormats, AttributeTypes} from '../../_types/attribute';
+import toLowercaseAction from './toLowercaseAction';
+
+describe('toLowercaseAction', () => {
+    const action = toLowercaseAction().action;
+    const ctx = {
+        attribute: {id: 'test_attr', format: AttributeFormats.TEXT, type: AttributeTypes.SIMPLE},
+        userId: 'test_user',
+    };
+
+    test('toLowercase', async () => {
+        expect((await action([{payload: 'AZERTY'}], {}, ctx)).values[0].payload).toBe('azerty');
+        expect((await action([{payload: null}], {}, ctx)).values[0].payload).toBe(null);
+    });
+});
---
+++ apps/core/src/domain/actions/toLowercaseAction.ts
@@ -0,0 +1,25 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {ActionsListIOTypes, type IActionsListFunction} from '../../_types/actionsList';
+
+export default function (): IActionsListFunction {
+    return {
+        id: 'toLowercase',
+        name: 'To Lowercase',
+        description: 'Convert the string to lowercase',
+        input_types: [ActionsListIOTypes.STRING],
+        output_types: [ActionsListIOTypes.STRING],
+        compute: false,
+        action: values => {
+            const computedValues = values.map(elementValue => {
+                elementValue.payload =
+                    elementValue.payload !== null ? (elementValue.payload as string).toLowerCase() : null;
+
+                return elementValue;
+            });
+
+            return {values: computedValues, errors: []};
+        },
+    };
+}
