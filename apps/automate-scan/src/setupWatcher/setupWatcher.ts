4681aaf1a615f8913a460dd96565036332e5c255
Sébastien Berthier <s.berthier@aristid.com>
2025-11-26 13:35:39 +0100

    chore(automate-scan): update redis client

--- apps/automate-scan/src/setupWatcher/setupWatcher.ts
+++ apps/automate-scan/src/setupWatcher/setupWatcher.ts
@@ -23,7 +23,7 @@ export const startWatch = async () => {
     // or we create a hash of the rootPath if no rootKey
     const rootKey = config.rootKey || Crypto.createHash('md5').update(config.rootPath).digest('hex');
 
-    createClient(config.redis.host, config.redis.port);
+    await createClient(config.redis.host, config.redis.port);
 
     if (config.amqp) {
         const amqpConfig: Options.Connect = {
