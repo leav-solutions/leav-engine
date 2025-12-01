7a5f200d99a4b144cd8d6f2019904f34ca39a4f1
Philippe Chevieux <philippe.chevieux@gmail.com>
2025-12-01 10:38:04 +0100

    test(app-studio): include missing currentPanel mock and add test to check if cta can be displayed

--- apps/app-studio/src/modules/ApplicationRouting/header/__tests__/PanelHeader.spec.tsx
+++ apps/app-studio/src/modules/ApplicationRouting/header/__tests__/PanelHeader.spec.tsx
@@ -52,7 +52,15 @@ describe('PanelHeader', () => {
 
     it('should display RecordIdCard component when panel is type record', async () => {
         spyUseParams.mockReturnValue({recordId: '1234567890'});
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
+        spyRetrievePanelDetails.mockReturnValue({
+            libraryId: 'test',
+            panelType: 'recordPanels',
+            currentPanel: {
+                id: 'panel1',
+                type: 'editionForm',
+                formId: 'edition',
+            },
+        });
         spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
 
         render(<PanelHeader enabled />);
@@ -70,7 +78,15 @@ describe('PanelHeader', () => {
     });
 
     it('should display RecordIdCard component when panel is type record and recordId is providing by parent', async () => {
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
+        spyRetrievePanelDetails.mockReturnValue({
+            libraryId: 'test',
+            panelType: 'recordPanels',
+            currentPanel: {
+                id: 'panel1',
+                type: 'editionForm',
+                formId: 'edition',
+            },
+        });
         spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
 
         render(<PanelHeader enabled currentRecordId="1234567890" />);
@@ -145,133 +161,241 @@ describe('PanelHeader', () => {
         expect(container.firstChild).toBeNull();
     });
 
-    it('should display expand button and navigate to popup when clicked in slider', async () => {
-        const mockNavigate = jest.fn();
-        spyUseNavigate.mockReturnValue(mockNavigate);
-        spyUseParams.mockReturnValue({
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-            where: 'slider',
-        });
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
-        spyGeneratePath.mockReturnValue('../../../1234567890/popup/panel123');
-
-        const user = userEvent.setup();
-
-        render(<PanelHeader enabled />);
-
-        const expandButton = screen.getByRole('button', {name: /expand/i});
-        expect(expandButton).toBeInTheDocument();
-
-        await user.click(expandButton);
-
-        expect(spyGeneratePath).toHaveBeenCalledWith('../../../:recordId/popup/:recordPanelId', {
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-        });
-        expect(mockNavigate).toHaveBeenCalledWith('../../../1234567890/popup/panel123', {relative: 'path'});
-    });
-
-    it('should display collapse button and navigate to slider when clicked in popup', async () => {
-        const mockNavigate = jest.fn();
-        spyUseNavigate.mockReturnValue(mockNavigate);
-        spyUseParams.mockReturnValue({
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-            where: 'popup',
-        });
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
-        spyGeneratePath.mockReturnValue('../../../1234567890/slider/panel123');
-
-        const user = userEvent.setup();
-
-        render(<PanelHeader enabled />);
-
-        const collapseButton = screen.getByRole('button', {name: /collapse/i});
-        expect(collapseButton).toBeInTheDocument();
-
-        await user.click(collapseButton);
-
-        expect(spyGeneratePath).toHaveBeenCalledWith('../../../:recordId/slider/:recordPanelId', {
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-        });
-        expect(mockNavigate).toHaveBeenCalledWith('../../../1234567890/slider/panel123', {relative: 'path'});
-    });
-
-    it('should not display expand/collapse button when in fullpage', async () => {
-        spyUseParams.mockReturnValue({
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-            where: 'fullpage',
-        });
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
-
-        render(<PanelHeader enabled />);
-
-        const expandButton = screen.queryByRole('button', {name: /expand/i});
-        const collapseButton = screen.queryByRole('button', {name: /collapse/i});
-        expect(expandButton).not.toBeInTheDocument();
-        expect(collapseButton).not.toBeInTheDocument();
-    });
-
-    it('should not display expand/collapse button when hideExpandCollapseButton is true', async () => {
-        spyUseParams.mockReturnValue({
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-            where: 'slider',
-        });
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
-
-        render(<PanelHeader enabled hideExpandCollapseButton />);
-
-        const expandButton = screen.queryByRole('button', {name: /expand/i});
-        const collapseButton = screen.queryByRole('button', {name: /collapse/i});
-        expect(expandButton).not.toBeInTheDocument();
-        expect(collapseButton).not.toBeInTheDocument();
-    });
-
-    it('should display OpenFlapButton components for record panels', async () => {
-        spyUseParams.mockReturnValue({
-            recordId: '1234567890',
-            recordPanelId: 'panel123',
-            where: 'slider',
-        });
-        spyRetrievePanelDetails.mockReturnValue({libraryId: 'test', panelType: 'recordPanels', currentPanel: null});
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
-
-        render(<PanelHeader enabled />);
-
-        const informationButton = screen.queryByRole('button', {name: /information/i});
-        const discussionButton = screen.queryByRole('button', {name: /discussion/i});
-        expect(informationButton).toBeInTheDocument();
-        expect(discussionButton).toBeInTheDocument();
-    });
-
-    it('should not display OpenFlapButton components for library panels', async () => {
-        spyRetrievePanelDetails.mockReturnValue({
-            libraryId: 'test',
-            panelType: 'libraryPanels',
-            currentPanel: {
-                id: '1',
-                name: {
-                    fr: 'un',
+    describe('Expand/Collapse Button', () => {
+        it('should display ExpandCollapseCurrentPanelButton component and navigate to popup when clicked in slider', async () => {
+            const mockNavigate = jest.fn();
+            spyUseNavigate.mockReturnValue(mockNavigate);
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+                where: 'slider',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'panel123',
+                    type: 'editionForm',
+                    formId: 'edition',
                 },
-                type: 'explorer',
-                actions: [],
-            },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+            spyGeneratePath.mockReturnValue('../../../1234567890/popup/panel123');
+
+            const user = userEvent.setup();
+
+            render(<PanelHeader enabled />);
+
+            const expandButton = screen.getByRole('button', {name: /expand/i});
+            expect(expandButton).toBeInTheDocument();
+
+            await user.click(expandButton);
+
+            expect(spyGeneratePath).toHaveBeenCalledWith('../../../:recordId/popup/:recordPanelId', {
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+            });
+            expect(mockNavigate).toHaveBeenCalledWith('../../../1234567890/popup/panel123', {relative: 'path'});
         });
-        spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
 
-        render(<PanelHeader enabled />);
+        it('should display ExpandCollapseCurrentPanelButton component and navigate to slider when clicked in popup', async () => {
+            const mockNavigate = jest.fn();
+            spyUseNavigate.mockReturnValue(mockNavigate);
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+                where: 'popup',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'panel123',
+                    type: 'editionForm',
+                    formId: 'edition',
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+            spyGeneratePath.mockReturnValue('../../../1234567890/slider/panel123');
 
-        const informationButton = screen.queryByRole('button', {name: /information/i});
-        const discussionButton = screen.queryByRole('button', {name: /discussion/i});
-        expect(informationButton).not.toBeInTheDocument();
-        expect(discussionButton).not.toBeInTheDocument();
+            const user = userEvent.setup();
+
+            render(<PanelHeader enabled />);
+
+            const collapseButton = screen.getByRole('button', {name: /collapse/i});
+            expect(collapseButton).toBeInTheDocument();
+
+            await user.click(collapseButton);
+
+            expect(spyGeneratePath).toHaveBeenCalledWith('../../../:recordId/slider/:recordPanelId', {
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+            });
+            expect(mockNavigate).toHaveBeenCalledWith('../../../1234567890/slider/panel123', {relative: 'path'});
+        });
+
+        it('should not display ExpandCollapseCurrentPanelButton component when in fullpage', async () => {
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+                where: 'fullpage',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'panel123',
+                    type: 'editionForm',
+                    formId: 'edition',
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled />);
+
+            const expandButton = screen.queryByRole('button', {name: /expand/i});
+            const collapseButton = screen.queryByRole('button', {name: /collapse/i});
+            expect(expandButton).not.toBeInTheDocument();
+            expect(collapseButton).not.toBeInTheDocument();
+        });
+
+        it('should not display ExpandCollapseCurrentPanelButton component when hideExpandCollapseButton is true', async () => {
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+                where: 'slider',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'panel123',
+                    type: 'editionForm',
+                    formId: 'edition',
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled hideExpandCollapseButton />);
+
+            const expandButton = screen.queryByRole('button', {name: /expand/i});
+            const collapseButton = screen.queryByRole('button', {name: /collapse/i});
+            expect(expandButton).not.toBeInTheDocument();
+            expect(collapseButton).not.toBeInTheDocument();
+        });
+
+        it('should not display ExpandCollapseCurrentPanelButton component when panel is a creation form', async () => {
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'creation_panel',
+                where: 'slider',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'creation_panel',
+                    type: 'creationForm',
+                    formId: 'creation',
+                    attributeSource: 'pac_campaigns_list',
+                    name: {
+                        en: 'Create',
+                        fr: 'Créer une campagne',
+                    },
+                    isStandalone: true,
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled />);
+
+            const expandButton = screen.queryByRole('button', {name: /expand/i});
+            const collapseButton = screen.queryByRole('button', {name: /collapse/i});
+            expect(expandButton).not.toBeInTheDocument();
+            expect(collapseButton).not.toBeInTheDocument();
+        });
+    });
+
+    describe('Toggle Flap Buttons', () => {
+        it('should display ToggleFlapButton components for record panels', async () => {
+            spyUseParams.mockReturnValue({
+                recordId: '1234567890',
+                recordPanelId: 'panel123',
+                where: 'slider',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'panel123',
+                    type: 'editionForm',
+                    formId: 'edition',
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled />);
+
+            const informationButton = screen.queryByRole('button', {name: /information/i});
+            const discussionButton = screen.queryByRole('button', {name: /discussion/i});
+            expect(informationButton).toBeInTheDocument();
+            expect(discussionButton).toBeInTheDocument();
+        });
+
+        it('should not display ToggleFlapButton components for library panels', async () => {
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'test',
+                panelType: 'libraryPanels',
+                currentPanel: {
+                    id: '1',
+                    name: {
+                        fr: 'un',
+                    },
+                    type: 'explorer',
+                    actions: [],
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled />);
+
+            const informationButton = screen.queryByRole('button', {name: /information/i});
+            const discussionButton = screen.queryByRole('button', {name: /discussion/i});
+            expect(informationButton).not.toBeInTheDocument();
+            expect(discussionButton).not.toBeInTheDocument();
+        });
+
+        it('should not display ToggleFlapButton components for creation form panels', async () => {
+            spyUseParams.mockReturnValue({
+                recordId: 'newRecord',
+                recordPanelId: 'creation_panel',
+                where: 'slider',
+            });
+            spyRetrievePanelDetails.mockReturnValue({
+                libraryId: 'campaigns',
+                panelType: 'recordPanels',
+                currentPanel: {
+                    id: 'creation_panel',
+                    type: 'creationForm',
+                    formId: 'creation',
+                    attributeSource: 'pac_campaigns_list',
+                    name: {
+                        en: 'Create',
+                        fr: 'Créer une campagne',
+                    },
+                    isStandalone: true,
+                },
+            });
+            spyUseApplicationSettingsContext.mockReturnValue([emptyApplication] as any);
+
+            render(<PanelHeader enabled />);
+
+            const informationButton = screen.queryByRole('button', {name: /information/i});
+            const discussionButton = screen.queryByRole('button', {name: /discussion/i});
+            const expandButton = screen.queryByRole('button', {name: /expand/i});
+            expect(informationButton).not.toBeInTheDocument();
+            expect(discussionButton).not.toBeInTheDocument();
+            expect(expandButton).not.toBeInTheDocument();
+        });
     });
 });
