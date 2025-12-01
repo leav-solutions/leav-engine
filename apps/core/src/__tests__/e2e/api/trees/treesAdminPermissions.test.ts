f93c070e7612a3034741e80d376f690e413db2ee
Jérémy Bel <j.bel@aristid.com>
2025-12-01 13:39:23 +0000

    feat(core): add admin permissions e2e tests for tree operations
    !1393

---
+++ apps/core/src/__tests__/e2e/api/trees/treesAdminPermissions.test.ts
@@ -0,0 +1,87 @@
+// Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
+// This file is released under LGPL V3
+// License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+import {e2eAdminUser, e2eGuestUser, makeGraphQlCall} from '../e2eUtils';
+
+describe('TreesAdminPermissions', () => {
+    const treeId = 'trees_admin_permissions_tree_id';
+
+    beforeAll(async () => {
+        const res = await makeGraphQlCall(`mutation {
+            saveTree(
+                tree: {
+                    id: "${treeId}",
+                }
+            ) {
+                id
+            }
+        }`);
+
+        expect(res.status).toBe(200);
+        expect(res.data.data.saveTree.id).toBe(treeId);
+    });
+
+    describe('create tree', () => {
+        it('Should not be authorized to create a tree', async () => {
+            const gqlMutation = `mutation {
+                saveTree(
+                   tree: {
+                        id: "trees_admin_permissions_new_tree_id",
+                    }
+                ) {
+                    id
+                }
+            }`;
+
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eGuestUser()})).rejects.toThrow(/Action forbidden/);
+        });
+
+        it('Should be authorized to create a tree', async () => {
+            const gqlMutation = `mutation {
+                saveTree(
+                    tree: {
+                        id: "trees_admin_permissions_new_tree_id",
+                    }
+                ) {
+                    id
+                }
+            }`;
+
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eAdminUser()})).resolves.toBeDefined();
+        });
+    });
+
+    describe('edit tree', () => {
+        it('Should not be authorized to edit a tree', async () => {
+            const gqlMutation = `mutation {
+                saveTree(tree: {id: "${treeId}", label: {en: "new label"}}) {
+                    id
+                }
+            }`;
+
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eGuestUser()})).rejects.toThrow(/Action forbidden/);
+        });
+
+        it('Should be authorized to edit a tree', async () => {
+            const gqlMutation = `mutation {
+                saveTree(tree: {id: "${treeId}", label: {en: "new label"}}) {
+                    id
+                }
+            }`;
+
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eAdminUser()})).resolves.toBeDefined();
+        });
+    });
+
+    describe('delete tree', () => {
+        it('Should not be authorized to delete a tree', async () => {
+            const gqlMutation = `mutation { deleteTree(id: "${treeId}") { id } }`;
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eGuestUser()})).rejects.toThrow(/Action forbidden/);
+        });
+
+        it('Should be authorized to delete a tree', async () => {
+            const gqlMutation = `mutation { deleteTree(id: "${treeId}") { id } }`;
+            await expect(makeGraphQlCall(gqlMutation, {user: e2eAdminUser()})).resolves.toBeDefined();
+        });
+    });
+});
