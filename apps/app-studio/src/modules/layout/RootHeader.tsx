068221ee68d14c4cb5ffc4faae949d725c5534f4
Philippe Chevieux <philippe.chevieux@gmail.com>
2025-12-10 10:55:08 +0100

    feat(app-studio): add toggle activity center button that display an empty sidepanel

--- apps/app-studio/src/modules/layout/RootHeader.tsx
+++ apps/app-studio/src/modules/layout/RootHeader.tsx
@@ -11,7 +11,8 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faRightFromBracket, faUser} from '@fortawesome/free-solid-svg-icons';
 import {LanguageSelector} from '../switch-language/LanguageSelector';
 import {header, logo} from './layout.module.css';
-import {APP_BASE_URL, GLOBAL_BASE_URL} from '../../constants';
+import {APP_BASE_URL} from '../../constants';
+import {ToggleActivityCenterButton} from '../../modules/activity-center/ToggleActivityCenterButton';
 
 export const RootHeader: FunctionComponent = () => {
     const {t} = useTranslation();
@@ -61,7 +62,12 @@ export const RootHeader: FunctionComponent = () => {
                     />
                 </Link>
             }
-            profile={<KitHeader.Profile menu={profileMenuContent} profileCardProps={{avatarProps, title: identity}} />}
+            extraRight={
+                <>
+                    <ToggleActivityCenterButton />
+                    <KitHeader.Profile menu={profileMenuContent} profileCardProps={{avatarProps, title: identity}} />
+                </>
+            }
             langSwitcher={<LanguageSelector />}
         />
     );
