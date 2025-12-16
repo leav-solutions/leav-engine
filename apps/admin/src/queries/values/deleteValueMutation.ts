fec3e9171d2b67c6ccd06ca85973716419e6abd2
Sébastien Berthier <s.berthier@aristid.com>
2025-12-16 18:02:52 +0100

    chore(admin): graphql-generate cmd

--- apps/admin/src/queries/values/deleteValueMutation.ts
+++ apps/admin/src/queries/values/deleteValueMutation.ts
@@ -5,7 +5,7 @@ import gql from 'graphql-tag';
 
 export const deleteValueQuery = gql`
     mutation DELETE_VALUE($library: ID!, $recordId: ID!, $attribute: ID!, $valueId: ID) {
-        deleteValue(library: $library, recordId: $recordId, attribute: $attribute, valueId: $valueId) {
+        deleteValue(library: $library, recordId: $recordId, attribute: $attribute, value: {id_value: $valueId}) {
             attribute {
                 id
             }
