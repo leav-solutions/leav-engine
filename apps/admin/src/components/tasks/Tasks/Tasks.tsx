90097cd191b56ce0ac420d8b60f403c41cbf50c7
Sébastien Berthier <s.berthier@aristid.com>
2025-12-17 16:34:23 +0100

    refactor(admin): use apollo client useSubscription from new generated code

--- apps/admin/src/components/tasks/Tasks/Tasks.tsx
+++ apps/admin/src/components/tasks/Tasks/Tasks.tsx
@@ -1,7 +1,6 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {useSubscription} from '@apollo/client';
 import {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {useAppDispatch, useAppSelector} from 'reduxStore/store';
@@ -10,13 +9,17 @@ import {Button, Header, Icon, Tab} from 'semantic-ui-react';
 import styled from 'styled-components';
 import {type GET_TASKS_tasks_list} from '_gqlTypes/GET_TASKS';
 import useUserData from '../../../hooks/useUserData';
-import {subTaskUpdates} from '../../../queries/tasks/subTaskUpdates';
 import {TaskStatus} from '../../../_gqlTypes/globalTypes';
 import CancelTask from '../CancelTask';
 import DeleteAllTasks from '../DeleteAllTasks';
 import DeleteTask from '../DeleteTask';
 import TasksList from '../TasksList';
-import {useCancelTaskMutation, useDeleteTasksMutation, useGetTasksQuery} from '_gqlTypes';
+import {
+    useCancelTaskMutation,
+    useDeleteTasksMutation,
+    useGetTasksQuery,
+    useSubTasksUpdateSubscription,
+} from '_gqlTypes';
 
 const Title = styled(Header)`
     display: flex;
@@ -57,10 +60,10 @@ const Tasks = (): JSX.Element => {
         },
     });
 
-    useSubscription(subTaskUpdates, {
+    useSubTasksUpdateSubscription({
         onSubscriptionData: subData => {
             const task = {...subData.subscriptionData.data.task};
-            dispatch(addTask(task));
+            dispatch(addTask(task as GET_TASKS_tasks_list));
         },
     });
 
