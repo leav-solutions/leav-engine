4e11adeed386324ee33ed57279d7d9149f413dab
Philippe Chevieux <p.chevieux@aristid.com>
2025-12-18 16:44:17 +0100

    feat(app-studio): add archive user tasks - LEAVC-526
    !1482

--- apps/app-studio/src/modules/activity-center/ActivityCenter.tsx
+++ apps/app-studio/src/modules/activity-center/ActivityCenter.tsx
@@ -1,8 +1,8 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
+// import {NotificationsList} from './notifications/NotificationsList';
 import {useTranslation} from 'react-i18next';
-import {NotificationsList} from './notifications/NotificationsList';
 import {TasksList} from './tasks/TasksList';
 import {KitTabs} from 'aristid-ds';
 import {type IKitTabItem} from 'aristid-ds/dist/Kit/DataDisplay/Tabs/types';
@@ -12,11 +12,12 @@ export const ActivityCenter = () => {
     const {t} = useTranslation();
 
     const tabsItems: IKitTabItem[] = [
-        {
-            key: 'notifications',
-            label: t('activity_center.notifications'),
-            tabContent: <NotificationsList />,
-        },
+        // TODO: Uncomment when notifications are implemented
+        // {
+        //     key: 'notifications',
+        //     label: t('activity_center.notifications'),
+        //     tabContent: <NotificationsList />,
+        // },
         {
             key: 'tasks',
             label: t('activity_center.long_tasks'),
