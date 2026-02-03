b1790eef746fa8d55b602be388c99c60aed9a99d
Jérémy Bel <j.bel@aristid.com>
2026-02-03 11:02:48 +0100

    Refactor CRUD e2e tests for views to improve clarity

--- apps/core/src/__tests__/e2e/api/views/views.test.ts
+++ apps/core/src/__tests__/e2e/api/views/views.test.ts
@@ -7,123 +7,92 @@ import {ViewSizes, ViewTypes} from '../../../../_types/views';
 
 describe('Views', () => {
     const testLibName = 'test_views_lib';
+    let viewId: string;
 
     beforeAll(async () => {
         await gqlSaveLibrary(testLibName, 'Test Lib');
     });
 
-    test('CRUD', async () => {
-        // Create view
-        const resSaveView = await makeGraphQlCall(`mutation {
-            v1: saveView(view: {
-              library: "${testLibName}",
-              display: {type: ${ViewTypes.LIST}, size: ${ViewSizes.MEDIUM}},
-              shared: true,
-              label: {en: "test_first_view"},
-              description: {en: "Best view ever!"},
-              color: "#FFFFFF",
-              filters: [
-                {field: "label", value: "Test", condition: ${AttributeCondition.EQUAL}}
-              ],
-              sort: {field: "created_at", order: asc}
-            }) {
-              id
-            },
-            v2: saveView(view: {
-                library: "${testLibName}",
-                display: {type: ${ViewTypes.LIST}, size: ${ViewSizes.MEDIUM}},
-                shared: true,
-                label: {en: "test_second_view"},
-                description: {en: "Best view ever!"},
-                color: "#FFFFFF",
-                filters: [
-                  {field: "label", value: "Test", condition: ${AttributeCondition.EQUAL}}
-                ],
-                sort: {field: "created_at", order: asc}
-              }) {
-                id
-              }
-          }
-        `);
+    describe('CRUD operations', () => {
+        test('Create view', async () => {
+            const resSaveView = await makeGraphQlCall(`mutation {
+                saveView(view: {
+                  library: "${testLibName}",
+                  display: {type: ${ViewTypes.LIST}, size: ${ViewSizes.MEDIUM}},
+                  shared: true,
+                  label: {en: "test_first_view"},
+                  description: {en: "Best view ever!"},
+                  color: "#FFFFFF",
+                  filters: [
+                    {field: "label", value: "Test", condition: ${AttributeCondition.EQUAL}}
+                  ],
+                  sort: {field: "created_at", order: asc}
+                }) {
+                  id
+                }
+            }`);
 
-        const viewId = resSaveView.data.data.v1.id;
-        expect(resSaveView.status).toBe(200);
-        expect(resSaveView.data.errors).toBeUndefined();
-        expect(viewId).toBeTruthy();
+            viewId = resSaveView.data.data.saveView.id;
 
-        // Get view
-        const resGetViews = await makeGraphQlCall(`{
-            views(library: "${testLibName}") {
-              totalCount
-              list {
-                id
-                created_by { whoAmI { id }}
-                modified_at
-                created_at
-                shared
-                label
-                description
-                color
-                filters {field value condition operator}
-                sort {field order}
-                attributes {id}
-              }
-            }
-          }`);
+            expect(resSaveView.status).toBe(200);
+            expect(resSaveView.data.errors).toBeUndefined();
+            expect(viewId).toBeTruthy();
+        });
 
-        expect(resGetViews.status).toBe(200);
-        expect(resGetViews.data.errors).toBeUndefined();
-        expect(resGetViews.data.data.views.list.length).toBeGreaterThanOrEqual(2);
-        expect(resGetViews.data.data.views.list[0].created_by.whoAmI.id).toBeTruthy();
+        test('Get views', async () => {
+            const resGetViews = await makeGraphQlCall(`{
+                views(library: "${testLibName}") {
+                  totalCount
+                  list {
+                    id
+                    created_by { whoAmI { id }}
+                    modified_at
+                    created_at
+                    shared
+                    label
+                    description
+                    color
+                    filters {field value condition operator}
+                    sort {field order}
+                    attributes {id}
+                  }
+                }
+            }`);
 
-        // Update view
-        const resUpdateView = await makeGraphQlCall(`mutation {
-            saveView(view: {
-              id: "${viewId}",
-              library: "${testLibName}",
-              display: {type: ${ViewTypes.LIST}, size: ${ViewSizes.MEDIUM}},
-              shared: true,
-              label: {en: "My super view!"},
-              description: {en: "Best view ever!"},
-              color: "#000000",
-              filters: [
-                {field: "label", value: "Test", condition: EQUAL}
-              ],
-              sort: {field: "created_at", order: asc}
-              attributes: ["label"]
-            }) {
-              id
-              library
-              display {
-                type
-                size
-              }
-              label
-              description
-              color
-              filters {field value condition operator}
-              sort {field order}
-              attributes {
-                id
-              }
-            }
-          }
-        `);
+            expect(resGetViews.status).toBe(200);
+            expect(resGetViews.data.errors).toBeUndefined();
+            expect(resGetViews.data.data.views.list.length).toBeGreaterThanOrEqual(1);
+            expect(resGetViews.data.data.views.list[0].created_by.whoAmI.id).toBeTruthy();
+        });
 
-        expect(resUpdateView.status).toBe(200);
-        expect(resUpdateView.data.errors).toBeUndefined();
-        expect(resUpdateView.data.data.saveView.color).toBe('#000000');
-        expect(resUpdateView.data.data.saveView.attributes).toEqual([{id: 'label'}]);
+        test('Update view', async () => {
+            const resUpdateView = await makeGraphQlCall(`mutation {
+                updateView(view: {
+                  id: "${viewId}",
+                  display: {type: ${ViewTypes.LIST}},
+                }) {
+                  id
+                  display {
+                    type
+                  }
+                }
+            }`);
 
-        // Delete view
-        const resDeleteView = await makeGraphQlCall(`mutation {
-            deleteView(viewId: "${viewId}") {
-                id
-            }
-        }`);
+            expect(resUpdateView.status).toBe(200);
+            expect(resUpdateView.data.errors).toBeUndefined();
+            expect(resUpdateView.data.data.updateView.id).toBe(viewId);
+        });
 
-        expect(resDeleteView.status).toBe(200);
-        expect(resDeleteView.data.errors).toBeUndefined();
-        expect(resDeleteView.data.data.deleteView.id).toBe(viewId);
+        test('Delete view', async () => {
+            const resDeleteView = await makeGraphQlCall(`mutation {
+                deleteView(viewId: "${viewId}") {
+                    id
+                }
+            }`);
+
+            expect(resDeleteView.status).toBe(200);
+            expect(resDeleteView.data.errors).toBeUndefined();
+            expect(resDeleteView.data.data.deleteView.id).toBe(viewId);
+        });
     });
 });
