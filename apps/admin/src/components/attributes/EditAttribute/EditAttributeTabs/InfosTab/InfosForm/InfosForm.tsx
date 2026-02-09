bfa196d7bc16d1d4b0679e29934a65f4378f6ebb
Jérémy Bel <j.bel@aristid.com>
2026-02-09 15:55:06 +0100

    add purgeMultipleValues mutation and integrate into InfosForm for admin attribute edition

--- apps/admin/src/components/attributes/EditAttribute/EditAttributeTabs/InfosTab/InfosForm/InfosForm.tsx
+++ apps/admin/src/components/attributes/EditAttribute/EditAttributeTabs/InfosTab/InfosForm/InfosForm.tsx
@@ -6,7 +6,7 @@ import VersionProfilesSelector from 'components/versionProfiles/VersionProfilesS
 import {Formik, type FormikProps} from 'formik';
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {Form, Icon, Message} from 'semantic-ui-react';
+import {Button, Form, Icon, Message} from 'semantic-ui-react';
 import styled from 'styled-components';
 import * as yup from 'yup';
 import {type GET_ATTRIBUTES_attributes_list} from '_gqlTypes/GET_ATTRIBUTES';
@@ -18,7 +18,13 @@ import {
 } from '_gqlTypes/GET_ATTRIBUTE_BY_ID';
 import useLang from '../../../../../../hooks/useLang';
 import {formatIDString, getFieldError} from '../../../../../../utils';
-import {AttributeFormat, AttributeType, ValueVersionMode, MultiDisplayOption} from '_gqlTypes';
+import {
+    AttributeFormat,
+    AttributeType,
+    ValueVersionMode,
+    MultiDisplayOption,
+    usePurgeMultipleValuesMutation,
+} from '_gqlTypes';
 import {ErrorTypes, type IFormError} from '../../../../../../_types/errors';
 import LibrariesSelector from '../../../../../libraries/LibrariesSelector';
 import FormFieldWrapper from '../../../../../shared/FormFieldWrapper';
@@ -91,6 +97,8 @@ function InfosForm({
 }: IInfosFormProps): JSX.Element {
     const {t} = useTranslation();
     const {lang: userLang, availableLangs, defaultLang} = useLang();
+
+    const [purgeMultipleValues, {loading: purgeMultipleValuesLoading}] = usePurgeMultipleValuesMutation();
 
     const isNewAttribute = attribute === null;
     const initialValues: AttributeInfosFormValues =
@@ -202,6 +210,16 @@ function InfosForm({
             }
         };
 
+        const _handlePurgeMultipleValues = async () => {
+            if (window.confirm(t('attributes.purge_multiple_values_confirm'))) {
+                await purgeMultipleValues({
+                    variables: {
+                        attributeId: attribute.id,
+                    },
+                });
+            }
+        };
+
         const allowFormat = [AttributeType.advanced, AttributeType.simple].includes(values.type);
         const allowMultipleValues = [AttributeType.advanced, AttributeType.advanced_link, AttributeType.tree].includes(
             values.type,
@@ -216,6 +234,9 @@ function InfosForm({
         const isTextAttribute = [AttributeFormat.text, AttributeFormat.rich_text].includes(values.format);
         const isMultiValuesLinkAttribute = AttributeType.advanced_link === values.type && !!values.multiple_values;
         const isMultiValuesTreeAttribute = isTreeAttribute && !!values.multiple_values;
+        const purgeMultipleValuesActionAvailable =
+            [AttributeType.advanced, AttributeType.advanced_link, AttributeType.tree].includes(values.type) &&
+            !attribute?.multiple_values;
 
         const _getErrorByField = (fieldName: string): string =>
             getFieldError<GET_ATTRIBUTES_attributes_list>(
@@ -449,6 +470,18 @@ function InfosForm({
                             onBlur={_handleBlur}
                             checked={!!values.multiple_values}
                         />
+                    </FormFieldWrapper>
+                )}
+                {purgeMultipleValuesActionAvailable && (
+                    <FormFieldWrapper>
+                        <Button
+                            color="red"
+                            onClick={_handlePurgeMultipleValues}
+                            loading={purgeMultipleValuesLoading}
+                            aria-label="purge_multiple_values"
+                        >
+                            <Icon name="trash alternate outline" /> {t('attributes.purge_multiple_values')}
+                        </Button>
                     </FormFieldWrapper>
                 )}
                 {isMultiValuesTreeAttribute && (
