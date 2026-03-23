9f94b86ade0299e706e94cf39a0514b6c9cc1bed
Sébastien Berthier <s.berthier@aristid.com>
2026-03-23 14:20:07 +0100

    fix(core): remove wrong index in helpers for awilix modules that are not

--- apps/core/src/domain/record/helpers/index.ts
+++ apps/core/src/domain/record/helpers/index.ts
@@ -4,5 +4,4 @@
 export {default as createRecord} from './createRecord';
 export {default as deleteRecord} from './deleteRecord';
 export {default as sendRecordUpdateEvent} from './sendRecordUpdateEvent';
-export {default as getAccessPermissionFilters} from './getAccessPermissionFilters';
 export {default as findRecords} from './findRecords';
