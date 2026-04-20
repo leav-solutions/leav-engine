05bfab1ae599a43a501b72680c4f5e685a785a7a
Sébastien Berthier <s.berthier@aristid.com>
2026-04-20 18:22:13 +0200

    chore(core): add tsconfig path to @leav/core
    necessary while plugins are still check/test/build in leav-core

--- apps/core/src/__tests__/e2e/api/_fixtures/fakeplugin/domain/fakeDomain.ts
+++ apps/core/src/__tests__/e2e/api/_fixtures/fakeplugin/domain/fakeDomain.ts
@@ -2,9 +2,9 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 
-import {type IQueryInfos} from '../../../../../../_types/queryInfos';
+import {type IQueryInfos} from '@leav/core/_types/queryInfos';
 import {FakePluginActions} from '..';
-import {type IEventsManagerDomain} from '../../../../../../domain/eventsManager/eventsManagerDomain';
+import {type IEventsManagerDomain} from '@leav/core/domain/eventsManager/eventsManagerDomain';
 
 export interface IFakeDomain {
     execWorker({fromTask, ctx}: {fromTask: string; ctx: IQueryInfos}): Promise<void>;
--- apps/core/src/__tests__/e2e/api/_fixtures/fakeplugin/index.ts
+++ apps/core/src/__tests__/e2e/api/_fixtures/fakeplugin/index.ts
@@ -2,17 +2,17 @@
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
 import {type i18n} from 'i18next';
-import {type IPluginInitModule} from '../../../../../_types/plugin';
-import {PermissionTypes} from '../../../../../_types/permissions';
-import {ActionsListIOTypes} from '../../../../../_types/actionsList';
-import {type IValueRepo} from '../../../../../infra/value/valueRepo';
-import {type IAttributeDomain} from '../../../../../domain/attribute/attributeDomain';
-import {type IAttribute} from '../../../../../_types/attribute';
-import {type ITasksManagerDomain} from '../../../../../domain/tasksManager/tasksManagerDomain';
+import {type IPluginInitModule} from '@leav/core/_types/plugin';
+import {PermissionTypes} from '@leav/core/_types/permissions';
+import {ActionsListIOTypes} from '@leav/core/_types/actionsList';
+import {type IValueRepo} from '@leav/core/infra/value/valueRepo';
+import {type IAttributeDomain} from '@leav/core/domain/attribute/attributeDomain';
+import {type IAttribute} from '@leav/core/_types/attribute';
+import {type ITasksManagerDomain} from '@leav/core/domain/tasksManager/tasksManagerDomain';
 import {FakePluginTaskType} from './_types/_types';
 import {type IFakeDomain} from './domain/fakeDomain';
-import {type INotificationDomain} from '../../../../../domain/notification/notificationDomain';
-import {TaskPriority} from '../../../../../_types/tasksManager';
+import {type INotificationDomain} from '@leav/core/domain/notification/notificationDomain';
+import {TaskPriority} from '@leav/core/_types/tasksManager';
 
 interface IDeps {
     translator: i18n;
