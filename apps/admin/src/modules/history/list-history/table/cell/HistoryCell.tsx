273ad5cc496c16b53cc7de9052e1f3746048aac7
Jules HABLOT <j.hablot-ext@aristid.com>
2026-05-11 15:22:19 +0200

    refactor(admin): remove truncate function to use DS
    !1971

--- apps/admin/src/modules/history/list-history/table/cell/HistoryCell.tsx
+++ apps/admin/src/modules/history/list-history/table/cell/HistoryCell.tsx
@@ -1,30 +1,11 @@
 // Copyright LEAV Solutions 2017 until 2023/11/05, Copyright Aristid from 2023/11/06
 // This file is released under LGPL V3
 // License text available at https://www.gnu.org/licenses/lgpl-3.0.txt
-import {KitTooltip} from 'aristid-ds';
-import {useRef, useState} from 'react';
+import {KitTypography} from 'aristid-ds';
 import {historyCellContainer} from './historyCell.module.css';
-import {TOOLTIP_DEFAULT_DELAY_IN_SECONDS} from '_ui/constants';
 
-type HistoryCellProps = {
-    value: string;
-};
-
-export const HistoryCell = ({value}: HistoryCellProps) => {
-    const ref = useRef<HTMLDivElement>(null);
-    const [isTruncated, setIsTruncated] = useState(false);
-
-    const handleMouseEnter = () => {
-        if (ref.current) {
-            setIsTruncated(ref.current.scrollWidth > ref.current.clientWidth);
-        }
-    };
-
-    return (
-        <KitTooltip title={isTruncated ? value : null} mouseEnterDelay={TOOLTIP_DEFAULT_DELAY_IN_SECONDS}>
-            <div ref={ref} className={historyCellContainer} onMouseEnter={handleMouseEnter}>
-                {value}
-            </div>
-        </KitTooltip>
-    );
-};
+export const HistoryCell = ({value}: {value: string | null | undefined}) => (
+    <KitTypography.Text className={historyCellContainer} ellipsis>
+        {value ?? ''}
+    </KitTypography.Text>
+);
