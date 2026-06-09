7a7ee17ff8f9d397ea9eb82452ac389f3e8a31e1
Sébastien Berthier <s.berthier@aristid.com>
2026-06-09 10:08:52 +0200

    fix(core): tscheck mock with vitest

--- apps/core/src/__tests__/mockers/amqp.ts
+++ apps/core/src/__tests__/mockers/amqp.ts
@@ -1,7 +1,11 @@
-export default {
+import {type Mock} from 'vitest';
+
+const amqp: {consumer: {channel: {ack: Mock}}} = {
     consumer: {
         channel: {
             ack: vi.fn(),
         },
     },
 };
+
+export default amqp;
