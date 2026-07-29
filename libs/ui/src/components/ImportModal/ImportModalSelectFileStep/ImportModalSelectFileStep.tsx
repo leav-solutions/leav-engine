68b733d2728e95b0a208f7c3e5341818f715efa8
Sébastien Berthier <s.berthier@aristid.com>
2026-07-29 10:59:44 +0200

    chore(ui): remove xlsx dep
    Disable xlsx dep because of xlsx npm audit.
    ImportModal component is not used anymore for now (was in data-studio)

--- libs/ui/src/components/ImportModal/ImportModalSelectFileStep/ImportModalSelectFileStep.tsx
+++ libs/ui/src/components/ImportModal/ImportModalSelectFileStep/ImportModalSelectFileStep.tsx
@@ -1,28 +1,52 @@
-import {extractArgsFromString} from '@leav/utils';
 import {message, Space, Spin} from 'antd';
 import {KitAlert, KitUpload, KitNotification} from 'aristid-ds';
 import {type IKitDragger} from 'aristid-ds/dist/Kit/DataEntry/Upload/types';
 import {useState} from 'react';
-import {read as xlsxRead, utils as xlsxUtils} from 'xlsx';
 import {useSharedTranslation} from '_ui/hooks/useSharedTranslation';
-import {
-    type AttributesByLibAttributeFragment,
-    type AttributesByLibAttributeLinkAttributeFragment,
-    AttributeType,
-    ImportMode,
-    ImportType,
-} from '_ui/_gqlTypes';
+import {type AttributesByLibAttributeFragment, ImportMode, ImportType} from '_ui/_gqlTypes';
 import {ImportReducerActionTypes} from '../importReducer/importReducer';
 import {useImportReducerContext} from '../importReducer/ImportReducerContext';
 import {type ISheet} from '../_types';
 import {ERROR_NOTIFICATION_DURATION} from '_ui/constants';
 
+/**
+Disable xlsx deps because of xlsx npm audit. This component is not used anymore for now (was in data-studio)
+
+    ├─ xlsx
+    │  ├─ ID: 1108110
+    │  ├─ Issue: Prototype Pollution in sheetJS
+    │  ├─ URL: https://github.com/advisories/GHSA-4r6h-8v6p-xvw6
+    │  ├─ Severity: high
+    │  ├─ Vulnerable Versions: <0.19.3
+    │  │ 
+    │  ├─ Tree Versions
+    │  │  └─ 0.18.5
+    │  │ 
+    │  └─ Dependents
+    │     └─ @leav/ui@workspace:libs/ui
+    │
+    └─ xlsx
+    ├─ ID: 1108111
+    ├─ Issue: SheetJS Regular Expression Denial of Service (ReDoS)
+    ├─ URL: https://github.com/advisories/GHSA-5pgg-2g8v-p4x9
+    ├─ Severity: high
+    ├─ Vulnerable Versions: <0.20.2
+    │ 
+    ├─ Tree Versions
+    │  └─ 0.18.5
+    │ 
+    └─ Dependents
+        └─ @leav/ui@workspace:libs/ui
+        
+see https://gitlab.aristid.com/dev/leav/leav/-/merge_requests/2294/diffs#cd3d3fa5e0a0010cc00d43569de91ab8ea63b3f1 for alternatives
+*/
+// import {read as xlsxRead, utils as xlsxUtils} from 'xlsx';
+// const defaultType = ImportType.STANDARD;
+// const defaultMode = ImportMode.upsert;
+
 interface IImportModalSelectFileStepsProps {
     onGetAttributes: (library: string) => Promise<AttributesByLibAttributeFragment[]>;
 }
-
-const defaultType = ImportType.STANDARD;
-const defaultMode = ImportMode.upsert;
 
 function ImportModalSelectFileStep({onGetAttributes}: IImportModalSelectFileStepsProps): JSX.Element {
     const {t} = useSharedTranslation();
@@ -31,127 +55,127 @@ function ImportModalSelectFileStep({onGetAttributes}: IImportModalSelectFileStep
     const {file} = state;
 
     const _setFileData = async content => {
-        const workbook = xlsxRead(content, {type: 'binary'});
-
         const s: ISheet[] = [];
 
-        for (const sheetName in workbook.Sheets) {
-            if (Object.hasOwn(workbook.Sheets, sheetName)) {
-                // Use the sheet_to_json method to convert excel to json data
-                const sheetData: Array<{[col: string]: string}> = xlsxUtils.sheet_to_json(workbook.Sheets[sheetName], {
-                    blankrows: false,
-                    defval: null,
-                });
+        // disable, see comment below
+        // const workbook = xlsxRead(content, {type: 'binary'});
+        // for (const sheetName in workbook.Sheets) {
+        //     if (Object.hasOwn(workbook.Sheets, sheetName)) {
+        //         // Use the sheet_to_json method to convert excel to json data
+        //         const sheetData: Array<{[col: string]: string}> = xlsxUtils.sheet_to_json(workbook.Sheets[sheetName], {
+        //             blankrows: false,
+        //             defval: null,
+        //         });
 
-                // if sheet is empty we skip it
-                if (!sheetData.length) {
-                    continue;
-                }
+        //         // if sheet is empty we skip it
+        //         if (!sheetData.length) {
+        //             continue;
+        //         }
 
-                // get columns' name
-                const sheetColumns = Object.keys(sheetData[0]);
+        //         // get columns' name
+        //         const sheetColumns = Object.keys(sheetData[0]);
 
-                // match only first line cells (A1, B1, etc.)
-                const firstRowAddresses = Object.keys(workbook.Sheets[sheetName]).filter(
-                    k => !!k.match(/\b[A-Z]+1\b/g),
-                );
+        //         // match only first line cells (A1, B1, etc.)
+        //         const firstRowAddresses = Object.keys(workbook.Sheets[sheetName]).filter(
+        //             k => !!k.match(/\b[A-Z]+1\b/g),
+        //         );
 
-                const isMapped = !!workbook.Sheets[sheetName][firstRowAddresses[0]]?.c;
+        //         const isMapped = !!workbook.Sheets[sheetName][firstRowAddresses[0]]?.c;
 
-                // Mapping is present.
-                if (isMapped) {
-                    const comments: string[] = [];
+        //         // Mapping is present.
+        //         if (isMapped) {
+        //             const comments: string[] = [];
 
-                    for (const address of firstRowAddresses) {
-                        // get first line comments and delete "\n" characters
-                        comments.push(workbook.Sheets[sheetName][address].c?.[0]?.t?.replace(/\n/g, ' ') || null);
-                    }
+        //             for (const address of firstRowAddresses) {
+        //                 // get first line comments and delete "\n" characters
+        //                 comments.push(workbook.Sheets[sheetName][address].c?.[0]?.t?.replace(/\n/g, ' ') || null);
+        //             }
 
-                    const params = extractArgsFromString(comments[0]);
+        //             const params = extractArgsFromString(comments[0]);
 
-                    const sType = params.type ?? defaultType;
-                    const sLibrary = params.library;
-                    const sLinkAttribute = params.linkAttribute;
-                    const sMode = params.mode ?? defaultMode;
-                    let sMapping = [];
-                    const sTreeLinkLibrary = params.treeLinkLibrary;
-                    let sKeyColumnIndex: number;
-                    let sKeyToColumnIndex: number;
+        //             const sType = params.type ?? defaultType;
+        //             const sLibrary = params.library;
+        //             const sLinkAttribute = params.linkAttribute;
+        //             const sMode = params.mode ?? defaultMode;
+        //             let sMapping = [];
+        //             const sTreeLinkLibrary = params.treeLinkLibrary;
+        //             let sKeyColumnIndex: number;
+        //             let sKeyToColumnIndex: number;
 
-                    let sKeyToAttributes: AttributesByLibAttributeFragment[] = [];
+        //             let sKeyToAttributes: AttributesByLibAttributeFragment[] = [];
 
-                    const attributes = await onGetAttributes(sLibrary);
+        //             const attributes = await onGetAttributes(sLibrary);
 
-                    // Extract mapping, keyIndex and keyToIndex from all columns comments
-                    for (const [index, comm] of comments.entries()) {
-                        const commArgs = extractArgsFromString(comm);
-                        sMapping.push(commArgs.id != null ? String(commArgs.id) : null);
+        //             // Extract mapping, keyIndex and keyToIndex from all columns comments
+        //             for (const [index, comm] of comments.entries()) {
+        //                 const commArgs = extractArgsFromString(comm);
+        //                 sMapping.push(commArgs.id != null ? String(commArgs.id) : null);
 
-                        if (commArgs.key) {
-                            sKeyColumnIndex = index;
-                        }
+        //                 if (commArgs.key) {
+        //                     sKeyColumnIndex = index;
+        //                 }
 
-                        if (commArgs.keyTo) {
-                            sKeyToColumnIndex = index;
-                        }
-                    }
+        //                 if (commArgs.keyTo) {
+        //                     sKeyToColumnIndex = index;
+        //                 }
+        //             }
 
-                    const linkAttributeProps =
-                        sKeyToColumnIndex && sLinkAttribute
-                            ? (attributes.find(
-                                  a => a.id === sLinkAttribute,
-                              ) as AttributesByLibAttributeLinkAttributeFragment)
-                            : null;
+        //             const linkAttributeProps =
+        //                 sKeyToColumnIndex && sLinkAttribute
+        //                     ? (attributes.find(
+        //                           a => a.id === sLinkAttribute,
+        //                       ) as AttributesByLibAttributeLinkAttributeFragment)
+        //                     : null;
 
-                    if (linkAttributeProps && (linkAttributeProps.type !== AttributeType.tree || sTreeLinkLibrary)) {
-                        const keyToLibrary =
-                            linkAttributeProps.type === AttributeType.tree
-                                ? sTreeLinkLibrary
-                                : linkAttributeProps?.linked_library?.id;
-                        sKeyToAttributes = await onGetAttributes(keyToLibrary);
-                    }
+        //             if (linkAttributeProps && (linkAttributeProps.type !== AttributeType.tree || sTreeLinkLibrary)) {
+        //                 const keyToLibrary =
+        //                     linkAttributeProps.type === AttributeType.tree
+        //                         ? sTreeLinkLibrary
+        //                         : linkAttributeProps?.linked_library?.id;
+        //                 sKeyToAttributes = await onGetAttributes(keyToLibrary);
+        //             }
 
-                    // Check if all mapping attributes actually exist
-                    sMapping = sMapping.map((mappingAttribute, index) => {
-                        const attributesToCheck = index === sKeyToColumnIndex ? sKeyToAttributes : attributes;
-                        return attributesToCheck.find(attribute => attribute.id === mappingAttribute)
-                            ? mappingAttribute
-                            : null;
-                    });
+        //             // Check if all mapping attributes actually exist
+        //             sMapping = sMapping.map((mappingAttribute, index) => {
+        //                 const attributesToCheck = index === sKeyToColumnIndex ? sKeyToAttributes : attributes;
+        //                 return attributesToCheck.find(attribute => attribute.id === mappingAttribute)
+        //                     ? mappingAttribute
+        //                     : null;
+        //             });
 
-                    s.push({
-                        name: sheetName,
-                        attributes,
-                        columns: sheetColumns,
-                        type: ImportType[sType],
-                        mode: ImportMode[sMode],
-                        library: sLibrary,
-                        linkAttribute: sLinkAttribute,
-                        linkAttributeProps,
-                        keyColumnIndex: sKeyColumnIndex,
-                        keyToColumnIndex: sKeyToColumnIndex,
-                        keyToAttributes: sKeyToAttributes,
-                        treeLinkLibrary: sTreeLinkLibrary,
-                        data: sheetData.length ? sheetData : null,
-                        mapping: sMapping,
-                    });
-                } else {
-                    const sLibrary = state.defaultLibrary;
-                    const attributes = await onGetAttributes(sLibrary);
+        //             s.push({
+        //                 name: sheetName,
+        //                 attributes,
+        //                 columns: sheetColumns,
+        //                 type: ImportType[sType],
+        //                 mode: ImportMode[sMode],
+        //                 library: sLibrary,
+        //                 linkAttribute: sLinkAttribute,
+        //                 linkAttributeProps,
+        //                 keyColumnIndex: sKeyColumnIndex,
+        //                 keyToColumnIndex: sKeyToColumnIndex,
+        //                 keyToAttributes: sKeyToAttributes,
+        //                 treeLinkLibrary: sTreeLinkLibrary,
+        //                 data: sheetData.length ? sheetData : null,
+        //                 mapping: sMapping,
+        //             });
+        //         } else {
+        //             const sLibrary = state.defaultLibrary;
+        //             const attributes = await onGetAttributes(sLibrary);
 
-                    s.push({
-                        name: sheetName,
-                        library: sLibrary,
-                        columns: sheetColumns,
-                        type: defaultType,
-                        mode: defaultMode,
-                        attributes,
-                        mapping: [],
-                        data: sheetData.length ? sheetData : null,
-                    });
-                }
-            }
-        }
+        //             s.push({
+        //                 name: sheetName,
+        //                 library: sLibrary,
+        //                 columns: sheetColumns,
+        //                 type: defaultType,
+        //                 mode: defaultMode,
+        //                 attributes,
+        //                 mapping: [],
+        //                 data: sheetData.length ? sheetData : null,
+        //             });
+        //         }
+        //     }
+        // }
 
         if (!s.length || s.every(sheet => sheet.data === null)) {
             message.error(t('import.empty_file'));
