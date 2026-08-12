7429a2d021f89214c741809d460bfcec7ced256f
Sébastien Berthier <s.berthier@aristid.com>
2026-08-12 11:05:54 +0200

    test(core): fix globalSettings set app name test conflict

--- apps/core/src/__tests__/e2e/api/constants.ts
+++ apps/core/src/__tests__/e2e/api/constants.ts
@@ -1,2 +1,3 @@
 export const GUEST_USER_EMAIL = 'guest@aristid.com';
 export const NON_ADMIN_USER_EMAIL = 'non-admin@aristid.com';
+export const GLOBAL_SETTINGS_APP_NAME = 'My App';
--- apps/core/src/__tests__/e2e/api/permissions/admin/globalSettingsAdminPermissions.test.ts
+++ apps/core/src/__tests__/e2e/api/permissions/admin/globalSettingsAdminPermissions.test.ts
@@ -1,10 +1,11 @@
+import {GLOBAL_SETTINGS_APP_NAME} from '../../constants';
 import {e2eAdminUser, e2eGuestUser, makeGraphQlCall} from '../../e2eUtils';
 
 describe('GlobalSettingsAdminPermissions', () => {
     describe('save global settings', () => {
         it('Should not be authorized to save global settings', async () => {
             const gqlMutation = `mutation {
-                saveGlobalSettings(settings: {name: "test"}) {
+                saveGlobalSettings(settings: {name: "${GLOBAL_SETTINGS_APP_NAME}"}) {
                     name
                 }
             }`;
@@ -14,7 +15,7 @@ describe('GlobalSettingsAdminPermissions', () => {
 
         it('Should be authorized to save global settings', async () => {
             const gqlMutation = `mutation {
-                saveGlobalSettings(settings: {name: "test"}) {
+                saveGlobalSettings(settings: {name: "${GLOBAL_SETTINGS_APP_NAME}"}) {
                     name
                 }
             }`;
